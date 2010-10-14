c7c4ff3d9d17c2190b7aff069895275b3c4d42c0
giuseppe <giuseppe@353d7cec-4390-07bf-75d8-aa00341b881f>
2010-10-14 20:08:41 +0000

    Add Geo-Portal Example

---
+++ geoplatform-gui/application/geo-portal/src/main/java/org/geosdi/geoplatform/gui/client/Application.java
@@ -0,0 +1,17 @@
+package org.geosdi.geoplatform.gui.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Application implements EntryPoint {
+
+
+	/**
+	 * This is the entry point method.
+	 */
+	public void onModuleLoad() {
+
+	}
+}

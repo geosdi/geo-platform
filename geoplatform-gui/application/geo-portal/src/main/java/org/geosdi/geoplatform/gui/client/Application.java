f67bfdbad00e8b3fc75cacf8f955f37e79a0bf69
giuseppe <giuseppe@353d7cec-4390-07bf-75d8-aa00341b881f>
2010-10-15 14:33:11 +0000

    Some Improvements and add Google Map Key in Application.html

--- geoplatform-gui/application/geo-portal/src/main/java/org/geosdi/geoplatform/gui/client/Application.java
+++ geoplatform-gui/application/geo-portal/src/main/java/org/geosdi/geoplatform/gui/client/Application.java
@@ -1,5 +1,8 @@
 package org.geosdi.geoplatform.gui.client;
 
+import org.geosdi.geoplatform.gui.action.ToolbarActionFactory;
+import org.geosdi.geoplatform.gui.client.action.toolbar.ZoomInAction;
+
 import com.google.gwt.core.client.EntryPoint;
 
 /**
@@ -7,11 +10,15 @@ import com.google.gwt.core.client.EntryPoint;
  */
 public class Application implements EntryPoint {
 
-
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
 
+		ZoomInAction action = (ZoomInAction) ToolbarActionFactory.get("zoomIn",
+				null);
+
+		System.out.println("TEST ******************************** "
+				+ action.getTooltip());
 	}
 }

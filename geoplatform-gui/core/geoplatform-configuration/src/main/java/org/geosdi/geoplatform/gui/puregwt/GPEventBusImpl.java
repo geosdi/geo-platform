1334e3a4e086108b4119a346e47d9213f86bb33d
giuseppe <giuseppe@353d7cec-4390-07bf-75d8-aa00341b881f>
2010-12-31 19:18:04 +0000

    Add EventBus Support

---
+++ geoplatform-gui/core/geoplatform-configuration/src/main/java/org/geosdi/geoplatform/gui/puregwt/GPEventBusImpl.java
@@ -0,0 +1,107 @@
+/*
+ *  geo-platform
+ *  Rich webgis framework
+ *  http://geo-plartform.org
+ * ====================================================================
+ *
+ * Copyright (C) 2008-2010 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ *
+ * This program is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version. This program is distributed in the 
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without 
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+ * A PARTICULAR PURPOSE. See the GNU General Public License 
+ * for more details. You should have received a copy of the GNU General 
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/ 
+ *
+ * ====================================================================
+ *
+ * Linking this library statically or dynamically with other modules is 
+ * making a combined work based on this library. Thus, the terms and 
+ * conditions of the GNU General Public License cover the whole combination. 
+ * 
+ * As a special exception, the copyright holders of this library give you permission 
+ * to link this library with independent modules to produce an executable, regardless 
+ * of the license terms of these independent modules, and to copy and distribute 
+ * the resulting executable under terms of your choice, provided that you also meet, 
+ * for each linked independent module, the terms and conditions of the license of 
+ * that module. An independent module is a module which is not derived from or 
+ * based on this library. If you modify this library, you may extend this exception 
+ * to your version of the library, but you are not obligated to do so. If you do not 
+ * wish to do so, delete this exception statement from your version. 
+ *
+ */
+package org.geosdi.geoplatform.gui.puregwt;
+
+import com.google.gwt.event.shared.EventHandler;
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.event.shared.GwtEvent.Type;
+import com.google.gwt.event.shared.SimpleEventBus;
+
+/**
+ * @author giuseppe
+ * 
+ */
+public class GPEventBusImpl implements GPEventBus {
+
+	private SimpleEventBus eventBus = new SimpleEventBus();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.geosdi.geoplatform.gui.puregwt.GPEventBus#addHandler(com.google.gwt
+	 * .event.shared.GwtEvent.Type, com.google.gwt.event.shared.EventHandler)
+	 */
+	@Override
+	public <H extends EventHandler> HandlerRegistration addHandler(
+			Type<H> type, H handler) {
+		// TODO Auto-generated method stub
+		return eventBus.addHandler(type, handler);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.geosdi.geoplatform.gui.puregwt.GPEventBus#addHandlerToSource(com.
+	 * google.gwt.event.shared.GwtEvent.Type, java.lang.Object,
+	 * com.google.gwt.event.shared.EventHandler)
+	 */
+	@Override
+	public <H extends EventHandler> HandlerRegistration addHandlerToSource(
+			Type<H> type, Object source, H handler) {
+		// TODO Auto-generated method stub
+		return eventBus.addHandlerToSource(type, source, handler);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.geosdi.geoplatform.gui.puregwt.GPEventBus#fireEvent(com.google.gwt
+	 * .event.shared.GwtEvent)
+	 */
+	@Override
+	public void fireEvent(GwtEvent<?> event) {
+		// TODO Auto-generated method stub
+		this.eventBus.fireEvent(event);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.geosdi.geoplatform.gui.puregwt.GPEventBus#fireEventFromSource(com
+	 * .google.gwt.event.shared.GwtEvent, java.lang.Object)
+	 */
+	@Override
+	public void fireEventFromSource(GwtEvent<?> event, Object source) {
+		// TODO Auto-generated method stub
+		this.eventBus.fireEventFromSource(event, source);
+	}
+
+}

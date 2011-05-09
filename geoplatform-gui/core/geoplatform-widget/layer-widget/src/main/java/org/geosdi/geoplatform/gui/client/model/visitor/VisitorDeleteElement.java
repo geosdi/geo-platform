05657b20729bd747ed60c9c77387958e8cac1bee
andypower <andypower@353d7cec-4390-07bf-75d8-aa00341b881f>
2011-05-09 10:14:07 +0000

    Coded VisitorDeleteElement

--- geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/model/visitor/VisitorDeleteElement.java
+++ geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/model/visitor/VisitorDeleteElement.java
@@ -35,36 +35,104 @@
  */
 package org.geosdi.geoplatform.gui.client.model.visitor;
 
+import com.extjs.gxt.ui.client.data.ModelData;
+import java.util.List;
+import org.geosdi.geoplatform.gui.client.model.GPRootTreeNode;
+import org.geosdi.geoplatform.gui.model.GPLayerBean;
 import org.geosdi.geoplatform.gui.model.GPRasterBean;
 import org.geosdi.geoplatform.gui.model.GPVectorBean;
 import org.geosdi.geoplatform.gui.model.tree.AbstractFolderTreeNode;
 import org.geosdi.geoplatform.gui.model.tree.AbstractRootTreeNode;
+import org.geosdi.geoplatform.gui.model.tree.GPBeanTreeModel;
 import org.geosdi.geoplatform.gui.model.tree.visitor.IVisitor;
 
 /**
- *
- * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
- * @email  giuseppe.lascaleia@geosdi.org
+ * @author Nazzareno Sileno - CNR IMAA geoSDI Group
+ * @email  nazzareno.sileno@geosdi.org
  */
 public class VisitorDeleteElement extends AbstractVisitTree implements IVisitor {
 
+    private GPBeanTreeModel endPosition;
+    private GPBeanTreeModel tmpElement;
+    private int tmpIndex;
+    private GPRootTreeNode rootElement;
+    private boolean stopIterating;
+
+    public VisitorDeleteElement(GPRootTreeNode root){
+        this.rootElement = root;
+    }
+
+    public VisitorDeleteElement(){}
+
+    public void deleteElement(GPBeanTreeModel removedElement,
+            GPBeanTreeModel parentElementRemoved, int indexElementRemoved) {
+        parentElementRemoved.remove(removedElement);
+        this.endPosition = super.getNextUnvisitedElement(removedElement);
+        this.preorderTraversal();
+    }
+
+    //TODO: Gestire gli indici nel caso di aggiunta di più elementi
+    private void preorderTraversal() {
+        if(this.rootElement == null){
+            this.rootElement = super.findRootElement(this.endPosition);
+        }
+        this.rootElement.setzIndex(this.rootElement.getzIndex()-1);
+        this.tmpElement = this.rootElement;
+        this.tmpIndex = this.tmpElement.getzIndex();
+        while (!this.isPreorderExitCondition()) {
+            this.tmpElement.accept(this);
+            this.tmpElement = super.getNextUnvisitedElement(this.tmpElement);
+        }
+        this.resetVisit();
+    }
+
+    private void resetVisit() {
+        this.endPosition = null;
+        this.tmpIndex = -1;
+        this.stopIterating = false;
+        this.tmpElement = null;
+    }
+
+    private boolean isPreorderExitCondition() {
+        return this.tmpElement == null || this.tmpElement.equals(this.endPosition);
+    }
+
     @Override
     public void visitRoot(AbstractRootTreeNode root) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        //Do nothing on root element
     }
 
     @Override
     public void visitFolder(AbstractFolderTreeNode folder) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        folder.setzIndex(--this.tmpIndex);
+        System.out.println("Visitor Folder set zIndex: " + this.tmpIndex
+                + " to the folder: " + folder.getLabel());
+        List<ModelData> childrens = folder.getChildren();
+        for (int i = 0; i < childrens.size() && !this.stopIterating; i++) {
+            this.tmpElement = (GPBeanTreeModel) childrens.get(i);
+            if (this.endPosition != null && this.isPreorderExitCondition()) {
+                this.tmpElement = this.getPrecedingElement(this.endPosition);
+                this.stopIterating = true;
+                return;
+            }
+            this.tmpElement.accept(this);
+        }
+
+    }
+
+    private void visitLeaf(GPLayerBean leaf) {
+        ((GPBeanTreeModel) leaf).setzIndex(--this.tmpIndex);
+        System.out.println("Visitor Leaf set zIndex: " + this.tmpIndex
+                + " to the leaf: " + leaf.getLabel());
     }
 
     @Override
     public void visitVector(GPVectorBean vector) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.visitLeaf(vector);
     }
 
     @Override
     public void visitRaster(GPRasterBean raster) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        this.visitLeaf(raster);
     }
 }

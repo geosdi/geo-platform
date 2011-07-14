1337202a22c12171ee24667b6c027db6c4302548
luca.paolino@geosdi.org <luca.paolino@geosdi.org@353d7cec-4390-07bf-75d8-aa00341b881f>
2011-07-14 17:41:01 +0000

    changed references to publish services

--- geoplatform-gui/core/geoplatform-ws-client/src/test/java/org/geosdi/geoplatform/GeoPlatformWSTest.java
+++ geoplatform-gui/core/geoplatform-ws-client/src/test/java/org/geosdi/geoplatform/GeoPlatformWSTest.java
@@ -38,7 +38,7 @@ package org.geosdi.geoplatform;
 import junit.framework.Assert;
 
 import org.geosdi.geoplatform.services.GeoPlatformService;
-import org.geosdi.publisher.services.GPPublisherService;
+import org.geosdi.geoplatform.publish.GPPublisherService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
--- geoplatform-gui/extensions/gp-publisher-widget/src/main/java/org/geosdi/geoplatform/gui/server/UploadServlet.java
+++ geoplatform-gui/extensions/gp-publisher-widget/src/main/java/org/geosdi/geoplatform/gui/server/UploadServlet.java
@@ -6,31 +6,31 @@
  *
  * Copyright (C) 2008-2011 geoSDI Group (CNR IMAA - Potenza - ITALY).
  *
- * This program is free software: you can redistribute it and/or modify it 
- * under the terms of the GNU General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version. This program is distributed in the 
- * hope that it will be useful, but WITHOUT ANY WARRANTY; without 
- * even the implied warranty of MERCHANTABILITY or FITNESS FOR 
- * A PARTICULAR PURPOSE. See the GNU General Public License 
- * for more details. You should have received a copy of the GNU General 
- * Public License along with this program. If not, see http://www.gnu.org/licenses/ 
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
  *
  * ====================================================================
  *
- * Linking this library statically or dynamically with other modules is 
- * making a combined work based on this library. Thus, the terms and 
- * conditions of the GNU General Public License cover the whole combination. 
- * 
- * As a special exception, the copyright holders of this library give you permission 
- * to link this library with independent modules to produce an executable, regardless 
- * of the license terms of these independent modules, and to copy and distribute 
- * the resulting executable under terms of your choice, provided that you also meet, 
- * for each linked independent module, the terms and conditions of the license of 
- * that module. An independent module is a module which is not derived from or 
- * based on this library. If you modify this library, you may extend this exception 
- * to your version of the library, but you are not obligated to do so. If you do not 
- * wish to do so, delete this exception statement from your version. 
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  *
  */
 package org.geosdi.geoplatform.gui.server;
@@ -51,11 +51,12 @@ import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FilenameUtils;
+import org.geosdi.geoplatform.exception.ResourceNotFoundFault;
 import org.geosdi.geoplatform.gui.global.GeoPlatformException;
 import org.geosdi.geoplatform.gui.spring.GeoPlatformContextUtil;
-import org.geosdi.publisher.exception.ResourceNotFoundFault;
-import org.geosdi.publisher.responce.InfoPreview;
-import org.geosdi.publisher.services.GPPublisherService;
+import org.geosdi.geoplatform.publish.GPPublisherService;
+import org.geosdi.geoplatform.responce.InfoPreview;
+
 
 /**
  * @author Nazzareno Sileno - CNR IMAA geoSDI Group
@@ -112,10 +113,10 @@ public class UploadServlet extends HttpServlet {
                     resp.setStatus(HttpServletResponse.SC_CREATED);
                     resp.flushBuffer();
                 }
-                infoPreview = this.geoPlatformPublishClient.uploadZIPInPreview(uploadedFile);
+                List<InfoPreview> infoPreviews = this.geoPlatformPublishClient.uploadZIPInPreview(uploadedFile);
                 resp.setContentType("text/x-json;charset=UTF-8");
                 resp.setHeader("Cache-Control", "no-cache");
-                JsonObject jsonObject = this.generateJONObject(infoPreview);
+                JsonObject jsonObject = this.generateJONObject(infoPreviews.get(0));
                 resp.getWriter().write(jsonObject.toString());
                 System.out.println("Json Response: " + resp.getWriter().toString());
                 //geoPlatformPublishClient.publish("previews", "dataTest", infoPreview.getDataStoreName());

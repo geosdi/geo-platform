016c17a3b081a9547b86d569c0d6ce6cd51cc510
glascaleia@gmail.com <glascaleia@gmail.com@353d7cec-4390-07bf-75d8-aa00341b881f>
2011-08-02 06:51:53 +0000

    Commented  the test in publisher-impl that creates unnecessary files in the repo

--- geoplatform-services/geoplatform-modules/geoplatform-publisher/publisher-impl/src/test/java/org/geosdi/geoplatform/publish/PublisherWSTest.java
+++ geoplatform-services/geoplatform-modules/geoplatform-publisher/publisher-impl/src/test/java/org/geosdi/geoplatform/publish/PublisherWSTest.java
@@ -35,19 +35,11 @@
  */
 package org.geosdi.geoplatform.publish;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.slf4j.Logger;
 
 import junit.framework.Assert;
 import junit.framework.TestCase;
-import org.geosdi.geoplatform.exception.ResourceNotFoundFault;
-import org.geosdi.geoplatform.request.Attribute;
-import org.geosdi.geoplatform.request.Feature;
-import org.geosdi.geoplatform.responce.InfoPreview;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -63,89 +55,85 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * In order to execute this test you should provide two shapefiles. The first should be in one ZIP compressed file while the second should be provided provifing its shp, prj, shx and dbf uncrompressed files.
  * You should also modify the paths to access them.
  */
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
-public class PublisherWSTest extends TestCase{
+public class PublisherWSTest extends TestCase {
 
     protected Logger logger = LoggerFactory.getLogger(org.geosdi.geoplatform.publish.PublisherWSTest.class);
-
     @Autowired
     private GPPublisherService gppublisherService;
 
     @Test
-    public void testWS(){
-        Assert.assertNotNull(gppublisherService);
-        try {
-            Attribute attribute1 = new Attribute("int","1", "int");
-            Attribute attribute2 = new Attribute("String","1", "String");
-            List<Attribute> attributeList1 = new ArrayList<Attribute>();
-            attributeList1.add(attribute1);
-            attributeList1.add(attribute2);
-            Feature feature = new Feature("POINT(1 1)", attributeList1);
-            List<Feature> featureList = new ArrayList<Feature>();
-            featureList.add(feature);
-            byte[]  stream = gppublisherService.createSHP("luca", featureList, "feature.shp");
-            FileOutputStream outputStream = new FileOutputStream("C:\\Users\\Luca\\AppData\\Local\\Temp\\geoportal\\zip\\luca\\pippo.zip");
-            outputStream.write(stream);
-            outputStream.close();
-        }
-        catch (Exception ex) {
-                logger.error("\n **** Generic exception"+ex.getMessage());
-                ex.printStackTrace();
-        }
-    }
-
-/*
-    @Test
     public void testWS() {
         Assert.assertNotNull(gppublisherService);
-        try {
-            logger.info("\n **** START TEST");
-            logger.info("\n **** CALL TO UPLOADSHAPEINPREVIEW ON prova.zip");
-            File zipFile = new File("./src/test/resources/prova.zip");
-            List<InfoPreview> infoList = gppublisherService.uploadZIPInPreview("luca",zipFile);
-            for (InfoPreview info: infoList){
-                logger.info("\n **** Preview at: "+info.getWorkspace()+":"+info.getDataStoreName()+" --> "+info.getMessage());
-                logger.info("\n **** continue at: "+info.getCrs()+":"+info.getMinX()+","+info.getMinY());
-            }
-/*            logger.info("\n **** CALL TO PUBLISH ON zip_it_aeropo ");
-            logger.info("\n **** RESULT "+gppublisherService.publish("luca", "preview2", "data", "it_aeropo" ));
-            logger.info("\n **** CALL TO REMOVEFROMPREVIEW zip_it_aree_meteoclimatiche ");
-     
-            File shpFile = new File("./src/test/resources/limiti_adb_4326.shp");
-            File dbfFile = new File("./src/test/resources/limiti_adb_4326.dbf");
-            File shxFile = new File("./src/test/resources/limiti_adb_4326.shx");
-            File prjFile = new File("./src/test/resources/limiti_adb_4326.prj");
-            List<InfoPreview> successfullPreview = null ;
-            logger.info("\n **** CALL TO UPLOADSHAPEINPREVIEW ON Limiti_AdB_4326 files");
-            successfullPreview = gppublisherService.uploadShapeInPreview("rosanna",shpFile, dbfFile, shxFile, prjFile, null);
-            logger.info("\n **** RESULT: "+successfullPreview);
-            if (successfullPreview!=null) {
-                logger.info("\n **** CALL TO PUBLISH ON Limiti_AdB_4326 files");
-                logger.info("\n **** RESULT "+gppublisherService.publish("rosanna","preview2", "data", "limiti_adb_4326"));
-                }
-            List<String> listNames = new ArrayList<String>();
-            listNames.add("it_augustus_ccs");
-            listNames.add("it_augustus_ccs_sede");
-            logger.info("\n **** RESULT MULTIPLE PUBLISHING"+gppublisherService.publishAll("luca","preview2", "data", listNames));
-              logger.info("\n **** CALL TO GETPREVIEWDATASTORES");
-            List<InfoPreview> infoList2 = gppublisherService.getPreviewDataStores("luca");
-             for (InfoPreview info: infoList2){
-                logger.info("\n **** Preview at: "+info.getWorkspace()+":"+info.getDataStoreName()+" --> "+info.getMessage());
-            }
-
-
-            logger.info("\n **** END TEST");
-            } catch (ResourceNotFoundFault ex) {
-                logger.error("\n **** Eccezione nella pubblicazione: "+ex.getMessage());
-                System.out.println("\n **** Eccezione nella pubblicazione: "+ex.getMessage());
-            }
-            catch (Exception ex) {
-                logger.error("\n **** Generic exception"+ex.getMessage());
-                ex.printStackTrace();
-                
-            }
+//        try {
+//            Attribute attribute1 = new Attribute("int", "1", "int");
+//            Attribute attribute2 = new Attribute("String", "1", "String");
+//            List<Attribute> attributeList1 = new ArrayList<Attribute>();
+//            attributeList1.add(attribute1);
+//            attributeList1.add(attribute2);
+//            Feature feature = new Feature("POINT(1 1)", attributeList1);
+//            List<Feature> featureList = new ArrayList<Feature>();
+//            featureList.add(feature);
+//            byte[] stream = gppublisherService.createSHP("luca", featureList, "feature.shp");
+//            FileOutputStream outputStream = new FileOutputStream("C:\\Users\\Luca\\AppData\\Local\\Temp\\geoportal\\zip\\luca\\pippo.zip");
+//            outputStream.write(stream);
+//            outputStream.close();
+//        } catch (Exception ex) {
+//            logger.error("\n **** Generic exception" + ex.getMessage());
+//        }
     }
-*/
+
+    /*
+    @Test
+    public void testWS() {
+    Assert.assertNotNull(gppublisherService);
+    try {
+    logger.info("\n **** START TEST");
+    logger.info("\n **** CALL TO UPLOADSHAPEINPREVIEW ON prova.zip");
+    File zipFile = new File("./src/test/resources/prova.zip");
+    List<InfoPreview> infoList = gppublisherService.uploadZIPInPreview("luca",zipFile);
+    for (InfoPreview info: infoList){
+    logger.info("\n **** Preview at: "+info.getWorkspace()+":"+info.getDataStoreName()+" --> "+info.getMessage());
+    logger.info("\n **** continue at: "+info.getCrs()+":"+info.getMinX()+","+info.getMinY());
+    }
+    /*            logger.info("\n **** CALL TO PUBLISH ON zip_it_aeropo ");
+    logger.info("\n **** RESULT "+gppublisherService.publish("luca", "preview2", "data", "it_aeropo" ));
+    logger.info("\n **** CALL TO REMOVEFROMPREVIEW zip_it_aree_meteoclimatiche ");
+    
+    File shpFile = new File("./src/test/resources/limiti_adb_4326.shp");
+    File dbfFile = new File("./src/test/resources/limiti_adb_4326.dbf");
+    File shxFile = new File("./src/test/resources/limiti_adb_4326.shx");
+    File prjFile = new File("./src/test/resources/limiti_adb_4326.prj");
+    List<InfoPreview> successfullPreview = null ;
+    logger.info("\n **** CALL TO UPLOADSHAPEINPREVIEW ON Limiti_AdB_4326 files");
+    successfullPreview = gppublisherService.uploadShapeInPreview("rosanna",shpFile, dbfFile, shxFile, prjFile, null);
+    logger.info("\n **** RESULT: "+successfullPreview);
+    if (successfullPreview!=null) {
+    logger.info("\n **** CALL TO PUBLISH ON Limiti_AdB_4326 files");
+    logger.info("\n **** RESULT "+gppublisherService.publish("rosanna","preview2", "data", "limiti_adb_4326"));
+    }
+    List<String> listNames = new ArrayList<String>();
+    listNames.add("it_augustus_ccs");
+    listNames.add("it_augustus_ccs_sede");
+    logger.info("\n **** RESULT MULTIPLE PUBLISHING"+gppublisherService.publishAll("luca","preview2", "data", listNames));
+    logger.info("\n **** CALL TO GETPREVIEWDATASTORES");
+    List<InfoPreview> infoList2 = gppublisherService.getPreviewDataStores("luca");
+    for (InfoPreview info: infoList2){
+    logger.info("\n **** Preview at: "+info.getWorkspace()+":"+info.getDataStoreName()+" --> "+info.getMessage());
+    }
+    
+    
+    logger.info("\n **** END TEST");
+    } catch (ResourceNotFoundFault ex) {
+    logger.error("\n **** Eccezione nella pubblicazione: "+ex.getMessage());
+    System.out.println("\n **** Eccezione nella pubblicazione: "+ex.getMessage());
+    }
+    catch (Exception ex) {
+    logger.error("\n **** Generic exception"+ex.getMessage());
+    ex.printStackTrace();
+    
+    }
+    }
+     */
 }

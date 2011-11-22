03a3810ce468bdf4eaac2fee8a29494c49481bc6
michele.santomauro@geosdi.org <michele.santomauro@geosdi.org@353d7cec-4390-07bf-75d8-aa00341b881f>
2011-11-22 21:00:12 +0000

    Added test for GPPooledPBEStringEncryptorDecorator

---
+++ geoplatform-tools/geoplatform-configurator/src/test/java/org/geosdi/geoplatform/jasypt/GPPooledPBEStringEncryptorDecoratorTest.java
@@ -0,0 +1,71 @@
+//<editor-fold defaultstate="collapsed" desc="License">
+/*
+ *  geo-platform
+ *  Rich webgis framework
+ *  http://geo-platform.org
+ * ====================================================================
+ *
+ * Copyright (C) 2008-2011 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ *
+ * ====================================================================
+ *
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
+ *
+ */
+//</editor-fold>
+package org.geosdi.geoplatform.jasypt;
+
+import junit.framework.Assert;
+import org.geosdi.geoplatform.configurator.jasypt.GPPooledPBEStringEncryptorDecorator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+/**
+ * @author Michele Santomauro
+ * @email michele.santomauro@geosdi.org
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext-TEST.xml"})
+public class GPPooledPBEStringEncryptorDecoratorTest {
+
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    protected GPPooledPBEStringEncryptorDecorator pooledPBEStringEncryptorDecorator;
+
+    @Test
+    public void testEncryptedStringEquals() {
+        logger.trace("\n\t@@@ testEncryptedStringEquals @@@");
+        System.out.println("\n\n #### pooledPBEStringEncryptorDecorator: " + pooledPBEStringEncryptorDecorator + "\n\n");
+        Assert.assertTrue(true);
+    }
+}

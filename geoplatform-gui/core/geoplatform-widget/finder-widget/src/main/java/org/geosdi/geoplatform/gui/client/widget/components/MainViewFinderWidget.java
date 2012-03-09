57e87a039505b9d92ee8e89377e0a6e09bb8d1cb
vincenzo.monteverde@geosdi.org <vincenzo.monteverde@geosdi.org@353d7cec-4390-07bf-75d8-aa00341b881f>
2012-03-09 11:50:30 +0000

    ADD Initial implementation

--- geoplatform-gui/core/geoplatform-widget/finder-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/components/MainViewFinderWidget.java
+++ geoplatform-gui/core/geoplatform-widget/finder-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/components/MainViewFinderWidget.java
@@ -35,6 +35,24 @@
  */
 package org.geosdi.geoplatform.gui.client.widget.components;
 
+import com.extjs.gxt.ui.client.Style;
+import com.extjs.gxt.ui.client.Style.Orientation;
+import com.extjs.gxt.ui.client.store.ListStore;
+import com.extjs.gxt.ui.client.widget.Label;
+import com.extjs.gxt.ui.client.widget.LayoutContainer;
+import com.extjs.gxt.ui.client.widget.TabItem;
+import com.extjs.gxt.ui.client.widget.TabPanel;
+import com.extjs.gxt.ui.client.widget.Text;
+import com.extjs.gxt.ui.client.widget.button.Button;
+import com.extjs.gxt.ui.client.widget.form.CheckBox;
+import com.extjs.gxt.ui.client.widget.form.CheckBoxGroup;
+import com.extjs.gxt.ui.client.widget.form.TextField;
+import com.extjs.gxt.ui.client.widget.grid.ColumnConfig;
+import com.extjs.gxt.ui.client.widget.grid.ColumnModel;
+import com.extjs.gxt.ui.client.widget.grid.Grid;
+import com.extjs.gxt.ui.client.widget.layout.ColumnData;
+import com.extjs.gxt.ui.client.widget.layout.ColumnLayout;
+import java.util.Collections;
 import org.geosdi.geoplatform.gui.client.widget.GeoPlatformContentPanel;
 
 /**
@@ -49,15 +67,106 @@ public class MainViewFinderWidget extends GeoPlatformContentPanel {
     }
 
     @Override
-    public void addComponent() {
-    }
-
-    @Override
     public void initSize() {
+        super.setSize(600, 650);
     }
 
     @Override
     public void setPanelProperties() {
         super.setHeaderVisible(false);
+    }
+
+    @Override
+    public void addComponent() {
+        TabPanel tabPanel = new TabPanel();
+        super.add(tabPanel);
+
+        TabItem searchTab = new TabItem("Search");
+        searchTab.setSize(600, 650);
+        tabPanel.add(searchTab);
+
+        searchTab.add(this.addSearchPanel());
+        searchTab.add(this.addGridPanel());
+
+        TabItem metadataTab = new TabItem("Full Metadata");
+        tabPanel.add(metadataTab);
+    }
+
+    private LayoutContainer addSearchPanel() {
+        // Search Panel
+        LayoutContainer searchPanel = new LayoutContainer();
+        searchPanel.setLayout(new ColumnLayout());
+
+        // Search Panel - Left
+        LayoutContainer left = new LayoutContainer();
+        left.setLayout(new ColumnLayout());
+//        left.setStyleAttribute("paddingRight", "10px");
+
+        Label searchLabel = new Label("Search Text");
+        left.add(searchLabel, new ColumnData(300.0));
+
+        TextField<String> searchTextField = new TextField<String>();
+        searchTextField.setWidth(250);
+        left.add(searchTextField);
+
+        Button searchButton = new Button("Search");
+        left.add(searchButton);
+
+        // Search Panel - Right
+        LayoutContainer right = new LayoutContainer();
+        right.setLayout(new ColumnLayout());
+//        right.setStyleAttribute("paddingLeft", "10px");
+
+        Label optionsLabel = new Label("Search Options");
+        right.add(optionsLabel, new ColumnData(240.0));
+
+        CheckBoxGroup optionsCheckboxgroup = new CheckBoxGroup();
+        optionsCheckboxgroup.setOrientation(Orientation.VERTICAL);
+        right.add(optionsCheckboxgroup, new ColumnData(100.0));
+
+        CheckBox TitleCheckbox = new CheckBox();
+        TitleCheckbox.setBoxLabel("Title");
+        TitleCheckbox.setHideLabel(true);
+        optionsCheckboxgroup.add(TitleCheckbox);
+
+        CheckBox abstractCheckbox = new CheckBox();
+        abstractCheckbox.setBoxLabel("Abstract");
+        abstractCheckbox.setHideLabel(true);
+        optionsCheckboxgroup.add(abstractCheckbox);
+
+        CheckBox keywordsCheckbox = new CheckBox();
+        keywordsCheckbox.setBoxLabel("Keywords");
+        keywordsCheckbox.setHideLabel(true);
+        optionsCheckboxgroup.add(keywordsCheckbox);
+
+        CheckBox allCheckbox = new CheckBox();
+        allCheckbox.setBoxLabel("Select/Deselect all");
+        allCheckbox.setHideLabel(true);
+        right.add(allCheckbox, new ColumnData(Style.DEFAULT));
+
+        searchPanel.add(left, new ColumnData(0.6));
+        searchPanel.add(right, new ColumnData(0.4));
+
+        return searchPanel;
+    }
+
+    private LayoutContainer addGridPanel() {
+        // Grid Panel
+        LayoutContainer gridPanel = new LayoutContainer();
+
+        Text resultLabel = new Text("Search Result");
+        gridPanel.add(resultLabel);
+
+        Grid grid = new Grid(new ListStore(), new ColumnModel(Collections.<ColumnConfig>emptyList()));
+        grid.setBorders(true);
+        gridPanel.add(grid);
+
+        Text operationLabel = new Text("Operations with selected");
+        gridPanel.add(operationLabel);
+
+        Button addLayerButton = new Button("Add To Layer Tree");
+        gridPanel.add(addLayerButton);
+
+        return gridPanel;
     }
 }

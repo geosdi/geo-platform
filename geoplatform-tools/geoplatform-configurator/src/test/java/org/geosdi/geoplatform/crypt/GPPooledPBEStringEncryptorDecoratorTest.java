5772705b4a37b7629f0d69ebc07beb524be025f7
glascaleia@gmail.com <glascaleia@gmail.com@353d7cec-4390-07bf-75d8-aa00341b881f>
2012-05-11 18:23:44 +0000

    Some Improvements

--- geoplatform-tools/geoplatform-configurator/src/test/java/org/geosdi/geoplatform/crypt/GPPooledPBEStringEncryptorDecoratorTest.java
+++ geoplatform-tools/geoplatform-configurator/src/test/java/org/geosdi/geoplatform/crypt/GPPooledPBEStringEncryptorDecoratorTest.java
@@ -52,7 +52,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+@ContextConfiguration(locations = {"classpath:applicationContext-jasypt.xml"})
 public class GPPooledPBEStringEncryptorDecoratorTest {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());

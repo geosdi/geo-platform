5493d867393971c164716f69d860f614c8561cb9
vincenzo.monteverde@geosdi.org <vincenzo.monteverde@geosdi.org@353d7cec-4390-07bf-75d8-aa00341b881f>
2012-06-01 14:08:31 +0000

    FIX Invert minX and maxX

--- geoplatform-services/geoplatform-modules/geoplatform-catalog/csw-catalog/csw-catalog-impl/src/main/java/org/geosdi/geoplatform/services/BindingUtility.java
+++ geoplatform-services/geoplatform-modules/geoplatform-catalog/csw-catalog/csw-catalog-impl/src/main/java/org/geosdi/geoplatform/services/BindingUtility.java
@@ -125,10 +125,10 @@ public class BindingUtility {
 
         BBox bBox = new BBox();
 
-        bBox.setMinX(bBoxType.getLowerCorner().get(0));
+        bBox.setMaxX(bBoxType.getLowerCorner().get(0));
         bBox.setMinY(bBoxType.getLowerCorner().get(1));
 
-        bBox.setMaxX(bBoxType.getUpperCorner().get(0));
+        bBox.setMinX(bBoxType.getUpperCorner().get(0));
         bBox.setMaxY(bBoxType.getUpperCorner().get(1));
 
         return bBox;

60374c7fa4d4fc625413183a0c2e4e80e547a6f1
Vincenzo Monteverde <vincenzo.monteverde@geosdi.org>
2012-09-10 23:59:05 +0200

    REF Empiric test

--- test/geoplatform-services-test/src/test/java/org/geosdi/geoplatform/cxf/bus/GPSpringBusConfigurator.java
+++ test/geoplatform-services-test/src/test/java/org/geosdi/geoplatform/cxf/bus/GPSpringBusConfigurator.java
@@ -73,8 +73,8 @@ public class GPSpringBusConfigurator implements Disposable {
     
     @Override
     public void dispose() {
-        if (this.bus != null) {
-            this.bus.shutdown(false);
-        }
+        // if (this.bus != null) {
+            // this.bus.shutdown(false);
+        // }
     }
 }

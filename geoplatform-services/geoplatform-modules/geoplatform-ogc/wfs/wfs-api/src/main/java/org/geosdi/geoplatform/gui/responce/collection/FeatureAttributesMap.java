c326235b5f5d549947ca336c311d186d96be3132
Giuseppe La Scaleia <glascaleia@gmail.com>
2012-12-03 11:27:33 +0100

    FIXED GWT-MAVEN COMPILATION ERROR. Please all committers to have more attentions for this issue

--- geoplatform-services/geoplatform-modules/geoplatform-ogc/wfs/wfs-api/src/main/java/org/geosdi/geoplatform/gui/responce/collection/FeatureAttributesMap.java
+++ geoplatform-services/geoplatform-modules/geoplatform-ogc/wfs/wfs-api/src/main/java/org/geosdi/geoplatform/gui/responce/collection/FeatureAttributesMap.java
@@ -33,7 +33,7 @@
  * wish to do so, delete this exception statement from your version. 
  *
  */
-package org.geosdi.geoplatform.responce.collection;
+package org.geosdi.geoplatform.gui.responce.collection;
 
 import java.io.Serializable;
 import java.util.Map;

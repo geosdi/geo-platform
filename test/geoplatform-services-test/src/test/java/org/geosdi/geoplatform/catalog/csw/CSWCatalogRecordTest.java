054db6e5529bc7f0cfc47daa66f53bebfd528efd
Giuseppe La Scaleia <glascaleia@gmail.com>
2013-04-04 12:41:55 +0200

    Upgrade to cxf 2.7.4 and FIX CSW Tests

--- test/geoplatform-services-test/src/test/java/org/geosdi/geoplatform/catalog/csw/CSWCatalogRecordTest.java
+++ test/geoplatform-services-test/src/test/java/org/geosdi/geoplatform/catalog/csw/CSWCatalogRecordTest.java
@@ -195,7 +195,7 @@ public class CSWCatalogRecordTest extends CSWCatalogTest {
     @Test
     public void testGetRecordsOurCountAreaItaly() throws Exception {
         int tot = cswService.getRecordsCount(catalogFinder);
-        Assert.assertEquals(2835, tot);
+        Assert.assertEquals(2840, tot);
 
         AreaInfo areaInfo = catalogFinder.getAreaInfo();
         areaInfo.setActive(true);
@@ -204,7 +204,7 @@ public class CSWCatalogRecordTest extends CSWCatalogTest {
 
         areaInfo.setAreaSearchType(AreaInfo.AreaSearchType.ENCLOSES);
         int countEncloses = cswService.getRecordsCount(catalogFinder);
-        Assert.assertEquals(40, countEncloses);
+        Assert.assertEquals(36, countEncloses);
 
         areaInfo.setAreaSearchType(AreaInfo.AreaSearchType.IS);
         int countIs = cswService.getRecordsCount(catalogFinder);
@@ -212,11 +212,11 @@ public class CSWCatalogRecordTest extends CSWCatalogTest {
 
         areaInfo.setAreaSearchType(AreaInfo.AreaSearchType.OUTSIDE);
         int countOutside = cswService.getRecordsCount(catalogFinder);
-        Assert.assertEquals(17, countOutside);
+        Assert.assertEquals(16, countOutside);
 
         areaInfo.setAreaSearchType(AreaInfo.AreaSearchType.OVERLAP);
         int countOverlap = cswService.getRecordsCount(catalogFinder);
-        Assert.assertEquals(2818, countOverlap);
+        Assert.assertEquals(2824, countOverlap);
 
         logger.info("\n### TOT: {}\nENCLOSES: {}\nIS: {}\nOUTSIDE {}\nOVERLAP {}",
                 tot, countEncloses, countIs, countOutside, countOverlap);

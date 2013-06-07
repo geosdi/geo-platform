acba4559d4d348600d8c44d4b371c4b32ae284a3
Francesco Izzi <francesco.izzi@gmail.com>
2013-06-07 19:37:11 +0200

    Initial switch to new print system

--- geoplatform-gui/plugin/geoplatform-print/src/main/java/org/geosdi/geoplatform/gui/client/utility/PrintUtility.java
+++ geoplatform-gui/plugin/geoplatform-print/src/main/java/org/geosdi/geoplatform/gui/client/utility/PrintUtility.java
@@ -39,11 +39,26 @@ import java.util.ArrayList;
 import java.util.List;
 import org.geosdi.geoplatform.gui.client.model.DPI;
 import org.geosdi.geoplatform.gui.client.model.PrintTemplate;
+import org.geosdi.geoplatform.gui.client.model.Scale;
+import org.gwtopenmaps.openlayers.client.Bounds;
+import org.gwtopenmaps.openlayers.client.LonLat;
+import org.gwtopenmaps.openlayers.client.Map;
+import org.gwtopenmaps.openlayers.client.OpenLayers;
+import org.gwtopenmaps.openlayers.client.Pixel;
+import org.gwtopenmaps.openlayers.client.Size;
+import org.gwtopenmaps.openlayers.client.Style;
+import org.gwtopenmaps.openlayers.client.StyleMap;
+import org.gwtopenmaps.openlayers.client.control.DragFeature;
+import org.gwtopenmaps.openlayers.client.control.DragFeature.DragFeatureListener;
+import org.gwtopenmaps.openlayers.client.control.DragFeatureOptions;
+import org.gwtopenmaps.openlayers.client.feature.VectorFeature;
+import org.gwtopenmaps.openlayers.client.geometry.Geometry;
+import org.gwtopenmaps.openlayers.client.layer.Vector;
 
 /**
  *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
- * @email  giuseppe.lascaleia@geosdi.org
+ * @email giuseppe.lascaleia@geosdi.org
  */
 public class PrintUtility {
 
@@ -59,7 +74,7 @@ public class PrintUtility {
 
         return dpi;
     }
-    
+
     public static List<PrintTemplate> getTemplate() {
         List<PrintTemplate> templates = new ArrayList<PrintTemplate>();
 
@@ -74,5 +89,97 @@ public class PrintUtility {
         templates.add(new PrintTemplate("A1 Landscape"));
         templates.add(new PrintTemplate("A0 Landscape"));
         return templates;
+    }
+
+    public static List<Scale> getScale() {
+        List<Scale> scales = new ArrayList<Scale>();
+
+        scales.add(new Scale("1:1.000"));
+        scales.add(new Scale("1:1.600"));
+        scales.add(new Scale("1:2.000"));
+        scales.add(new Scale("1:5.000"));
+        scales.add(new Scale("1:10.000"));
+        scales.add(new Scale("1:20.000"));
+        scales.add(new Scale("1:50.000"));
+        scales.add(new Scale("1:100.000"));
+        scales.add(new Scale("1:200.000"));
+        scales.add(new Scale("1:500.000"));
+        scales.add(new Scale("1:1.000.000"));
+        scales.add(new Scale("1:2.000.000"));
+        scales.add(new Scale("1:4.000.000"));
+
+        return scales;
+
+    }
+
+    public static Vector createRectangle(LonLat center, float scale, Map map, double sizeFactor, boolean portrait) {
+
+        Bounds extent = getExtent(center, scale, map, sizeFactor, portrait);
+        Geometry rect = extent.toGeometry();
+        VectorFeature features = new VectorFeature(rect);
+        Vector vectorLayer = new Vector("VectorPrintExtent");
+        vectorLayer.addFeature(features);
+
+        //Define a style for the vectors
+        Style style = new Style();
+        style.setFillColor("white");
+        style.setStrokeColor("violet");
+        style.setStrokeWidth(1);
+        style.setStrokeDashstyle("dash");
+
+        style.setFillOpacity(0.4);
+
+        StyleMap styleMap = new StyleMap(style);
+        vectorLayer.setStyleMap(styleMap);
+
+        return vectorLayer;
+    }
+
+    public static DragFeature enableDragPrintArea(Map map, Vector vectorLayer) {
+        DragFeature dragFeature = createDragFeature(vectorLayer);
+        map.addControl(dragFeature);
+        dragFeature.activate();
+
+        return dragFeature;
+    }
+
+    public static void disableDragPrintArea(Map map, DragFeature dragFeature) {
+        map.removeControl(dragFeature);
+    }
+
+    public static Bounds getExtent(LonLat center, float scale, Map map, double sizeFactor, boolean portrait) {
+        double unitRatio = OpenLayers.getInchesPerUnitMeter();
+        Size size = map.getSize();
+        double w, h;
+        if (!portrait) {
+            w = size.getWidth() * sizeFactor / 72.0 / unitRatio * scale / 2.0;
+            h = size.getHeight() * sizeFactor / 72.0 / unitRatio * scale / 2.0;
+        } else {
+            h = size.getWidth() * sizeFactor / 72.0 / unitRatio * scale / 2.0;
+            w = size.getHeight() * sizeFactor / 72.0 / unitRatio * scale / 2.0;
+        }
+
+        Bounds bbox = new Bounds(center.lon() - w, center.lat() - h, center.lon() + w, center.lat() + h);
+        return bbox;
+
+
+    }
+
+    private static DragFeature createDragFeature(Vector layer) {
+        DragFeatureOptions dragFeatureOptions = new DragFeatureOptions();
+        dragFeatureOptions.onDrag(createDragFeatureListener("onDrag"));
+        dragFeatureOptions.onStart(createDragFeatureListener("onStart"));
+        dragFeatureOptions.onComplete(createDragFeatureListener("onComplete"));
+
+        DragFeature dragFeature = new DragFeature(layer, dragFeatureOptions);
+        return dragFeature;
+    }
+
+    private static DragFeatureListener createDragFeatureListener(final String type) {
+        return new DragFeatureListener() {
+            public void onDragEvent(VectorFeature vectorFeature,
+                    Pixel pixel) {
+            }
+        };
     }
 }

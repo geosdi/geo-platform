a4a43a57f61d3e5855d4b55a94bdd3b77b8a10fd
andypower <nazzareno.sileno@geosdi.org>
2013-06-12 15:55:00 +0200

    Fixed tag paragraph

--- geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-connector-api/src/main/java/org/geosdi/geoplatform/connector/server/request/GPAbstractConnectorRequest.java
+++ geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-connector-api/src/main/java/org/geosdi/geoplatform/connector/server/request/GPAbstractConnectorRequest.java
@@ -81,7 +81,7 @@ public abstract class GPAbstractConnectorRequest<T>
     /**
      * <p>
      * Setting basic configuration for HttpParams
-     * <p/>
+     * </p>
      */
     protected void prepareHttpParams() {
         HttpParams httpParams = this.clientConnection.getParams();

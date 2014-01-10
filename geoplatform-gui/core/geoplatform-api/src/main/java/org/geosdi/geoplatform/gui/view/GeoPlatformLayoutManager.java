71e417691d55a25ed21a0227726162688f78bedd
andypower <ssileno@tiscali.it>
2014-01-10 17:26:52 +0100

    Improvements on south panel

--- geoplatform-gui/core/geoplatform-api/src/main/java/org/geosdi/geoplatform/gui/view/GeoPlatformLayoutManager.java
+++ geoplatform-gui/core/geoplatform-api/src/main/java/org/geosdi/geoplatform/gui/view/GeoPlatformLayoutManager.java
@@ -1,37 +1,35 @@
 /**
  *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
+ * geo-platform Rich webgis framework http://geo-platform.org
+ * ====================================================================
  *
- *   Copyright (C) 2008-2014 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * Copyright (C) 2008-2014 geoSDI Group (CNR IMAA - Potenza - ITALY).
  *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
  *
- *   ====================================================================
+ * ====================================================================
  *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
  *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.view;
 
@@ -43,10 +41,12 @@ import com.extjs.gxt.ui.client.event.WidgetListener;
 import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.Status;
+import com.extjs.gxt.ui.client.widget.TabPanel;
 import com.extjs.gxt.ui.client.widget.Viewport;
 import com.extjs.gxt.ui.client.widget.layout.AccordionLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
+import com.extjs.gxt.ui.client.widget.layout.FitLayout;
 import com.extjs.gxt.ui.client.widget.toolbar.FillToolItem;
 import com.extjs.gxt.ui.client.widget.toolbar.ToolBar;
 import com.google.gwt.core.client.GWT;
@@ -64,6 +64,7 @@ public abstract class GeoPlatformLayoutManager {
     protected ContentPanel east;
     protected ContentPanel west;
     protected ContentPanel south;
+    protected TabPanel southTabPanel;
     protected ContentPanel center;
     protected ContentPanel north;
     protected Status statusMap;
@@ -168,13 +169,25 @@ public abstract class GeoPlatformLayoutManager {
      * Create South Panel in Main UI
      */
     private void createSouth() {
-        south = new ContentPanel();
-        BorderLayoutData southData = new BorderLayoutData(LayoutRegion.SOUTH,
-                100);
+        BorderLayoutData southData = new BorderLayoutData(LayoutRegion.SOUTH, 200,
+                200, 200);
+        southData.setMargins(new Margins(5, 0, 5, 5));
         southData.setSplit(true);
         southData.setCollapsible(true);
         southData.setFloatable(false);
-        southData.setMargins(new Margins(5, 0, 0, 0));
+
+        south = new ContentPanel();
+        south.setBodyBorder(false);
+        south.setLayout(new FitLayout());
+        south.setLayoutOnChange(true);
+        south.setScrollMode(Scroll.AUTOY);
+        south.setHeaderVisible(false);
+//        south.setScrollMode(Scroll.NONE);
+//        south.setHeaderVisible(true);
+        this.southTabPanel = new TabPanel();
+        this.southTabPanel.setAutoWidth(true);
+        this.southTabPanel.setAutoHeight(true);
+        south.add(this.southTabPanel);
 
         south.hide();
 
@@ -198,8 +211,8 @@ public abstract class GeoPlatformLayoutManager {
     /**
      * @return the south
      */
-    public ContentPanel getSouth() {
-        return south;
+    public TabPanel getSouth() {
+        return southTabPanel;
     }
 
     /**

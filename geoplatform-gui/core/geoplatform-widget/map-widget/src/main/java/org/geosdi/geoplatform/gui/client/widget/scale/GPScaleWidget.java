668c8386a5137bb9e4a4618fb25efb638247206e
andypower <ssileno@tiscali.it>
2014-07-10 17:09:49 +0200

    The scale widget is displayed in central panel and it is draggable, it acepts users inputs

--- geoplatform-gui/core/geoplatform-widget/map-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/scale/GPScaleWidget.java
+++ geoplatform-gui/core/geoplatform-widget/map-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/scale/GPScaleWidget.java
@@ -1,41 +1,38 @@
 /**
  *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
+ * geo-platform Rich webgis framework http://geo-platform.org
+ * ====================================================================
  *
- *   Copyright (C) 2008-2014 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * Copyright (C) 2008-2014 geoSDI Group (CNR IMAA - Potenza - ITALY).
  *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
  *
- *   ====================================================================
+ * ====================================================================
  *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
  *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.widget.scale;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.geosdi.geoplatform.gui.configuration.map.puregwt.MapHandlerManager;
@@ -47,6 +44,7 @@ import com.extjs.gxt.ui.client.core.XDOM;
 import com.extjs.gxt.ui.client.event.Events;
 import com.extjs.gxt.ui.client.event.FieldEvent;
 import com.extjs.gxt.ui.client.event.Listener;
+import com.extjs.gxt.ui.client.fx.Draggable;
 import com.extjs.gxt.ui.client.fx.FxConfig;
 import com.extjs.gxt.ui.client.store.ListStore;
 import com.extjs.gxt.ui.client.util.Format;
@@ -55,11 +53,16 @@ import com.extjs.gxt.ui.client.util.Size;
 import com.extjs.gxt.ui.client.widget.ContentPanel;
 import com.extjs.gxt.ui.client.widget.form.ComboBox;
 import com.extjs.gxt.ui.client.widget.form.ComboBox.TriggerAction;
+import com.extjs.gxt.ui.client.widget.form.Field;
+import com.extjs.gxt.ui.client.widget.form.Validator;
+import com.google.common.collect.Lists;
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.Accessibility;
 import com.google.gwt.user.client.ui.RootPanel;
 import org.geosdi.geoplatform.gui.client.i18n.MapModuleConstants;
 import org.geosdi.geoplatform.gui.configuration.map.puregwt.event.ScaleSelectorEvent;
+import org.geosdi.geoplatform.gui.impl.view.LayoutManager;
 import org.geosdi.geoplatform.gui.model.scale.GPScaleBean;
 
 /**
@@ -121,14 +124,17 @@ public class GPScaleWidget extends ContentPanel implements ScaleChangeHandler {
     }
 
     protected void onShowInfo() {
+        Draggable d = new Draggable(this, this.getHeader());
+        d.setContainer(LayoutManager.getInstance().getCenter());
+        d.setUseProxy(false);
+
         RootPanel.get().add(this);
         el().makePositionable(true);
 
         setTitle();
         setText();
 
-        List<GPScaleBean> scales = new ArrayList<GPScaleBean>();
-
+        List<GPScaleBean> scales = Lists.<GPScaleBean>newArrayList();
         scales.add(new GPScaleBean("1:1000"));
         scales.add(new GPScaleBean("1:10000"));
         scales.add(new GPScaleBean("1:100000"));
@@ -148,6 +154,26 @@ public class GPScaleWidget extends ContentPanel implements ScaleChangeHandler {
         comboScale.setTypeAhead(true);
         comboScale.setTriggerAction(TriggerAction.ALL);
 
+        comboScale.setEditable(true);
+        comboScale.setForceSelection(false);
+        comboScale.setValidator(new Validator() {
+
+            @Override
+            public String validate(Field<?> field, String value) {
+                String result = null;
+                try {
+                    Long.parseLong(value.substring(2));
+                } catch (NumberFormatException nfe) {
+                    result = "The scale value: " + value.substring(2) + " is not a number";
+                }
+                if (!value.startsWith("1:")) {
+                    String error = "The scale value must start with string 1:";
+                    result = (result == null) ? error : (result += '\n' + error);
+                }
+                return result;
+            }
+        });
+
         add(comboScale);
 
         Point p = position(XDOM.getViewportSize());
@@ -159,8 +185,18 @@ public class GPScaleWidget extends ContentPanel implements ScaleChangeHandler {
             public void handleEvent(FieldEvent fe) {
                 ComboBox cb = (ComboBox) fe.getComponent();
                 GPScaleBean s = (GPScaleBean) cb.getValue();
-                selectorEvent.setValue(s);
-                MapHandlerManager.fireEvent(selectorEvent);
+                scaleSelectedElement(s);
+            }
+        });
+        comboScale.addListener(Events.OnKeyPress, new Listener<FieldEvent>() {
+            @Override
+            public void handleEvent(FieldEvent fe) {
+                ComboBox cb = (ComboBox) fe.getComponent();
+                if (fe.getKeyCode() == KeyCodes.KEY_ENTER && cb.validate()) {
+                    GPScaleBean scaleBean = new GPScaleBean(cb.getRawValue());
+                    scaleSelectedElement(scaleBean);
+                }
+
             }
         });
 
@@ -168,12 +204,21 @@ public class GPScaleWidget extends ContentPanel implements ScaleChangeHandler {
 
     }
 
+    private void scaleSelectedElement(GPScaleBean scaleBean) {
+        selectorEvent.setValue(scaleBean);
+        MapHandlerManager.fireEvent(selectorEvent);
+    }
+
     protected Point position(Size s) {
         this.size = s;
-        int left = this.size.width - config.width - 10
-                + XDOM.getBodyScrollLeft();
-        int top = this.size.height - config.height - 54
-                - (level * (config.height + 10)) + XDOM.getBodyScrollTop();
+        int left = LayoutManager.getInstance().getCenter().getWidth()
+                + LayoutManager.getInstance().getCenter().getPosition(true).x - this.config.width - 10;
+        int top = LayoutManager.getInstance().getCenter().getPosition(true).y
+                + LayoutManager.getInstance().getCenter().getHeight() - config.height - 50;
+//        int left = this.size.width - config.width - 10
+//                + XDOM.getBodyScrollLeft();
+//        int top = this.size.height - config.height - 54
+//                - (level * (config.height + 10)) + XDOM.getBodyScrollTop();
 
         return new Point(left, top);
     }

e0ea48468826df2333a3572680b4a19cb3b63973
andypower <ssileno@tiscali.it>
2014-07-11 17:19:40 +0200

    The layer panels: legend widget and layer tree  can be disposed using the property file indicating the west panel or the east panel

--- geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/LayerManagementWidget.java
+++ geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/LayerManagementWidget.java
@@ -1,37 +1,35 @@
 /**
  *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
+ * geo-platform Rich webgis framework http://geo-platform.org
+ * ====================================================================
  *
- *   Copyright (C) 2008-2014 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * Copyright (C) 2008-2014 geoSDI Group (CNR IMAA - Potenza - ITALY).
  *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
  *
- *   ====================================================================
+ * ====================================================================
  *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
  *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.widget;
 
@@ -42,9 +40,12 @@ import com.extjs.gxt.ui.client.event.WidgetListener;
 import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
+import com.google.gwt.user.client.ui.Widget;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.geosdi.geoplatform.gui.client.i18n.LayerModuleConstants;
+import org.geosdi.geoplatform.gui.client.model.layout.LayerBeanLayoutFactory;
+import org.geosdi.geoplatform.gui.impl.view.LayoutManager;
 
 /**
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
@@ -61,7 +62,6 @@ public class LayerManagementWidget extends GeoPlatformContentPanel {
     public LayerManagementWidget(LayerTreePanel theLayerTreePanel,
             GPLegendPanel theLegendPanel) {
         super(true);
-        
         this.layerTreePanel = theLayerTreePanel;
         this.legendPanel = theLegendPanel;
     }
@@ -71,13 +71,54 @@ public class LayerManagementWidget extends GeoPlatformContentPanel {
         BorderLayoutData northData = new BorderLayoutData(LayoutRegion.NORTH);
         northData.setMargins(new Margins(5, 5, 0, 5));
 
-        super.add(this.layerTreePanel, northData);
+        LayerManagementWidget.super.add(layerTreePanel, northData);
 
         BorderLayoutData southData = new BorderLayoutData(LayoutRegion.SOUTH,
                 180);
         southData.setMargins(new Margins(5, 5, 5, 5));
+        if (LayerBeanLayoutFactory.getLayerBeanLayout().getLayerTreePosition()
+                == LayerBeanLayoutFactory.getLayerBeanLayout().getLegendPosition()) {
+            LayerManagementWidget.super.add(legendPanel, southData);
+        }
+//                        if (!LayoutManager.isEastVisible()) {
+//                            LayoutManager.manageEast(true);
+//                        }
+//                        ContentPanel cp = new ContentPanel();
+//                        cp.add(legendPanel);
+//                        LayoutManager.addComponentToEast(cp);          
 
-        super.add(this.legendPanel, southData);
+    }
+
+    public void showLayersWidget() {
+        this.showLayerPanel(LayerBeanLayoutFactory.getLayerBeanLayout().getLayerTreePosition(), this);
+        if (LayerBeanLayoutFactory.getLayerBeanLayout().getLayerTreePosition()
+                != LayerBeanLayoutFactory.getLayerBeanLayout().getLegendPosition()) {
+            this.showLayerPanel(LayerBeanLayoutFactory.getLayerBeanLayout().getLegendPosition(), legendPanel);
+        }
+    }
+
+    private void showLayerPanel(int panel, Widget w) {
+        if (!LayoutManager.isPanelVisible(panel)) {
+            LayoutManager.managePanel(panel, true);
+        }
+        LayoutManager.addComponentToPanel(panel, w);
+    }
+
+    public void hideLayersWidget() {
+        this.hideLayerPanel(LayerBeanLayoutFactory.getLayerBeanLayout().getLayerTreePosition(), this);
+        if (LayerBeanLayoutFactory.getLayerBeanLayout().getLayerTreePosition()
+                != LayerBeanLayoutFactory.getLayerBeanLayout().getLegendPosition()) {
+            this.hideLayerPanel(LayerBeanLayoutFactory.getLayerBeanLayout().getLegendPosition(), legendPanel);
+        }
+    }
+
+    private void hideLayerPanel(int panel, Widget w) {
+        if (LayoutManager.isWidgetPresentOnPanel(panel, w)) {
+            LayoutManager.removeComponentFromPanel(panel, w);
+            if (!LayoutManager.isOneWidgetVisibleAtPanel(panel)) {
+                LayoutManager.managePanel(panel, false);
+            }
+        }
     }
 
     @Override
@@ -95,7 +136,11 @@ public class LayerManagementWidget extends GeoPlatformContentPanel {
             @Override
             public void widgetResized(ComponentEvent ce) {
                 if (getHeight() > 0) {
-                    layerTreePanel.setHeight(getHeight() - 220);
+                    layerTreePanel.setHeight(getHeight());
+                    if (LayerBeanLayoutFactory.getLayerBeanLayout().getLayerTreePosition()
+                            != LayerBeanLayoutFactory.getLayerBeanLayout().getLegendPosition()) {
+                        legendPanel.setHeight(getHeight());
+                    }
                 }
             }
         });
@@ -116,4 +161,5 @@ public class LayerManagementWidget extends GeoPlatformContentPanel {
     public GPLegendPanel getLegendPanel() {
         return legendPanel;
     }
+
 }

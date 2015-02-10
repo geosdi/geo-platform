a5fbb6c4a47ea25257900addb0f7bc0648118675
Giuseppe La Scaleia <glascaleia@gmail.com>
2015-02-10 15:49:36 +0100

    FIXED Bug on Generic GPWidgetSizeHandler for GeoPlatformTabItem

--- geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/FolderPropertiesWidget.java
+++ geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/FolderPropertiesWidget.java
@@ -1,37 +1,35 @@
 /**
  *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
+ * geo-platform Rich webgis framework http://geo-platform.org
+ * ====================================================================
  *
- *   Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
  *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
  *
- *   ====================================================================
+ * ====================================================================
  *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
  *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.widget;
 
@@ -58,7 +56,7 @@ public class FolderPropertiesWidget extends GeoPlatformWindow
         implements GPTreeBindingFolderHandler, GPWidgetSizeHandler {
 
     private FolderTabWidget foldersTabWidget;
-    private VerticalPanel vp;
+    private final VerticalPanel vp = new VerticalPanel();
     private FolderTreeNode model;
 
     public FolderPropertiesWidget() {
@@ -67,7 +65,6 @@ public class FolderPropertiesWidget extends GeoPlatformWindow
 
     @Override
     public void addComponent() {
-        this.vp = new VerticalPanel();
         vp.setSpacing(10);
 
         this.foldersTabWidget = new FolderTabWidget();
@@ -77,11 +74,12 @@ public class FolderPropertiesWidget extends GeoPlatformWindow
 
         Button close = new Button(ButtonsConstants.INSTANCE.closeText(),
                 new SelectionListener<ButtonEvent>() {
-            @Override
-            public void componentSelected(ButtonEvent ce) {
-                hide();
-            }
-        });
+
+                    @Override
+                    public void componentSelected(ButtonEvent ce) {
+                        hide();
+                    }
+                });
 
         super.addButton(close);
     }
@@ -94,7 +92,8 @@ public class FolderPropertiesWidget extends GeoPlatformWindow
 
     @Override
     public void setWindowProperties() {
-        setHeadingHtml(LayerModuleConstants.INSTANCE.FolderPropertiesWidget_headingText());
+        setHeadingHtml(
+                LayerModuleConstants.INSTANCE.FolderPropertiesWidget_headingText());
         setModal(true);
         setResizable(false);
         setLayout(new FlowLayout());
@@ -102,6 +101,7 @@ public class FolderPropertiesWidget extends GeoPlatformWindow
         setCollapsible(true);
 
         addWindowListener(new WindowListener() {
+
             @Override
             public void windowShow(WindowEvent we) {
                 foldersTabWidget.bind(model);
@@ -127,7 +127,9 @@ public class FolderPropertiesWidget extends GeoPlatformWindow
      *
      */
     public void addHandlers() {
-        WidgetPropertiesHandlerManager.addHandler(GPTreeBindingFolderHandler.TYPE, this);
-        WidgetPropertiesHandlerManager.addHandler(GPWidgetSizeHandler.TYPE, this);
+        WidgetPropertiesHandlerManager.addHandler(
+                GPTreeBindingFolderHandler.TYPE, this);
+        WidgetPropertiesHandlerManager.addHandlerToSource(GPWidgetSizeHandler.TYPE,
+                this.vp, this);
     }
 }

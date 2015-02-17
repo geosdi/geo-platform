f122a4815bd1ecb7fc088d21923fcdac91edb79e
Giuseppe La Scaleia <glascaleia@gmail.com>
2015-02-17 16:16:12 +0100

    FIXED Heavy Bug On GeoPlatformMap Widget on Resize Browser Events

--- geoplatform-gui/core/geoplatform-widget/map-widget/src/main/java/org/geosdi/geoplatform/gui/client/mvc/MapView.java
+++ geoplatform-gui/core/geoplatform-widget/map-widget/src/main/java/org/geosdi/geoplatform/gui/client/mvc/MapView.java
@@ -1,37 +1,35 @@
 /**
  *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
+ * geo-platform Rich webgis framework http://geo-platform.org
+ * ====================================================================
  *
- *   Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
  *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
  *
- *   ====================================================================
+ * ====================================================================
  *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
  *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.mvc;
 
@@ -68,7 +66,8 @@ public class MapView extends GeoPlatformView {
     public MapView(Controller controller) {
         super(controller);
         this.mapLayout = new MapLayoutWidget();
-        Registry.register(MapRegistryEnum.MAP_LAYOUT_WIDGET.toString(), this.mapLayout);
+        Registry.register(MapRegistryEnum.MAP_LAYOUT_WIDGET.toString(),
+                this.mapLayout);
 //        this.revGeoWidget = new ReverseGeocodingWidget(this.mapLayout);
     }
 
@@ -120,7 +119,8 @@ public class MapView extends GeoPlatformView {
      * @param event
      */
     private void onAttachToolbar() {
-        this.buttonBar = new MapToolbar(mapLayout, GeoPlatformUtils.getInstance().
+        this.buttonBar = new MapToolbar(mapLayout,
+                GeoPlatformUtils.getInstance().
                 getGlobalConfiguration().getToolbarContainerTool().getGroupTools());
 
         if (this.buttonBar.getItemsCount() > 0) {
@@ -181,6 +181,7 @@ public class MapView extends GeoPlatformView {
         Component component = toolBar.getItemByItemId("USER_MENU");
         Button button = (Button) component;
         String domain = button.getHtml();
-        button.setText(event.getData().toString() + domain.substring(domain.indexOf("@") - 1));
+        button.setText(event.getData().toString() + domain.substring(
+                domain.indexOf("@") - 1));
     }
 }

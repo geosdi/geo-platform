a71687e6c7eda56c546bee45f2f0cf27a708b3b7
andypower <ssileno@tiscali.it>
2015-02-24 18:07:23 +0100

    Managing layer OGC getCapabilities info BBOX and SRS from ancestor layer in case of lack of layer info

--- geoplatform-services/geoplatform-modules/geoplatform-ogc/wms/wms-impl/src/main/java/org/geosdi/geoplatform/services/GPWMSServiceImpl.java
+++ geoplatform-services/geoplatform-modules/geoplatform-ogc/wms/wms-impl/src/main/java/org/geosdi/geoplatform/services/GPWMSServiceImpl.java
@@ -1,37 +1,35 @@
 /**
  *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
+ * geo-platform Rich webgis framework http://geo-platform.org
+ * ====================================================================
  *
- *   Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
  *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
  *
- *   ====================================================================
+ * ====================================================================
  *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
  *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
  */
 package org.geosdi.geoplatform.services;
 
@@ -159,14 +157,14 @@ public class GPWMSServiceImpl implements GPWMSService {
             String urlServer) {
         List<RasterLayerDTO> shortLayers = Lists.<RasterLayerDTO>newArrayList();
 
-        RasterLayerDTO raster = this.getRasterAndSubRaster(layer, urlServer);
+        RasterLayerDTO raster = this.getRasterAndSubRaster(layer, layer, urlServer);
         shortLayers.add(raster);
 
         return shortLayers;
     }
 
-    private RasterLayerDTO getRasterAndSubRaster(Layer layer, String urlServer) {
-        RasterLayerDTO raster = this.convertLayerToRaster(layer, urlServer);
+    private RasterLayerDTO getRasterAndSubRaster(Layer ancestorLayer, Layer layer, String urlServer) {
+        RasterLayerDTO raster = this.convertLayerToRaster(ancestorLayer, layer, urlServer);
 
         List<Layer> subLayerList = layer.getLayerChildren();
         List<RasterLayerDTO> subRasterList = Lists.<RasterLayerDTO>newArrayListWithExpectedSize(
@@ -175,7 +173,7 @@ public class GPWMSServiceImpl implements GPWMSService {
 
         // ADD subRaster
         for (Layer layerIth : subLayerList) {
-            RasterLayerDTO rasterIth = this.getRasterAndSubRaster(layerIth,
+            RasterLayerDTO rasterIth = this.getRasterAndSubRaster(ancestorLayer, layerIth,
                     urlServer);
             subRasterList.add(rasterIth);
         }
@@ -183,7 +181,7 @@ public class GPWMSServiceImpl implements GPWMSService {
         return raster;
     }
 
-    private RasterLayerDTO convertLayerToRaster(Layer layer, String urlServer) {
+    private RasterLayerDTO convertLayerToRaster(Layer ancestorLayer, Layer layer, String urlServer) {
         RasterLayerDTO raster = new RasterLayerDTO();
 
         raster.setUrlServer(this.getUrlServer(urlServer));
@@ -199,7 +197,7 @@ public class GPWMSServiceImpl implements GPWMSService {
         logger.debug("ADDITIONAL BOUNDS ###############################"
                 + additionalBounds.toString());
 
-        if (additionalBounds.size() > 0) {
+        if (!additionalBounds.isEmpty()) {
             if (additionalBounds.containsKey(EPSG_GOOGLE)
                     || additionalBounds.containsKey(EPSG_3857)) {
                 CRSEnvelope env = additionalBounds
@@ -214,7 +212,29 @@ public class GPWMSServiceImpl implements GPWMSService {
                 raster.setSrs(EPSG_4326);
             }
 
+        } else {
+            additionalBounds = ancestorLayer.getBoundingBoxes();
+            if (additionalBounds.containsKey(EPSG_GOOGLE)
+                    || additionalBounds.containsKey(EPSG_3857)) {
+                CRSEnvelope env = additionalBounds
+                        .get(EPSG_GOOGLE);
+                if (env == null) {
+                    env = additionalBounds.get(EPSG_3857);
+                }
+                raster.setBbox(this.createBbox(env));
+                raster.setSrs(env.getEPSGCode());
+                logger.info("GOOGLE");
+            } else {
+                raster.setBbox(this.createBbox(ancestorLayer.getLatLonBoundingBox()));
+                raster.setSrs(EPSG_4326);
+                logger.info("4326");
+            }
+
         }
+
+        logger.debug("Raster Name: " + raster.getName());
+        logger.debug("Raster BBOX: " + raster.getBbox());
+        logger.debug("Raster SRS: " + raster.getSrs());
 
         if (urlServer.contains(GEB)) {
             if (layer.getLatLonBoundingBox() != null) {

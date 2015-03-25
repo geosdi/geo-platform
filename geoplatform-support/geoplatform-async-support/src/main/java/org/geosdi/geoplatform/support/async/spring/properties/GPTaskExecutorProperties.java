97a6e071b2c12b4c2715a6a3aad660427dcfcc0b
Giuseppe La Scaleia <glascaleia@gmail.com>
2015-03-25 14:32:59 +0100

    Improvements For Async Module Support

--- geoplatform-support/geoplatform-async-support/src/main/java/org/geosdi/geoplatform/support/async/spring/properties/GPTaskExecutorProperties.java
+++ geoplatform-support/geoplatform-async-support/src/main/java/org/geosdi/geoplatform/support/async/spring/properties/GPTaskExecutorProperties.java
@@ -1,37 +1,35 @@
 /**
  *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
+ * geo-platform Rich webgis framework http://geo-platform.org
+ * ====================================================================
  *
- *   Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
  *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
  *
- *   ====================================================================
+ * ====================================================================
  *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
  *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
  */
 package org.geosdi.geoplatform.support.async.spring.properties;
 
@@ -52,6 +50,7 @@ public class GPTaskExecutorProperties implements TaskExecutorProperties {
     static final Integer DEFAULT_CORE_POOL_SIZE = 7;
     static final Integer DEFAULT_MAX_POOL_SIZE = 42;
     static final Integer DEFAULT_QUEUE_CAPACITY = 11;
+    static final Boolean DEFAULT_WAIT_FOR_TASKS_TO_COMPLETE_ON_SHUTDOWN = Boolean.FALSE;
 
     @Value(value = "gpAsyncConfigurator{gp.async.corePoolSize:@null}")
     private Integer corePoolSize;
@@ -59,6 +58,8 @@ public class GPTaskExecutorProperties implements TaskExecutorProperties {
     private Integer maxPoolSize;
     @Value(value = "gpAsyncConfigurator{gp.async.queueCapacity:@null}")
     private Integer queueCapacity;
+    @Value(value = "gpAsyncConfigurator{gp.async.waitForTasksToCompleteOnShutdown:@null}")
+    private Boolean waitForTasksToCompleteOnShutdown;
     @Value(value = "gpAsyncConfigurator{gp.async.threadNamePrefix:@null}")
     private String threadNamePrefix;
 
@@ -80,6 +81,16 @@ public class GPTaskExecutorProperties implements TaskExecutorProperties {
                 ? DEFAULT_QUEUE_CAPACITY : this.queueCapacity);
     }
 
+    /**
+     * @return the waitForTasksToCompleteOnShutdown
+     */
+    public Boolean getWaitForTasksToCompleteOnShutdown() {
+        return this.waitForTasksToCompleteOnShutdown
+                = ((this.waitForTasksToCompleteOnShutdown == null)
+                        ? DEFAULT_WAIT_FOR_TASKS_TO_COMPLETE_ON_SHUTDOWN
+                        : this.waitForTasksToCompleteOnShutdown);
+    }
+
     @Override
     public String getThreadNamePrefix() {
         return this.threadNamePrefix = ((this.threadNamePrefix == null)
@@ -92,6 +103,7 @@ public class GPTaskExecutorProperties implements TaskExecutorProperties {
                 + "corePoolSize = " + getCorePoolSize()
                 + ", maxPoolSize = " + getMaxPoolSize()
                 + ", queueCapacity = " + getQueueCapacity()
+                + ", waitForTasksToCompleteOnShutdown = " + getWaitForTasksToCompleteOnShutdown()
                 + ", threadNamePrefix = " + getThreadNamePrefix() + '}';
     }
 

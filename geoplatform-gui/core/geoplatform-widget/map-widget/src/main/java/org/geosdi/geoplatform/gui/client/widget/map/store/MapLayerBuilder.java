165390ba9bb2da3f32e5f8e69abcee6569057872
andypower <ssileno@tiscali.it>
2015-06-08 11:32:12 +0200

    Improvements

--- geoplatform-gui/core/geoplatform-widget/map-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/map/store/MapLayerBuilder.java
+++ geoplatform-gui/core/geoplatform-widget/map-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/map/store/MapLayerBuilder.java
@@ -1,37 +1,35 @@
 /**
  *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
+ * geo-platform Rich webgis framework http://geo-platform.org
+ * ====================================================================
  *
- *   Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
  *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
  *
- *   ====================================================================
+ * ====================================================================
  *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
  *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.widget.map.store;
 
@@ -47,6 +45,7 @@ import org.geosdi.geoplatform.gui.impl.map.store.AbstractMapLayerBuilder;
 import org.geosdi.geoplatform.gui.model.GPLayerBean;
 import org.geosdi.geoplatform.gui.model.GPRasterBean;
 import org.geosdi.geoplatform.gui.model.GPVectorBean;
+import org.geosdi.geoplatform.gui.shared.util.GPSharedUtils;
 import org.gwtopenmaps.openlayers.client.Bounds;
 import org.gwtopenmaps.openlayers.client.MapWidget;
 import org.gwtopenmaps.openlayers.client.Projection;
@@ -76,7 +75,7 @@ public class MapLayerBuilder extends AbstractMapLayerBuilder<GPLayerBean> {
         }
         wmsParams.setTransparent(Boolean.TRUE);
         final String cqlFilter = rasterBean.getCqlFilter();
-        if (cqlFilter != null && !cqlFilter.equals("")) {
+        if (GPSharedUtils.isNotEmpty(cqlFilter)) {
             wmsParams.setCQLFilter(rasterBean.getCqlFilter());
         }
 
@@ -142,7 +141,7 @@ public class MapLayerBuilder extends AbstractMapLayerBuilder<GPLayerBean> {
         wmsParams.setStyles("");
         wmsParams.setTransparent(Boolean.TRUE);
         final String cqlFilter = vectorBean.getCqlFilter();
-        if (cqlFilter != null && !cqlFilter.equals("")) {
+        if (GPSharedUtils.isNotEmpty(cqlFilter)) {
             wmsParams.setCQLFilter(vectorBean.getCqlFilter());
         }
 

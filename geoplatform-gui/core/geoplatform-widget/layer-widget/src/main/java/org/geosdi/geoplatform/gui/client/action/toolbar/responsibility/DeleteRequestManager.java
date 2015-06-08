51ebf1d282cfae9a28193636b81b56c7463238b0
andypower <ssileno@tiscali.it>
2015-06-08 12:37:48 +0200

    Try to fix multilayer deletion issue

--- geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/action/toolbar/responsibility/DeleteRequestManager.java
+++ geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/action/toolbar/responsibility/DeleteRequestManager.java
@@ -1,37 +1,35 @@
 /**
  *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
+ * geo-platform Rich webgis framework http://geo-platform.org
+ * ====================================================================
  *
- *   Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
  *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
  *
- *   ====================================================================
+ * ====================================================================
  *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
  *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.action.toolbar.responsibility;
 
@@ -39,9 +37,12 @@ import com.extjs.gxt.ui.client.event.Listener;
 import com.extjs.gxt.ui.client.event.MessageBoxEvent;
 import com.extjs.gxt.ui.client.widget.Dialog;
 import com.extjs.gxt.ui.client.widget.treepanel.TreePanel;
+import java.util.Collections;
+import java.util.List;
 import org.geosdi.geoplatform.gui.client.i18n.LayerModuleConstants;
 import org.geosdi.geoplatform.gui.configuration.message.GeoPlatformMessage;
 import org.geosdi.geoplatform.gui.model.tree.GPBeanTreeModel;
+import org.geosdi.geoplatform.gui.model.tree.comparator.GPBeanTreeZIndexComparator;
 
 /**
  *
@@ -64,17 +65,18 @@ public class DeleteRequestManager {
                 DeleteRequestManager_confirmDeleteTitleText(),
                 LayerModuleConstants.INSTANCE.DeleteRequestManager_confirmDeleteBodyText(),
                 new Listener<MessageBoxEvent>() {
-            @Override
-            public void handleEvent(MessageBoxEvent be) {
-                if (Dialog.YES.equals(be.getButtonClicked().getItemId())) {
-                    for (GPBeanTreeModel model
-                            : DeleteRequestManager.this.tree.getSelectionModel().getSelectedItems()) {
-                        DeleteRequestManager.this.deleteFolder.deleteRequest(
-                                model);
+                    @Override
+                    public void handleEvent(MessageBoxEvent be) {
+                        if (Dialog.YES.equals(be.getButtonClicked().getItemId())) {
+                            List<GPBeanTreeModel> selectedItems = DeleteRequestManager.this.tree.getSelectionModel().getSelectedItems();
+                            Collections.sort(selectedItems, new GPBeanTreeZIndexComparator());
+                            for (GPBeanTreeModel model : selectedItems) {
+                                DeleteRequestManager.this.deleteFolder.deleteRequest(
+                                        model);
+                            }
+                        }
                     }
-                }
-            }
-        });
+                });
 
     }
 

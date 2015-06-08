2e87bc3a66ca702eec895f09de677a79cc4fe0dd
andypower <ssileno@tiscali.it>
2015-06-08 17:00:34 +0200

    ADD open catalog finder tootlbar plugin ADD gui component id: OPEN_CATALOG

---
+++ geoplatform-gui/core/geoplatform-widget/finder-widget/src/main/java/org/geosdi/geoplatform/gui/client/plugin/tree/toolbar/OpenCatalogToolbarPlugin.java
@@ -0,0 +1,78 @@
+/**
+ *
+ * geo-platform Rich webgis framework http://geo-platform.org
+ * ====================================================================
+ *
+ * Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
+ *
+ * ====================================================================
+ *
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
+ */
+package org.geosdi.geoplatform.gui.client.plugin.tree.toolbar;
+
+import com.extjs.gxt.ui.client.widget.treepanel.TreePanel;
+import org.geosdi.geoplatform.gui.action.tree.ToolbarLayerTreeAction;
+import org.geosdi.geoplatform.gui.client.action.toolbar.WmsFromMetadataTreeAction;
+import org.geosdi.geoplatform.gui.model.tree.TreeStatusEnum;
+import org.geosdi.geoplatform.gui.plugin.tree.toolbar.TreeToolbarPluginButton;
+import org.geosdi.geoplatform.gui.plugin.tree.toolbar.TreeToolbarRegion;
+
+/**
+ * @author Nazzareno Sileno - CNR IMAA geoSDI Group
+ * @email nazzareno.sileno@geosdi.org
+ */
+public class OpenCatalogToolbarPlugin extends TreeToolbarPluginButton {
+
+    private ToolbarLayerTreeAction action;
+
+    @Override
+    public String getId() {
+        return "OPEN_CATALOG";
+    }
+
+    @Override
+    public TreeToolbarRegion getRegion() {
+        return TreeToolbarRegion.END_REGION;
+    }
+
+    @Override
+    protected ToolbarLayerTreeAction getTreeAction(TreePanel treePanel) {
+        if (action == null) {
+            action = new WmsFromMetadataTreeAction(treePanel);
+        }
+        return action;
+    }
+
+    @Override
+    protected boolean isInitialEnabled() {
+        return Boolean.TRUE;
+    }
+
+    @Override
+    public boolean setEnabledByStatus(TreeStatusEnum status) {
+        return Boolean.TRUE;
+    }
+}

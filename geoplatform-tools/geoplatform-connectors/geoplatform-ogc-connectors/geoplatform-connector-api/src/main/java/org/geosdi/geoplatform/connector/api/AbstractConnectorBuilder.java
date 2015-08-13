8930335b7dbebd821f8cdd003a38d09e781d7acb
Giuseppe La Scaleia <glascaleia@gmail.com>
2015-08-13 23:13:46 +0200

    Great improvements for GPJAXB Pooled.   TODO : implement this for all Connectors

--- geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-connector-api/src/main/java/org/geosdi/geoplatform/connector/api/AbstractConnectorBuilder.java
+++ geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-connector-api/src/main/java/org/geosdi/geoplatform/connector/api/AbstractConnectorBuilder.java
@@ -1,41 +1,40 @@
 /**
  *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
+ * geo-platform Rich webgis framework http://geo-platform.org
+ * ====================================================================
  *
- *   Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
  *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version. This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/
  *
- *   ====================================================================
+ * ====================================================================
  *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the
+ * GNU General Public License cover the whole combination.
  *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms
+ * and conditions of the license of that module. An independent module is a
+ * module which is not derived from or based on this library. If you modify this
+ * library, you may extend this exception to your version of the library, but
+ * you are not obligated to do so. If you do not wish to do so, delete this
+ * exception statement from your version.
  */
 package org.geosdi.geoplatform.connector.api;
 
 import java.net.URL;
+import org.geosdi.geoplatform.connector.server.GPServerConnector.GPPooledConnectorConfig;
 import org.geosdi.geoplatform.connector.server.security.GPSecurityConnector;
 import org.geosdi.geoplatform.support.httpclient.proxy.HttpClientProxyConfiguration;
 
@@ -43,11 +42,15 @@ import org.geosdi.geoplatform.support.httpclient.proxy.HttpClientProxyConfigurat
  *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
  * @email giuseppe.lascaleia@geosdi.org
+ *
+ * @param <B>
+ * @param <C>
  */
 public abstract class AbstractConnectorBuilder<B extends AbstractConnectorBuilder, C extends GPConnectorStore>
         implements GPConnectorBuilder<B> {
 
     protected URL serverUrl;
+    protected GPPooledConnectorConfig pooledConnectorConfig;
     protected GPSecurityConnector securityConnector;
     protected HttpClientProxyConfiguration proxyConfiguration;
     protected String version;
@@ -59,6 +62,13 @@ public abstract class AbstractConnectorBuilder<B extends AbstractConnectorBuilde
     public B withClientSecurity(GPSecurityConnector theSecurityConnector) {
         this.securityConnector = theSecurityConnector;
         return (B) this;
+    }
+
+    @Override
+    public B withPooledConnectorConfig(
+            GPPooledConnectorConfig thePooledConnectorConfig) {
+       this.pooledConnectorConfig = thePooledConnectorConfig;
+       return (B) this;
     }
 
     @Override
@@ -81,4 +91,5 @@ public abstract class AbstractConnectorBuilder<B extends AbstractConnectorBuilde
     }
 
     public abstract C build() throws Exception;
+
 }

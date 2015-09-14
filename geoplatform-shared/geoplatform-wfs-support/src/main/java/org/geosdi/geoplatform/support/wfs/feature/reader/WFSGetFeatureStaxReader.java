b0b314be5a3f6ec89ab1302f7bb57a1848dff48e
Giuseppe La Scaleia <glascaleia@gmail.com>
2015-09-14 15:31:21 +0200

    Added the possibility to Filter WFSGetRequest with attributeNames

--- geoplatform-shared/geoplatform-wfs-support/src/main/java/org/geosdi/geoplatform/support/wfs/feature/reader/WFSGetFeatureStaxReader.java
+++ geoplatform-shared/geoplatform-wfs-support/src/main/java/org/geosdi/geoplatform/support/wfs/feature/reader/WFSGetFeatureStaxReader.java
@@ -1,48 +1,40 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p/>
+ * Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p/>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p/>
+ * ====================================================================
+ * <p/>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p/>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.support.wfs.feature.reader;
 
 import com.vividsolutions.jts.io.WKTWriter;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import javax.xml.bind.DatatypeConverter;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.XMLEvent;
 import org.geosdi.geoplatform.connector.wfs.response.FeatureCollectionDTO;
 import org.geosdi.geoplatform.connector.wfs.response.FeatureDTO;
 import org.geosdi.geoplatform.connector.wfs.response.GeometryAttributeDTO;
@@ -58,8 +50,16 @@ import org.geosdi.geoplatform.xml.gml.v311.AbstractGeometryType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.xml.bind.DatatypeConverter;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- *
+ * @author Giuseppe La Scaleia - <giuseppe.lascaleia@geosdi.org>
  * @author Vincenzo Monteverde <vincenzo.monteverde@geosdi.org>
  */
 public class WFSGetFeatureStaxReader extends AbstractStaxStreamReader<FeatureCollectionDTO> {
@@ -67,9 +67,9 @@ public class WFSGetFeatureStaxReader extends AbstractStaxStreamReader<FeatureCol
     static {
         jaxbContextBuilder = GPJAXBContextBuilder.newInstance();
     }
+
     //
-    private static final Logger logger = LoggerFactory.getLogger(
-            WFSGetFeatureStaxReader.class);
+    private static final Logger logger = LoggerFactory.getLogger(WFSGetFeatureStaxReader.class);
     //
     private static final GPJAXBContextBuilder jaxbContextBuilder;
     //
@@ -107,30 +107,25 @@ public class WFSGetFeatureStaxReader extends AbstractStaxStreamReader<FeatureCol
             int evenType = reader.getEventType();
             if (evenType == XMLEvent.START_ELEMENT) {
                 if (super.isTagName("wfs", "FeatureCollection")) {
-
                     this.readInfo(fc);
-
                 } else if (super.isTagName(prefix, name)) {
-
-                    // TODO FID op is always previous of Gemetry op
                     feature = this.readFID();
                     fc.addFeature(feature);
-
+                    if (!super.isTagName(prefix, geometryName)) {
+                        FeatureAttributesMap attributes = this.readAttributes(name, attributeNames);
+                        feature.setAttributes(attributes);
+                    }
                 } else if (super.isTagName(prefix, geometryName)) {
-
-                    // TODO FID op is always previous of Gemetry op
                     assert (feature != null); // TODO DEL
                     String geometryWKT = this.readGeometry();
                     feature.setGeometry(geometryWKT);
 
                     super.goToEndTag(geometryName);
 
-                    FeatureAttributesMap attributes = this.readAttributes(name,
-                            attributeNames);
+                    FeatureAttributesMap attributes = this.readAttributes(name, attributeNames);
                     feature.setAttributes(attributes);
                 }
             }
-
             reader.next();
         }
 
@@ -138,22 +133,19 @@ public class WFSGetFeatureStaxReader extends AbstractStaxStreamReader<FeatureCol
     }
 
     private FeatureCollectionDTO readInfo(FeatureCollectionDTO fc) {
-        String numberOfFeatures = reader.getAttributeValue(null,
-                "numberOfFeatures");
+        String numberOfFeatures = reader.getAttributeValue(null, "numberOfFeatures");
         fc.setNumberOfFeatures(Integer.parseInt(numberOfFeatures));
 
         String timeStamp = reader.getAttributeValue(null, "timeStamp");
         fc.setTimeStamp(DatatypeConverter.parseDate(timeStamp).getTime());
 
-        logger.debug("\n@@@ Read info @@@\nfeature: {}\ntime: {}",
-                numberOfFeatures, timeStamp);
+        logger.debug("\n@@@ Read info @@@\nfeature: {}\ntime: {}", numberOfFeatures, timeStamp);
 
         return fc;
     }
 
     private FeatureDTO readFID() {
-        String featureID = reader.getAttributeValue("http://www.opengis.net/gml",
-                "id");
+        String featureID = reader.getAttributeValue("http://www.opengis.net/gml", "id");
         logger.debug("\n\n@@@ FEATURE_ID: {} @@@", featureID);
 
         FeatureDTO feature = new FeatureDTO(featureID);
@@ -166,9 +158,7 @@ public class WFSGetFeatureStaxReader extends AbstractStaxStreamReader<FeatureCol
         int eventType = reader.nextTag();
         if (eventType == XMLEvent.START_ELEMENT) {
 
-            AbstractGeometry geometry =
-                    jaxbContextBuilder.unmarshal(reader,
-                    AbstractGeometryType.class);
+            AbstractGeometry geometry = jaxbContextBuilder.unmarshal(reader, AbstractGeometryType.class);
 
             WKTWriter wktWriter = new WKTWriter(2);
 
@@ -176,9 +166,7 @@ public class WFSGetFeatureStaxReader extends AbstractStaxStreamReader<FeatureCol
 
             wktWriter.setFormatted(true);
             try {
-                geometryWKT = wktWriter.writeFormatted(
-                        this.sextanteParser.parseGeometry(
-                        geometry));
+                geometryWKT = wktWriter.writeFormatted(this.sextanteParser.parseGeometry(geometry));
 
                 logger.trace("WKT GEOMETRY @@@@@@@@@@@@@@@@ " + geometryWKT);
             } catch (ParserException ex) {
@@ -189,19 +177,13 @@ public class WFSGetFeatureStaxReader extends AbstractStaxStreamReader<FeatureCol
         return geometryWKT;
     }
 
-    private FeatureAttributesMap readAttributes(String featureName,
-            List<String> attributeNames)
+    private FeatureAttributesMap readAttributes(String featureName, List<String> attributeNames)
             throws XMLStreamException {
         if (attributeNames == null) {
             return null;
         }
 
-        Map<String, String> map = new HashMap<String, String>(
-                attributeNames.size());
-        for (String name : attributeNames) {
-            map.put(name, null);
-        }
-
+        Map<String, String> map = new HashMap<String, String>(attributeNames.size());
         FeatureAttributesMap fMap = new FeatureAttributesMap();
         fMap.setAttributesMap(map);
 
@@ -221,8 +203,7 @@ public class WFSGetFeatureStaxReader extends AbstractStaxStreamReader<FeatureCol
                 if (eventType == XMLEvent.CHARACTERS) {
                     String attributeValue = reader.getText();
                     map.put(localName, attributeValue);
-                    logger.debug("\n@@@ Attribute '{}': {} @@@", localName,
-                            attributeValue);
+                    logger.debug("\n@@@ Attribute '{}': {} @@@", localName, attributeValue);
 
                     super.goToEndTag(localName);
                 }

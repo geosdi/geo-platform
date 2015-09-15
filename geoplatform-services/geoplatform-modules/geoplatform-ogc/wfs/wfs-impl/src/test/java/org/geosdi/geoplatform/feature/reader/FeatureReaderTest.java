2987304061e9af8f67fea6dd858cef567214ad53
Giuseppe La Scaleia <glascaleia@gmail.com>
2015-09-15 13:23:58 +0200

    FIXED ISSUE#94

--- geoplatform-services/geoplatform-modules/geoplatform-ogc/wfs/wfs-impl/src/test/java/org/geosdi/geoplatform/feature/reader/FeatureReaderTest.java
+++ geoplatform-services/geoplatform-modules/geoplatform-ogc/wfs/wfs-impl/src/test/java/org/geosdi/geoplatform/feature/reader/FeatureReaderTest.java
@@ -1,47 +1,39 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p/>
+ * Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p/>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p/>
+ * ====================================================================
+ * <p/>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p/>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.feature.reader;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 import org.geosdi.geoplatform.connector.wfs.response.AttributeDTO;
 import org.geosdi.geoplatform.connector.wfs.response.FeatureCollectionDTO;
 import org.geosdi.geoplatform.connector.wfs.response.FeatureDTO;
@@ -57,17 +49,23 @@ import org.junit.runners.Parameterized.Parameters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
 /**
- *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
- * @email giuseppe.lascaleia@geosdi.org
  * @author Vincenzo Monteverde <vincenzo.monteverde@geosdi.org>
+ * @email giuseppe.lascaleia@geosdi.org
  */
 @RunWith(Parameterized.class)
 public class FeatureReaderTest {
 
-    private final static Logger logger = LoggerFactory.getLogger(
-            FeatureReaderTest.class);
+    private final static Logger logger = LoggerFactory.getLogger(FeatureReaderTest.class);
     private static String pathFile;
     private final FeatureSchemaReader featureReaderXSD = new GPFeatureSchemaReader();
     //
@@ -76,19 +74,14 @@ public class FeatureReaderTest {
 
     static {
         try {
-            pathFile = new File(".").getCanonicalPath() + File.separator
-                    + "src/test/resources/";
+            pathFile = new File(".").getCanonicalPath() + File.separator + "src/test/resources/";
         } catch (IOException ex) {
             logger.error("\n### Error: {}", ex.getMessage());
         }
     }
 
-    public FeatureReaderTest(String file,
-            int numAttributes,
-            int numFeatures) {
-        logger.trace(
-                "\n@@@ Test '{}' file (#attributes = {} - #features = {}) @@@",
-                file, numAttributes, numFeatures);
+    public FeatureReaderTest(String file, int numAttributes, int numFeatures) {
+        logger.trace("\n@@@ Test '{}' file (#attributes = {} - #features = {}) @@@", file, numAttributes, numFeatures);
 
         this.fileGF = pathFile + file + "-GetFeature.xml";
 
@@ -101,17 +94,13 @@ public class FeatureReaderTest {
 
         this.numAttributes = numAttributes;
         this.numFeatures = numFeatures;
+        logger.info("#######################ECCOLO : {}\n", this.fileGF);
     }
 
     @Parameters
     public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-            {"restricted", 1, 4},
-            {"restricted-null", 1, 4},
-            {"states", 22, 49},
-            {"giant_polygon", 0, 1},
-            {"tiger_roads", 2, 1000}
-        });
+        return Arrays.asList(
+                new Object[][]{{"restricted", 1, 4}, {"restricted-null", 1, 4}, {"states", 22, 49}, {"giant_polygon", 0, 1}, {"tiger_roads", 2, 1000}});
     }
 
     @Test
@@ -127,8 +116,7 @@ public class FeatureReaderTest {
 
             LayerSchemaDTO layerSchema = schemas.get(0);
             Assert.assertNotNull(layerSchema.getTypeName());
-            String name = layerSchema.getTypeName().substring(
-                    layerSchema.getTypeName().indexOf(":") + 1);
+            String name = layerSchema.getTypeName().substring(layerSchema.getTypeName().indexOf(":") + 1);
             Assert.assertNotNull(layerSchema.getTargetNamespace());
             Assert.assertNotNull(layerSchema.getGeometry());
             List<AttributeDTO> attributes = layerSchema.getAttributes();
@@ -142,8 +130,7 @@ public class FeatureReaderTest {
             }
             logger.debug("\n\n@@@@@@@@@@@@@@@@@@@@ {}", layerSchema);
 
-            WFSGetFeatureStaxReader featureReader = new WFSGetFeatureStaxReader(
-                    layerSchema);
+            WFSGetFeatureStaxReader featureReader = new WFSGetFeatureStaxReader(layerSchema);
             FeatureCollectionDTO fc = featureReader.read(new File(fileGF));
             Assert.assertNotNull(fc);
             Assert.assertNotNull(fc.getTimeStamp());
@@ -161,12 +148,11 @@ public class FeatureReaderTest {
                     Assert.assertNotNull(feature.getAttributes());
                     Map<String, String> fMap = feature.getAttributes().getAttributesMap();
                     Assert.assertNotNull(fMap);
+                    logger.debug("#################FMAP_SIZE : {}\n", fMap.size());
                     Assert.assertEquals(numAttributes, fMap.size());
                 }
-                logger.debug("\n\n@@@@@@@@@@@@@@@@@@@@ {} - {}",
-                        feature.getFID(), feature.getAttributes());
-                logger.debug("\nGEOMETRY @@@@@@@@@@@@@@@@ : {}",
-                        feature.getGeometry());
+                logger.debug("\n\n@@@@@@@@@@@@@@@@@@@@ {} - {}", feature.getFID(), feature.getAttributes());
+                logger.debug("\nGEOMETRY @@@@@@@@@@@@@@@@ : {}", feature.getGeometry());
             }
 
         } finally {

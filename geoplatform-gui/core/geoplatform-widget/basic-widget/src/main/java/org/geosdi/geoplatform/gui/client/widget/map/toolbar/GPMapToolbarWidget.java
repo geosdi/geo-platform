ce2461584faf2e7c52df3f41de171cb51f89da9e
Giuseppe La Scaleia <glascaleia@gmail.com>
2015-11-10 09:42:27 +0100

    Some Improvements to manage WidgetGenericTool , to add Some Text for Tool

--- geoplatform-gui/core/geoplatform-widget/basic-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/map/toolbar/GPMapToolbarWidget.java
+++ geoplatform-gui/core/geoplatform-widget/basic-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/map/toolbar/GPMapToolbarWidget.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p/>
+ * Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p/>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p/>
+ * ====================================================================
+ * <p/>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p/>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.widget.map.toolbar;
 
@@ -43,8 +42,10 @@ import com.extjs.gxt.ui.client.widget.toolbar.FillToolItem;
 import com.extjs.gxt.ui.client.widget.toolbar.SeparatorToolItem;
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import com.google.gwt.user.client.ui.Image;
+
 import java.util.Collections;
 import java.util.List;
+
 import org.geosdi.geoplatform.gui.action.IOpenEditorMapAction;
 import org.geosdi.geoplatform.gui.action.ToolbarAction;
 import org.geosdi.geoplatform.gui.action.ToolbarActionRegistar;
@@ -83,27 +84,27 @@ import org.geosdi.geoplatform.gui.impl.map.GeoPlatformMap;
  * method in GPMapToolbarWidget for the final build.
  *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
- * @email giuseppe.lascaleia@geosdi.org
  * @author Vincenzo Monteverde <vincenzo.monteverde@geosdi.org>
+ * @email giuseppe.lascaleia@geosdi.org
  */
 public class GPMapToolbarWidget extends GeoPlatformToolbarWidget
         implements IGeoPlatformToolbar {
-    
+
     protected GeoPlatformMap geoPlatformMap;
     private List<WidgetGenericTool<IGeoPlatformToolbar>> tools;
-    
+
     public GPMapToolbarWidget(GeoPlatformMap geoPlatformMap,
             List<WidgetGenericTool<IGeoPlatformToolbar>> tools) {
         this.geoPlatformMap = geoPlatformMap;
         this.tools = tools;
         postConstruct();
     }
-    
+
     protected final void postConstruct() {
         Collections.sort(tools);
         this.initialize();
     }
-    
+
     @Override
     public void initialize() {
         for (WidgetGenericTool<IGeoPlatformToolbar> tool : tools) {
@@ -129,11 +130,11 @@ public class GPMapToolbarWidget extends GeoPlatformToolbarWidget
     public void addApplicationButton(ToolbarActionTool tool) {
         ToolbarApplicationAction action = (ToolbarApplicationAction) this.getAction(
                 tool.getId());
-        
+
         final Button button = new Button();
         button.setText(action.getButtonName());
         this.prepareButton(button, action, tool);
-        
+
         this.toolBar.add(button);
     }
 
@@ -145,11 +146,11 @@ public class GPMapToolbarWidget extends GeoPlatformToolbarWidget
     @Override
     public void addMapButton(ToolbarActionTool tool) {
         ToolbarMapAction action = (ToolbarMapAction) this.getAction(tool.getId());
-        
+
         final GeoPlatformButton button = new GeoPlatformButton();
         button.setAction(action);
         this.prepareButton(button, action, tool);
-        
+
         this.toolBar.add(button);
     }
 
@@ -161,11 +162,11 @@ public class GPMapToolbarWidget extends GeoPlatformToolbarWidget
     @Override
     public void addMapToggleButton(ToolbarActionTool tool) {
         ToolbarMapAction action = (ToolbarMapAction) this.getAction(tool.getId());
-        
+
         final GeoPlatformToggleButton button = new GeoPlatformToggleButton();
         button.setAction(action);
         this.prepareButton(button, action, tool);
-        
+
         this.toolBar.add(button);
     }
 
@@ -178,11 +179,11 @@ public class GPMapToolbarWidget extends GeoPlatformToolbarWidget
     public void addIconInToolbar(IconInToolbar tool) {
         GPIconWidgetComponent gpWidgetIcon = new GPIconWidgetComponent(
                 this.toolBar);
-        
+
         Image image = new Image(BasicWidgetResources.ICONS.googleWhite());
         WidgetComponent widgetComponent = gpWidgetIcon.createWidgetComponent(
                 image, tool.getText());
-        
+
         this.toolBar.add(widgetComponent);
     }
 
@@ -195,14 +196,14 @@ public class GPMapToolbarWidget extends GeoPlatformToolbarWidget
     public void addMenuInToolBar(MenuInToolBar tool) {
         Button buttonItem = new Button(
                 GPAccountLogged.getInstance().getName() + " @ "
-                + GPAccountLogged.getInstance().getOrganization());
+                        + GPAccountLogged.getInstance().getOrganization());
         buttonItem.setIcon(AbstractImagePrototype.create(BasicWidgetResources.ICONS.logged_user()));
         buttonItem.setId(tool.getId());
-        
+
         Menu menu = new Menu();
         GPMenuBarBuilder.getIstance().buildTools(menu, tool.getTools());
         buttonItem.setMenu(menu);
-        
+
         this.toolBar.add(buttonItem);
     }
 
@@ -214,15 +215,15 @@ public class GPMapToolbarWidget extends GeoPlatformToolbarWidget
     @Override
     public void addMenuButton(MenuClientTool tool) {
         ToolbarAction action = this.getAction(tool.getId());
-        
+
         Button button = new Button();
         button.setId(tool.getId());
 //        button.setText(action.getButtonName());
         button.setIcon(action.getImage());
         button.setEnabled(tool.isEnabled());
-        
+
         button.setMenu(this.createMenu(tool.getActionTools()));
-        
+
         toolBar.add(button);
     }
 
@@ -236,19 +237,18 @@ public class GPMapToolbarWidget extends GeoPlatformToolbarWidget
     }
 
     /**
-     *
      * @param ToolbarActionEditorTool tool
      */
     @Override
     public void addMapToogleButton(ToolbarActionEditorTool tool) {
         ToolbarMapAction action = (ToolbarMapAction) this.getAction(tool.getId());
-        
+
         final GeoPlatformToggleButton button = new GeoPlatformToggleButton();
         button.setAction(action);
         this.prepareButton(button, action, tool);
-        
+
         ((IOpenEditorMapAction) action).setActionTools(tool.getTools());
-        
+
         this.toolBar.add(button);
     }
 
@@ -264,7 +264,7 @@ public class GPMapToolbarWidget extends GeoPlatformToolbarWidget
         for (ToolbarActionTool actionTool : actionTools) {
             MenuBaseAction action = (MenuBaseAction) menuRegistar.get(
                     actionTool.getId());
-            
+
             MenuItem item = new MenuItem(action.getTitle());
             item.addSelectionListener(action);
             item.setIcon(action.getImage());
@@ -294,24 +294,29 @@ public class GPMapToolbarWidget extends GeoPlatformToolbarWidget
      *
      * @param button the Button to prepare
      * @param action the enable Action
-     * @param tool the Item of reference
+     * @param tool   the Item of reference
      */
     private void prepareButton(final Button button, ToolbarAction action,
             WidgetGenericTool<IGeoPlatformToolbar> tool) {
         button.setId(action.getId());
         button.setToolTip(action.getTooltip());
-        button.setIcon(action.getImage());
+        if (action.getImage() != null)
+            button.setIcon(action.getImage());
+
+        if ((tool.getText() != null) && !(tool.getText().isEmpty()))
+            button.setText(tool.getText());
+
         button.addSelectionListener(action);
-        
+
         action.addActionEnableHandler(new ActionEnableHandler() {
-            
+
             @Override
             public void onActionEnabled(ActionEnableEvent event) {
                 button.setEnabled(event.isEnabled());
             }
-            
+
         });
-        
+
         action.setEnabled(tool.isEnabled());
     }
 
@@ -321,5 +326,5 @@ public class GPMapToolbarWidget extends GeoPlatformToolbarWidget
     public List<WidgetGenericTool<IGeoPlatformToolbar>> getTools() {
         return Collections.unmodifiableList(tools);
     }
-    
+
 }

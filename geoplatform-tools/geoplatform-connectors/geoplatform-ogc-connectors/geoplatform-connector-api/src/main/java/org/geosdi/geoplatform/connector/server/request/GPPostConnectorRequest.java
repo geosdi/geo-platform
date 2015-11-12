f9e8bf24e622b136b778941f0ec8b0ca6d8dc967
Giuseppe La Scaleia <glascaleia@gmail.com>
2015-11-12 10:07:55 +0100

    FIXED Jenkins build failure. Ispra catalog has a  lot of problems

--- geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-connector-api/src/main/java/org/geosdi/geoplatform/connector/server/request/GPPostConnectorRequest.java
+++ geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-connector-api/src/main/java/org/geosdi/geoplatform/connector/server/request/GPPostConnectorRequest.java
@@ -1,10 +1,9 @@
 /**
- *
  * geo-platform Rich webgis framework http://geo-platform.org
  * ====================================================================
- *
+ * <p/>
  * Copyright (C) 2008-2015 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
+ * <p/>
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
@@ -13,13 +12,13 @@
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details. You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/
- *
+ * <p/>
  * ====================================================================
- *
+ * <p/>
  * Linking this library statically or dynamically with other modules is making a
  * combined work based on this library. Thus, the terms and conditions of the
  * GNU General Public License cover the whole combination.
- *
+ * <p/>
  * As a special exception, the copyright holders of this library give you
  * permission to link this library with independent modules to produce an
  * executable, regardless of the license terms of these independent modules, and
@@ -35,22 +34,22 @@ package org.geosdi.geoplatform.connector.server.request;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.CharStreams;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.Unmarshaller;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.util.EntityUtils;
 import org.geosdi.geoplatform.connector.server.GPServerConnector;
 
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
- *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
- * @email giuseppe.lascaleia@geosdi.org
- *
  * @author Vincenzo Monteverde <vincenzo.monteverde@geosdi.org>
+ * @email giuseppe.lascaleia@geosdi.org
  */
 public abstract class GPPostConnectorRequest<T>
         extends GPAbstractConnectorRequest<T> {
@@ -92,9 +91,14 @@ public abstract class GPPostConnectorRequest<T>
             InputStream is = responseEntity.getContent();
 
             Unmarshaller unmarshaller = getUnmarshaller();
-            Object content = unmarshaller.unmarshal(is);
-            if (!(content instanceof JAXBElement)) { // ExceptionReport
-                logger.error("\n#############\n{}\n#############", content);
+            Object content = null;
+            try {
+                content = unmarshaller.unmarshal(is);
+            } catch (JAXBException ex) {
+                logger.error("######################JAXBException : ", ex);
+            }
+            if ((content == null) || !(content instanceof JAXBElement)) { // ExceptionReport
+                logger.error("#############CONTENT : {} #############\n", content);
                 throw new IllegalStateException("Connector Server Error: "
                         + "incorrect responce");
             }

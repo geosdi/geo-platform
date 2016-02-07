c523214a11ad76e5b3ef192fb3d9a8d90b029757
Giuseppe La Scaleia <glascaleia@gmail.com>
2016-02-07 08:42:29 +0100

    Added Unit to commit

---
+++ geoplatform-support/geoplatform-google-maps-services-support/src/main/java/org/geosdi/geoplatform/support/google/spring/services/distance/Unit.java
@@ -0,0 +1,20 @@
+package org.geosdi.geoplatform.support.google.spring.services.distance;
+
+/**
+ * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
+ * @email giuseppe.lascaleia@geosdi.org
+ */
+public enum Unit {
+    /**
+     * is statute miles (default)
+     */
+    M,
+    /**
+     * is kilometers
+     */
+    K,
+    /**
+     * is nautical miles
+     */
+    N;
+}

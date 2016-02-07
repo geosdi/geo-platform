e0f295d97d8e2f60a38f9a121cbff54481024707
Giuseppe La Scaleia <glascaleia@gmail.com>
2016-02-07 08:41:15 +0100

    Added Method to calculate Distance Between two Points

--- geoplatform-support/geoplatform-google-maps-services-support/src/main/java/org/geosdi/geoplatform/support/google/spring/services/distance/GPDistanceMatrixServiceConfig.java
+++ geoplatform-support/geoplatform-google-maps-services-support/src/main/java/org/geosdi/geoplatform/support/google/spring/services/distance/GPDistanceMatrixServiceConfig.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2016 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p/>
+ * Copyright (C) 2008-2016 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p/>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p/>
+ * ====================================================================
+ * <p/>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p/>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.support.google.spring.services.distance;
 
@@ -84,6 +83,45 @@ class GPDistanceMatrixServiceConfig {
         @Override
         public DistanceMatrixApiRequest getDistanceMatrix(String[] origins, String[] destinations) {
             return DistanceMatrixApi.getDistanceMatrix(this.geoApiContext, origins, destinations);
+        }
+
+        /**
+         * <p>Calulate Distance between to Points</p>
+         *
+         * @param lat1
+         * @param lon1
+         * @param lat2
+         * @param lon2
+         * @param unit
+         * @return {@link Double}
+         */
+        @Override
+        public Double distance(double lat1, double lon1, double lat2, double lon2, Unit unit) {
+            double theta = lon1 - lon2;
+            double distance = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2))
+                    + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+            distance = Math.acos(distance);
+            distance = rad2deg(distance);
+            distance = distance * 60 * 1.1515;
+            switch (unit) {
+                case K:
+                    distance = distance * 1.609344;
+                    break;
+                case N:
+                    distance = distance * 0.8684;
+                    break;
+            }
+            return distance;
+        }
+
+        // This function converts decimal degrees to radians
+        private static double deg2rad(double deg) {
+            return (deg * Math.PI / 180.0);
+        }
+
+        // This function converts radians to decimal degrees
+        private static double rad2deg(double rad) {
+            return (rad * 180 / Math.PI);
         }
 
         @Override

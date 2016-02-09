fbe3c1f2d3ffbd46b96ab65da6f9e9c9597dc425
Giuseppe La Scaleia <glascaleia@gmail.com>
2016-02-09 10:48:43 +0100

    Some Improvements using java8 Features

--- geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-connector/src/main/java/org/geosdi/geoplatform/experimental/el/dao/AbstractElasticSearchDAO.java
+++ geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-connector/src/main/java/org/geosdi/geoplatform/experimental/el/dao/AbstractElasticSearchDAO.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2016 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p/>
+ * Copyright (C) 2008-2016 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p/>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p/>
+ * ====================================================================
+ * <p/>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p/>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.experimental.el.dao;
 
@@ -78,7 +77,6 @@ public abstract class AbstractElasticSearchDAO<D extends Document> implements GP
     public D persist(D document) throws Exception {
         logger.debug("#################Try to insert {}\n\n", document);
         IndexResponse response;
-
         if (document.isIdSetted()) {
             response = this.elastichSearchClient.prepareIndex(getIndexName(), getIndexType(), document.getId())
                     .setSource(this.mapper.writeAsString(document)).get();
@@ -108,7 +106,6 @@ public abstract class AbstractElasticSearchDAO<D extends Document> implements GP
     @Override
     public BulkResponse persist(Iterable<D> documents) throws Exception {
         Preconditions.checkArgument(((documents != null)), "The Documents " + "to save, must not be null.");
-
         BulkRequestBuilder bulkRequest = this.elastichSearchClient.prepareBulk();
         for (D document : documents) {
             if (document.isIdSetted()) {
@@ -119,7 +116,6 @@ public abstract class AbstractElasticSearchDAO<D extends Document> implements GP
                         .setSource(this.mapper.writeAsString(document)));
             }
         }
-
         BulkResponse bulkResponse = bulkRequest.get();
         if (bulkResponse.hasFailures()) {
             throw new IllegalStateException(bulkResponse.buildFailureMessage());
@@ -142,7 +138,6 @@ public abstract class AbstractElasticSearchDAO<D extends Document> implements GP
 
         Long total = searchResponse.getHits().getTotalHits();
         logger.debug("###################TOTAL HITS FOUND : {} .\n\n", total);
-
         return new PageResult<D>(total, Stream.of(searchResponse.getHits().hits())
                 .map(new GPSearchHitFunction())
                 .filter(s -> {
@@ -167,9 +162,7 @@ public abstract class AbstractElasticSearchDAO<D extends Document> implements GP
     public D find(String id) throws Exception {
         Preconditions.checkArgument((id != null) && !(id.isEmpty()),
                 "The ElasticSearch ID must not be null or an Empty String");
-
         GetResponse existResponse = elastichSearchClient.prepareGet(getIndexName(), getIndexType(), id).get();
-
         return (existResponse.isExists()) ? this.mapper.read(existResponse.getSourceAsString()) : null;
     }
 
@@ -204,10 +197,10 @@ public abstract class AbstractElasticSearchDAO<D extends Document> implements GP
                 .setScroll(new TimeValue(60000))
                 .setSize(100).execute().actionGet();
         while (true) {
-            for (SearchHit searchHit : searchResponse.getHits().hits()) {
-                D document = this.mapper.read(searchHit.getSourceAsString());
-                this.elastichSearchClient.delete(new DeleteRequest(getIndexName(), getIndexType(), document.getId())).actionGet();
-            }
+            Stream.of(searchResponse.getHits().hits()).forEach(document -> {
+                this.elastichSearchClient.delete(new DeleteRequest(getIndexName(), getIndexType(),
+                        document.getId())).actionGet();
+            });
             searchResponse = this.elastichSearchClient.prepareSearchScroll(searchResponse.getScrollId()).setScroll(new TimeValue(600000)).execute().actionGet();
             if (searchResponse.getHits().getHits().length == 0) {
                 break;

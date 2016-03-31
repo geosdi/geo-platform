d2ff5e2e32ac1bd533be14173be1cc2bda7713c5
Giuseppe La Scaleia <glascaleia@gmail.com>
2016-03-31 10:49:40 +0200

    Added Spatial Criteria for Search TODO : Try to control if QueryBuilder is null in the chain for MultiQuerySearch Parameters

---
+++ geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-connector/src/main/java/org/geosdi/geoplatform/experimental/el/search/geoshape/IGPGeoShapeQuerySearch.java
@@ -0,0 +1,91 @@
+package org.geosdi.geoplatform.experimental.el.search.geoshape;
+
+import com.google.common.base.Preconditions;
+import jdk.nashorn.internal.ir.annotations.Immutable;
+import org.elasticsearch.common.geo.builders.ShapeBuilder;
+import org.elasticsearch.index.query.GeoShapeQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.geosdi.geoplatform.experimental.el.search.bool.IBooleanSearch;
+
+/**
+ * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
+ * @email giuseppe.lascaleia@geosdi.org
+ */
+public interface IGPGeoShapeQuerySearch extends IBooleanSearch {
+
+    /**
+     * @return {@link GPShapeRelation}
+     */
+    GPShapeRelation getShapeRelation();
+
+    /**
+     * @return {@link ShapeBuilder}
+     */
+    ShapeBuilder getShapeBuilder();
+
+    /**
+     * @return {@link GeoShapeQueryBuilder}
+     */
+    default GeoShapeQueryBuilder buildGeoQuery() {
+        switch (getShapeRelation()) {
+            case INTERSECTS:
+                return QueryBuilders.geoIntersectionQuery(getField(), getShapeBuilder());
+            case DISJOINT:
+                return QueryBuilders.geoDisjointQuery(getField(), getShapeBuilder());
+            case WITHIN:
+                return QueryBuilders.geoDisjointQuery(getField(), getShapeBuilder());
+        }
+        return null;
+    }
+
+    /**
+     *
+     */
+    enum GPShapeRelation {
+        INTERSECTS, DISJOINT, WITHIN;
+    }
+
+    /**
+     *
+     */
+    @Immutable
+    class GPGeoShapeQuerySearch extends IBooleanSearch.AbstractBooleanSearch implements IGPGeoShapeQuerySearch {
+
+        private final GPShapeRelation shapeRelation;
+        private final ShapeBuilder shapeBuilder;
+
+        public GPGeoShapeQuerySearch(String theField, BooleanQueryType theType, GPShapeRelation theShapeRelation,
+                ShapeBuilder theShapeBuilder) {
+            super(theField, theType);
+            Preconditions.checkArgument((theShapeRelation != null), "The Parameter ShapeRelations must not be null.");
+            Preconditions.checkArgument((theShapeBuilder != null), "The Parameter ShapeBuilder must not be null.");
+            this.shapeRelation = theShapeRelation;
+            this.shapeBuilder = theShapeBuilder;
+        }
+
+        /**
+         * @return {@link GPShapeRelation}
+         */
+        @Override
+        public GPShapeRelation getShapeRelation() {
+            return this.shapeRelation;
+        }
+
+        /**
+         * @return {@link ShapeBuilder}
+         */
+        @Override
+        public ShapeBuilder getShapeBuilder() {
+            return this.shapeBuilder;
+        }
+
+        /**
+         * @return {@link QueryBuilder}
+         */
+        @Override
+        public QueryBuilder buildQuery() {
+            return this.buildGeoQuery();
+        }
+    }
+}

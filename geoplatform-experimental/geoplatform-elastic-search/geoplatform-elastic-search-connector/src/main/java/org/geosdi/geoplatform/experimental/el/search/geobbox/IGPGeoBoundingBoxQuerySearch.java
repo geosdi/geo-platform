62a2623eb4005849e2a65b591739300c0af545da
Giuseppe La Scaleia <glascaleia@gmail.com>
2016-05-28 11:25:11 +0200

    Added GeoBoundingBoxSearchQuery

---
+++ geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-connector/src/main/java/org/geosdi/geoplatform/experimental/el/search/geobbox/IGPGeoBoundingBoxQuerySearch.java
@@ -0,0 +1,61 @@
+package org.geosdi.geoplatform.experimental.el.search.geobbox;
+
+import com.vividsolutions.jts.geom.Envelope;
+import net.jcip.annotations.Immutable;
+import org.elasticsearch.index.query.GeoBoundingBoxQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.geosdi.geoplatform.experimental.el.search.bool.IBooleanSearch;
+
+/**
+ * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
+ * @email giuseppe.lascaleia@geosdi.org
+ */
+public interface IGPGeoBoundingBoxQuerySearch extends IBooleanSearch {
+
+    /**
+     * @return {@link Envelope}
+     */
+    Envelope getEnvelope();
+
+    /**
+     * @return {@link GeoBoundingBoxQueryBuilder}
+     */
+    default GeoBoundingBoxQueryBuilder buildGeoBoundingBoxQueryBuilder() {
+        return QueryBuilders.geoBoundingBoxQuery("mountainNowTodayPost.surveys.location")
+                .topLeft(getEnvelope().getMaxY(), getEnvelope().getMinX())
+                .topRight(getEnvelope().getMaxY(), getEnvelope().getMaxX())
+                .bottomLeft(getEnvelope().getMinY(), getEnvelope().getMinX())
+                .bottomRight(getEnvelope().getMinY(), getEnvelope().getMaxX());
+    }
+
+    /**
+     *
+     */
+    @Immutable
+    class GPGeoBoundingBoxQuerySearch extends AbstractBooleanSearch implements IGPGeoBoundingBoxQuerySearch {
+
+        private final Envelope envelope;
+
+        public GPGeoBoundingBoxQuerySearch(String theField, BooleanQueryType theType, Envelope theEnvelope) {
+            super(theField, theType);
+            this.envelope = theEnvelope;
+        }
+
+        /**
+         * @return {@link Envelope}
+         */
+        @Override
+        public Envelope getEnvelope() {
+            return this.envelope;
+        }
+
+        /**
+         * @return {@link QueryBuilder}
+         */
+        @Override
+        public QueryBuilder buildQuery() {
+            return buildGeoBoundingBoxQueryBuilder();
+        }
+    }
+}

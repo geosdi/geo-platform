0eccc363c3a049b6230047d31412aef7995942e5
Giuseppe La Scaleia <glascaleia@gmail.com>
2017-01-27 08:55:53 +0100

    Improvements

--- geoplatform-gui/core/geoplatform-widget/basic-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/tree/store/executor/GenericTreeStoreExecutor.java
+++ geoplatform-gui/core/geoplatform-widget/basic-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/tree/store/executor/GenericTreeStoreExecutor.java
@@ -1,42 +1,40 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.widget.tree.store.executor;
 
 import com.extjs.gxt.ui.client.data.ModelData;
-import java.util.List;
 import org.geosdi.geoplatform.gui.client.widget.tree.GPTreePanel;
 import org.geosdi.geoplatform.gui.client.widget.tree.store.GPTreeStoreOperations;
 import org.geosdi.geoplatform.gui.model.GPLayerBean;
@@ -44,8 +42,9 @@ import org.geosdi.geoplatform.gui.model.memento.IMemento;
 import org.geosdi.geoplatform.gui.model.tree.GPBeanTreeModel;
 import org.geosdi.geoplatform.gui.model.tree.GPLayerTreeModel;
 
+import java.util.List;
+
 /**
- *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
  * @email giuseppe.lascaleia@geosdi.org
  */
@@ -58,18 +57,28 @@ public abstract class GenericTreeStoreExecutor<T extends IMemento> implements
         this.tree = thetree;
     }
 
+    /**
+     * @param layer
+     * @param parentDestination
+     * @return {@link Boolean}
+     */
     protected boolean checkDuplicatedLayer(GPLayerBean layer,
             GPBeanTreeModel parentDestination) {
         for (ModelData element : parentDestination.getChildren()) {
             if (element != null && element instanceof GPLayerTreeModel
                     && ((GPLayerTreeModel) element).getTitle().equals(
-                            layer.getTitle())) {
+                    layer.getTitle())) {
                 return true;
             }
         }
         return false;
     }
 
+    /**
+     * @param layer
+     * @param parentDestination
+     * @return {@link String}
+     */
     protected String generateUnduplicateAliasForLayer(GPLayerBean layer,
             GPBeanTreeModel parentDestination) {
         String originalName;
@@ -81,9 +90,18 @@ public abstract class GenericTreeStoreExecutor<T extends IMemento> implements
         return this.buildAlias(originalName, parentDestination);
     }
 
+    /**
+     * @param originalName
+     * @param parentDestination
+     * @return {@link String}
+     */
     protected abstract String buildAlias(String originalName,
             GPBeanTreeModel parentDestination);
 
+    /**
+     * @param layers
+     * @param sourceLayer
+     */
     protected abstract void addRasterLayers(List<? extends GPLayerBean> layers,
             GPTreeStoreOperations sourceLayer);
 

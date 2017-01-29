c84b4c1bde62ac31733a3e532e693d5967470943
Giuseppe La Scaleia <glascaleia@gmail.com>
2017-01-29 10:53:01 +0100

    Improvements for CSW Parsing correct UTF8 Chararcters

--- geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-connector-api/src/main/java/org/geosdi/geoplatform/connector/server/request/GPPostConnectorRequest.java
+++ geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-connector-api/src/main/java/org/geosdi/geoplatform/connector/server/request/GPPostConnectorRequest.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.connector.server.request;
 
@@ -40,17 +39,23 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.util.EntityUtils;
 import org.geosdi.geoplatform.connector.server.GPServerConnector;
+import org.xml.sax.InputSource;
 
-import javax.xml.bind.JAXBElement;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Unmarshaller;
+import javax.xml.bind.*;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
 /**
  * @author Giuseppe La Scaleia <giuseppe.lascaleia@geosdi.org>
  * @author Vincenzo Monteverde <vincenzo.monteverde@geosdi.org>
  */
 public abstract class GPPostConnectorRequest<T> extends PostConnectorRequest<T> {
+
+    private static final Charset UTF_8_CHARSERT = Charset.forName("UTF-8");
+    private static final String INCORRECT_RESPONSE_MESSAGE = "Connector Server Error: Incorrect Response.";
+    private static final String CONNECTION_PROBLEM_MESSAGE = "Connector Server Error: Connection Problem.";
+
     /**
      * @param server
      */
@@ -60,38 +65,32 @@ public abstract class GPPostConnectorRequest<T> extends PostConnectorRequest<T> 
 
     @Override
     public T getResponse() throws Exception {
-        T response = null;
-
+        T response;
         HttpPost httpPost = super.getPostMethod();
-        CloseableHttpResponse httpResponse = super.securityConnector
-                .secure(this, httpPost);
-
+        CloseableHttpResponse httpResponse = super.securityConnector.secure(this, httpPost);
         HttpEntity responseEntity = httpResponse.getEntity();
         if (responseEntity != null) {
             InputStream is = responseEntity.getContent();
+            InputSource inputSource = new InputSource(new InputStreamReader(is, UTF_8_CHARSERT));
+            inputSource.setEncoding(UTF_8_CHARSERT.name());
 
             Unmarshaller unmarshaller = getUnmarshaller();
             Object content = null;
             try {
-                content = unmarshaller.unmarshal(is);
+                content = unmarshaller.unmarshal(inputSource.getCharacterStream());
             } catch (JAXBException ex) {
                 logger.error("######################JAXBException : ", ex);
             }
             if ((content == null) || !(content instanceof JAXBElement)) { // ExceptionReport
                 logger.error("#############CONTENT : {} #############\n", content);
-                throw new IllegalStateException("Connector Server Error: "
-                        + "incorrect responce");
+                throw new IllegalStateException(INCORRECT_RESPONSE_MESSAGE);
             }
 
-            JAXBElement<T> elementType = (JAXBElement<T>) content;
-            response = elementType.getValue();
-
+            response = ((JAXBElement<T>) content).getValue();
             EntityUtils.consume(responseEntity);
         } else {
-            throw new IllegalStateException("Connector Server Error: "
-                    + "Connection problem");
+            throw new IllegalStateException(CONNECTION_PROBLEM_MESSAGE);
         }
-
         return response;
     }
 }

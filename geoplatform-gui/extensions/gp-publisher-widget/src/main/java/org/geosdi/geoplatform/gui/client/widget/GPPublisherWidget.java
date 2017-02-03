7c074f13997524be26b33b8cd4fcd91da4019154
Vito Salvia <vito.salvia@gmail.com>
2017-02-03 09:43:26 +0100

    Added progress bar on publisher

--- geoplatform-gui/extensions/gp-publisher-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/GPPublisherWidget.java
+++ geoplatform-gui/extensions/gp-publisher-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/GPPublisherWidget.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.widget;
 
@@ -42,8 +41,8 @@ import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.treepanel.TreePanel;
 import com.google.common.collect.Lists;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
-import java.util.List;
 import org.geosdi.geoplatform.gui.client.BasicWidgetResources;
 import org.geosdi.geoplatform.gui.client.command.publish.basic.PublishLayerPreviewRequest;
 import org.geosdi.geoplatform.gui.client.command.publish.basic.PublishLayerPreviewResponse;
@@ -53,8 +52,8 @@ import org.geosdi.geoplatform.gui.client.i18n.PublisherWidgetMessages;
 import org.geosdi.geoplatform.gui.client.i18n.buttons.ButtonsConstants;
 import org.geosdi.geoplatform.gui.client.i18n.windows.WindowsConstants;
 import org.geosdi.geoplatform.gui.client.model.PreviewLayer;
-import static org.geosdi.geoplatform.gui.client.widget.GeoPlatformWindow.logger;
 import org.geosdi.geoplatform.gui.client.widget.SearchStatus.EnumSearchStatus;
+import org.geosdi.geoplatform.gui.client.widget.progressbar.PublisherProgressBar;
 import org.geosdi.geoplatform.gui.client.widget.tree.store.puregwt.event.AddRasterFromPublisherEvent;
 import org.geosdi.geoplatform.gui.command.api.ClientCommandDispatcher;
 import org.geosdi.geoplatform.gui.command.api.GPClientCommand;
@@ -69,6 +68,8 @@ import org.geosdi.geoplatform.gui.puregwt.layers.LayerHandlerManager;
 import org.geosdi.geoplatform.gui.shared.util.GPSharedUtils;
 import org.geosdi.geoplatform.gui.utility.GPReloadURLException;
 import org.geosdi.geoplatform.gui.utility.GPSessionTimeout;
+
+import java.util.List;
 
 /**
  * @author Nazzareno Sileno - CNR IMAA geoSDI Group
@@ -125,56 +126,67 @@ public class GPPublisherWidget extends AbstractPublisherWidget {
                                             @Override
                                             public void onCommandSuccess(
                                                     PublishLayerPreviewResponse response) {
+                                                PublisherProgressBar.getInstance().show(
+                                                        PublisherWidgetConstants.INSTANCE.
+                                                                EPSGTablePanel_processingDataProgressBarText());
+                                                Timer t = new Timer() {
+                                                    public void run() {
                                                         LayerHandlerManager.fireEvent(
                                                                 new AddRasterFromPublisherEvent(
                                                                         layerList));
                                                         reset();
                                                         logger.info("Response: " + response);
                                                         LayoutManager.getInstance().getStatusMap().
-                                                        setStatus(
-                                                                PublisherWidgetConstants.INSTANCE.
-                                                                GPPublisherWidget_statusShapePublishedSuccesfullyText(),
-                                                                EnumSearchStatus.STATUS_SEARCH.
-                                                                toString());
-                                                    }
+                                                                setStatus(
+                                                                        PublisherWidgetConstants.INSTANCE.
+                                                                                GPPublisherWidget_statusShapePublishedSuccesfullyText(),
+                                                                        EnumSearchStatus.STATUS_SEARCH.
+                                                                                toString());
+                                                        PublisherProgressBar.getInstance().hide();
 
-                                                    @Override
-                                                    public void onCommandFailure(Throwable exception) {
-                                                        if (exception.getCause() instanceof GPSessionTimeout) {
-                                                            GPHandlerManager.fireEvent(new GPLoginEvent(
-                                                                            publishShapePreviewEvent));
-                                                        } else if (exception.getCause() instanceof GPReloadURLException) {
-                                                            GeoPlatformMessage.errorMessage(
-                                                                    PublisherWidgetConstants.INSTANCE.
+                                                    }
+                                                };
+                                                // Schedule the timer to run once in 5 seconds.
+                                                t.schedule(5000);
+                                            }
+
+                                            @Override
+                                            public void onCommandFailure(Throwable exception) {
+                                                if (exception.getCause() instanceof GPSessionTimeout) {
+                                                    GPHandlerManager.fireEvent(new GPLoginEvent(
+                                                            publishShapePreviewEvent));
+                                                } else if (exception.getCause() instanceof GPReloadURLException) {
+                                                    GeoPlatformMessage.errorMessage(
+                                                            PublisherWidgetConstants.INSTANCE.
                                                                     GPPublisherWidget_errorReloadClusterTitleText(),
-                                                                    PublisherWidgetMessages.INSTANCE.
+                                                            PublisherWidgetMessages.INSTANCE.
                                                                     GPPublisherWidget_errorReloadClusterBodyMessage(
                                                                             exception.getCause().getMessage()));
-                                                            LayoutManager.getInstance().getStatusMap().
+                                                    LayoutManager.getInstance().getStatusMap().
                                                             setStatus(PublisherWidgetConstants.INSTANCE.
-                                                                    statusErrorShapePublishingText(),
+                                                                            statusErrorShapePublishingText(),
                                                                     EnumSearchStatus.STATUS_NO_SEARCH.
-                                                                    toString());
-                                                            System.out.println(
-                                                                    "Error Publishing previewed shape: " + exception.
+                                                                            toString());
+                                                    System.out.println(
+                                                            "Error Publishing previewed shape: " + exception.
                                                                     toString()
                                                                     + " data: " + exception.getMessage());
-                                                        } else {
-                                                            GeoPlatformMessage.errorMessage(
-                                                                    PublisherWidgetConstants.INSTANCE.errorPublishingText(),
-                                                                    WindowsConstants.INSTANCE.errorMakingConnectionBodyText());
-                                                            LayoutManager.getInstance().getStatusMap().
+                                                } else {
+                                                    GeoPlatformMessage.errorMessage(
+                                                            PublisherWidgetConstants.INSTANCE.errorPublishingText(),
+                                                            WindowsConstants.INSTANCE.errorMakingConnectionBodyText());
+                                                    LayoutManager.getInstance().getStatusMap().
                                                             setStatus(
                                                                     PublisherWidgetConstants.INSTANCE.
-                                                                    statusErrorShapePublishingText(),
+                                                                            statusErrorShapePublishingText(),
                                                                     EnumSearchStatus.STATUS_NO_SEARCH.
-                                                                    toString());
-                                                            System.out.println(
-                                                                    "Error Publishing previewed shape: " + exception.
+                                                                            toString());
+                                                    System.out.println(
+                                                            "Error Publishing previewed shape: " + exception.
                                                                     toString()
                                                                     + " data: " + exception.getMessage());
-                                                        }
-                                                    }
+                                                }
+                                            }
                                         });
                             } else {
                                 GeoPlatformMessage.alertMessage(

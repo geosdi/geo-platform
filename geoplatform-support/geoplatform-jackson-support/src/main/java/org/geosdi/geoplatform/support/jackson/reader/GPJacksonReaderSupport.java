b94cad344afdef907028dcc1a520f9ed9f7441e9
Giuseppe La Scaleia <glascaleia@gmail.com>
2017-03-11 10:59:22 +0100

    Improvements

---
+++ geoplatform-support/geoplatform-jackson-support/src/main/java/org/geosdi/geoplatform/support/jackson/reader/GPJacksonReaderSupport.java
@@ -0,0 +1,74 @@
+package org.geosdi.geoplatform.support.jackson.reader;
+
+import com.google.common.base.Preconditions;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.Collection;
+
+/**
+ * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
+ * @email giuseppe.lascaleia@geosdi.org
+ */
+public interface GPJacksonReaderSupport<T extends Object> {
+
+    /**
+     * @param url
+     * @return {@link T}
+     * @throws Exception
+     */
+    T read(URL url) throws Exception;
+
+    /**
+     * @param file
+     * @return {@link T}
+     * @throws Exception
+     */
+    T read(File file) throws Exception;
+
+    /**
+     * @param inputStream
+     * @return {@link T}
+     * @throws Exception
+     */
+    T read(InputStream inputStream) throws Exception;
+
+    /**
+     * @param reader
+     * @return {@link T+}
+     * @throws Exception
+     */
+    T read(Reader reader) throws Exception;
+
+    /**
+     * @param entityAsString
+     * @return {@link T}
+     * @throws Exception
+     */
+    T read(String entityAsString) throws Exception;
+
+    /**
+     * @param direrctory
+     * @return {@link Collection<T>}
+     * @throws Exception
+     */
+    Collection<T> readFromDirectory(Path direrctory) throws Exception;
+
+    /**
+     * @param thePath
+     * @return {@link T}
+     */
+    default T read(Path thePath) {
+        Preconditions.checkArgument((thePath != null) && (thePath.toFile().exists()),
+                "The Parameter Path must not be null and the Associated File must exist.");
+        try {
+            return this.read(thePath.toFile());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ geoplatform-support/geoplatform-jackson-support/src/test/java/org/geosdi/geoplatform/support/jackson/mapper/SimpleBeanJacksonMapperTest.java
@@ -0,0 +1,93 @@
+package org.geosdi.geoplatform.support.jackson.mapper;
+
+import org.geosdi.geoplatform.support.jackson.GPJacksonSupport;
+import org.geosdi.geoplatform.support.jackson.model.SimpleBean;
+import org.geosdi.geoplatform.support.jackson.property.GPJsonIncludeFeature;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.geosdi.geoplatform.support.jackson.property.GPJacksonSupportEnum.*;
+
+/**
+ * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
+ * @email giuseppe.lascaleia@geosdi.org
+ */
+public class SimpleBeanJacksonMapperTest {
+
+    private static final GPJacksonMapper<SimpleBean> GP_JACKSON_MAPPER = new GPBaseJacksonMapper<>(SimpleBean.class,
+            new GPJacksonSupport(UNWRAP_ROOT_VALUE_DISABLE,
+                    FAIL_ON_UNKNOW_PROPERTIES_DISABLE,
+                    ACCEPT_SINGLE_VALUE_AS_ARRAY_ENABLE,
+                    WRAP_ROOT_VALUE_DISABLE,
+                    INDENT_OUTPUT_ENABLE)
+                    .configure(WRITE_DATES_AS_TIMESTAMPS_DISABLE)
+                    .configure(GPJsonIncludeFeature.NON_NULL));
+
+    @Test
+    public void readJsonFromStringTest() throws Exception {
+        SimpleBean simpleBean = GP_JACKSON_MAPPER.read("{\n" +
+                "  \"args\": {\n" +
+                "    \"color\": \"red\", \n" +
+                "    \"shape\": \"square\"\n" +
+                "  }, \n" +
+                "  \"headers\": {\n" +
+                "    \"Accept\": \"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\", \n" +
+                "    \"Accept-Encoding\": \"gzip, deflate, br\", \n" +
+                "    \"Accept-Language\": \"it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3\", \n" +
+                "    \"Host\": \"httpbin.org\", \n" +
+                "    \"Upgrade-Insecure-Requests\": \"1\", \n" +
+                "    \"User-Agent\": \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:52.0) Gecko/20100101 Firefox/52.0\"\n" +
+                "  }, \n" +
+                "  \"origin\": \"82.61.27.192\", \n" +
+                "  \"url\": \"https://httpbin.org/get?color=red&shape=square\"\n" +
+                "}");
+        Assert.assertNotNull(simpleBean);
+    }
+
+    @Test
+    public void readJsonFromFileTest() throws Exception {
+        SimpleBean simpleBean = GP_JACKSON_MAPPER.read(new File("./src/test/resources/simple_bean.json"));
+        Assert.assertNotNull(simpleBean);
+    }
+
+    @Test
+    public void writeJsonTest() throws Exception {
+        System.out.println("###################################JSON_AS_STRING : " + GP_JACKSON_MAPPER
+                .writeAsString(simpleBean()));
+    }
+
+    @Test
+    public void writeFilesFromDirectoryTest() throws Exception {
+        GP_JACKSON_MAPPER.readFromDirectory(Paths.get("./src/test/resources/files"))
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void writeJsonAsFileTest() throws Exception {
+        GP_JACKSON_MAPPER.write(new File("./target/SimpleBean.json"), simpleBean());
+    }
+
+    public static SimpleBean simpleBean() {
+        SimpleBean simpleBean = new SimpleBean();
+        Map<String, String> args = new HashMap<>();
+        args.put("color", "white");
+        args.put("shape", "square");
+        simpleBean.setArguments(args);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+        headers.put("Accept-Encoding", "gzip, deflate, br");
+        headers.put("Accept-Language", "it-IT,it;q=0.8,en-US;q=0.5,en;q=0.3");
+        headers.put("Host", "httpbin.org");
+        headers.put("Upgrade-Insecure-Requests", "1");
+        headers.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:52.0) Gecko/20100101 Firefox/52.0");
+        simpleBean.setHeaders(headers);
+        simpleBean.setOrigin("82.61.27.192");
+        simpleBean.setUrl("https://httpbin.org/get?color=red&shape=square");
+        return simpleBean;
+    }
+}

b28789825891ee717a3fd000fd781aa3f078d5d8
Giuseppe La Scaleia <glascaleia@gmail.com>
2017-03-16 10:19:27 +0100

    Try to fix Jenkins Build Failure

--- geoplatform-services/geoplatform-modules/geoplatform-catalog/csw-catalog/csw-catalog-impl/src/main/java/org/geosdi/geoplatform/services/delegate/dc/DublinCoreAnalyzer.java
+++ geoplatform-services/geoplatform-modules/geoplatform-catalog/csw-catalog/csw-catalog-impl/src/main/java/org/geosdi/geoplatform/services/delegate/dc/DublinCoreAnalyzer.java
@@ -1,46 +1,42 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.services.delegate.dc;
 
-import java.util.List;
-import javax.xml.bind.JAXBElement;
 import org.geosdi.geoplatform.gui.responce.DCEsriScheme;
 import org.geosdi.geoplatform.gui.responce.OnlineResourceProtocolType;
 import org.geosdi.geoplatform.gui.responce.URIDTO;
-import org.geosdi.geoplatform.gui.shared.bean.BBox;
 import org.geosdi.geoplatform.responce.FullRecordDTO;
 import org.geosdi.geoplatform.services.BindingUtility;
 import org.geosdi.geoplatform.xml.csw.v202.RecordType;
@@ -49,6 +45,9 @@ import org.geosdi.geoplatform.xml.csw.v202.dc.terms.URI;
 import org.geosdi.geoplatform.xml.ows.v100.BoundingBoxType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.xml.bind.JAXBElement;
+import java.util.List;
 
 /**
  *
@@ -65,7 +64,7 @@ public interface DublinCoreAnalyzer {
      */
     <F extends FullRecordDTO> void analyzeRecord(RecordType record, F fullRecord);
 
-    static class GPDublinCoreAnalyzer implements DublinCoreAnalyzer {
+    class GPDublinCoreAnalyzer implements DublinCoreAnalyzer {
 
         private static final Logger logger = LoggerFactory.getLogger(GPDublinCoreAnalyzer.class);
 
@@ -79,16 +78,16 @@ public interface DublinCoreAnalyzer {
 
             for (JAXBElement<? extends SimpleLiteral> element : record.getDCElement()) {
                 logger.trace("###########LOCAL_PART : {} - PREFIX: {} - SCHEME : {} "
-                        + "- ELEMENTS : {}", element.getName().getLocalPart(),
+                                + "- ELEMENTS : {}", element.getName().getLocalPart(),
                         element.getName().getNamespaceURI(), element.getValue()
-                        .getScheme(), element.getValue().getContent());
+                                .getScheme(), element.getValue().getContent());
 
                 String localPartElement = element.getName().getLocalPart();
                 List<String> contentElement = element.getValue().getContent();
                 SimpleLiteral value = element.getValue();
 
                 logger.trace("\n\n###################LOCAL_PART_NAME : {} "
-                        + "- CONTENT_ELEMENT : {}\n\n", localPartElement,
+                                + "- CONTENT_ELEMENT : {}\n\n", localPartElement,
                         contentElement);
 
                 if ("identifier".equals(localPartElement)) {

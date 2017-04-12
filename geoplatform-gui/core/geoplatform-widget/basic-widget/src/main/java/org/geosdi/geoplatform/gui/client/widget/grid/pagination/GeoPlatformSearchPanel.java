14eb1b591af8e8db5198b09be6b2e9269618954c
Vito Salvia <vito.salvia@gmail.com>
2017-04-12 14:26:01 +0200

    GeoPlatformSearchPanel improvements

--- geoplatform-gui/core/geoplatform-widget/basic-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/grid/pagination/GeoPlatformSearchPanel.java
+++ geoplatform-gui/core/geoplatform-widget/basic-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/grid/pagination/GeoPlatformSearchPanel.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.widget.grid.pagination;
 
@@ -178,17 +177,17 @@ public abstract class GeoPlatformSearchPanel<C extends Widget, T extends GeoPlat
         formPanel.getButtonBar().add(this.searchStatus);
         formPanel.getButtonBar().add(new LabelToolItem("    "));
         formPanel.setButtonAlign(HorizontalAlignment.RIGHT);
-        selectButton = new Button(ButtonsConstants.INSTANCE.selectText(),
-                new SelectionListener<ButtonEvent>() {
+        selectButton = new Button();
+        selectButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
 
-                    @Override
-                    public void componentSelected(ButtonEvent ce) {
-                        executeSelect();
-                    }
+            @Override
+            public void componentSelected(ButtonEvent ce) {
+                executeSelect();
+            }
 
-                });
-        selectButton.setIcon(
-                AbstractImagePrototype.create(BasicWidgetResources.ICONS.select()));
+        });
+        setTextSelectButton();
+        setIconSelectButton();
         selectButton.disable();
         formPanel.addButton(this.selectButton);
         cancelButton = new Button(ButtonsConstants.INSTANCE.cancelText(),
@@ -205,6 +204,15 @@ public abstract class GeoPlatformSearchPanel<C extends Widget, T extends GeoPlat
         formPanel.addButton(cancelButton);
         formPanel.setBottomComponent(this.toolBar);
         vp.add(formPanel);
+    }
+
+    protected void setTextSelectButton() {
+        selectButton.setText(ButtonsConstants.INSTANCE.selectText());
+    }
+
+    protected void setIconSelectButton(){
+        selectButton.setIcon(
+                AbstractImagePrototype.create(BasicWidgetResources.ICONS.select()));
     }
 
     /**

e8ef280f73c4458959e543c0bbeea85778d5ae4b
Giuseppe La Scaleia <glascaleia@gmail.com>
2017-04-20 09:23:18 +0200

    Try to fix SaveCASStyleCommand

--- geoplatform-services/geoplatform-modules/geoplatform-publisher/geoplatform-publisher-base-impl/src/main/java/org/geosdi/geoplatform/services/GPPublisherBasicServiceImpl.java
+++ geoplatform-services/geoplatform-modules/geoplatform-publisher/geoplatform-publisher-base-impl/src/main/java/org/geosdi/geoplatform/services/GPPublisherBasicServiceImpl.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.services;
 
@@ -249,10 +248,10 @@ public class GPPublisherBasicServiceImpl implements IGPPublisherService,
      * @return
      * @throws ResourceNotFoundFault
      * @throws FileNotFoundException this service publishes the layer
-     * <publishRequest.getLayerName()> we loaded in the previews workspace into
-     * the DB datastore identified by the <publishRequest.getDataStoreName()>
-     * and published into the <publishRequest.getWorkspace()>
-     * workspace
+     *                               <publishRequest.getLayerName()> we loaded in the previews workspace into
+     *                               the DB datastore identified by the <publishRequest.getDataStoreName()>
+     *                               and published into the <publishRequest.getWorkspace()>
+     *                               workspace
      */
     @Override
     public Boolean publish(PublishLayerRequest publishRequest) throws
@@ -286,7 +285,7 @@ public class GPPublisherBasicServiceImpl implements IGPPublisherService,
      * @param layerName
      * @return
      * @throws ResourceNotFoundFault this service removes a layer from the
-     * workspace
+     *                               workspace
      */
     public Boolean removeSHPFromPreview(String workspace, String layerName)
             throws ResourceNotFoundFault {
@@ -308,7 +307,7 @@ public class GPPublisherBasicServiceImpl implements IGPPublisherService,
      * @param layerName
      * @return
      * @throws ResourceNotFoundFault this service removes a layer from the
-     * workspace
+     *                               workspace
      */
     public Boolean removeTIFFromPreview(String workspace, String layerName)
             throws ResourceNotFoundFault {
@@ -361,7 +360,7 @@ public class GPPublisherBasicServiceImpl implements IGPPublisherService,
      * @param workspace the wk to use
      * @param layerName the layerName to retrieve
      * @param styleName the style name to set or if null will be setted the
-     * default one
+     *                  default one
      * @return the builded InfoPreview
      * @throws ResourceNotFoundFault
      */
@@ -782,7 +781,7 @@ public class GPPublisherBasicServiceImpl implements IGPPublisherService,
      */
     @Override
     public Boolean existsStyle(String styleName) {
-        return restReader.existsStyle(styleName, true);
+        return restReader.existsStyle(styleName);
     }
 
     @Override
@@ -840,9 +839,7 @@ public class GPPublisherBasicServiceImpl implements IGPPublisherService,
      * ******************
      *
      * @param userName
-     * @return
-     *
-     * creates the workspace if not exists or returns the existing one
+     * @return creates the workspace if not exists or returns the existing one
      */
     private String getWorkspace(String userName) {
         List<String> workspaces = restReader.getWorkspaceNames();
@@ -1134,10 +1131,10 @@ public class GPPublisherBasicServiceImpl implements IGPPublisherService,
      * @param file
      * @return returns the URL to the PNG of the layer uploaded in the ZIP file
      * @throws ResourceNotFoundFault this exception may be launched when: the
-     * ZIP file does not contain a SHP file this service upload in the user
-     * workspace a shapefile. The ZIP file must contain the shp, the prj, the
-     * shx and the dbf files. Otherwise, an exception is raised
-     * ****************************
+     *                               ZIP file does not contain a SHP file this service upload in the user
+     *                               workspace a shapefile. The ZIP file must contain the shp, the prj, the
+     *                               shx and the dbf files. Otherwise, an exception is raised
+     *                               ****************************
      */
     @Override
     public InfoPreviewStore analyzeZIPEPSG(String sessionID, String userName,

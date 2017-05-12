2e91dad2b487a9cb85119cb3afa4bcf5bd832b40
Giuseppe La Scaleia <glascaleia@gmail.com>
2017-05-12 08:52:28 +0200

    FIXED Jenkins Build Failure

--- geoplatform-services/geoplatform-modules/geoplatform-ogc/wfs/wfs-impl/src/test/java/org/geosdi/geoplatform/feature/reader/FeatureReaderTest.java
+++ geoplatform-services/geoplatform-modules/geoplatform-ogc/wfs/wfs-impl/src/test/java/org/geosdi/geoplatform/feature/reader/FeatureReaderTest.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.feature.reader;
 
@@ -59,9 +58,8 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
+ * @author Giuseppe La Scaleia <giuseppe.lascaleia@geosdi.org>
  * @author Vincenzo Monteverde <vincenzo.monteverde@geosdi.org>
- * @email giuseppe.lascaleia@geosdi.org
  */
 @RunWith(Parameterized.class)
 public class FeatureReaderTest {
@@ -100,8 +98,8 @@ public class FeatureReaderTest {
 
     @Parameters
     public static Collection<Object[]> data() {
-        return Arrays.asList(
-                new Object[][]{{"restricted", 1, 4}, {"restricted-null", 1, 4}, {"states", 22, 49}, {"giant_polygon", 0, 1}, {"tiger_roads", 2, 1000}});
+        return Arrays.asList(new Object[][]{{"restricted", 1, 4}, {"restricted-null", 1, 4},
+                {"states", 22, 49}, {"giant_polygon", 0, 1}, {"tiger_roads", 2, 1000}});
     }
 
     @Test
@@ -129,7 +127,7 @@ public class FeatureReaderTest {
                 Assert.assertNotNull(att.getName());
                 Assert.assertNotNull(att.getType());
             }
-            logger.debug("\n\n@@@@@@@@@@@@@@@@@@@@ {}", layerSchema);
+            logger.debug("@@@@@@@@@@@@@@@@@@@@LAYER_SCHEMA : {}", layerSchema);
 
             WFSGetFeatureStaxReader featureReader = new WFSGetFeatureStaxReader(layerSchema);
             FeatureCollectionDTO fc = featureReader.read(new File(fileGF));
@@ -144,7 +142,7 @@ public class FeatureReaderTest {
                 Assert.assertTrue(feature.getFID().contains(name));
                 Assert.assertNotNull(feature.getGeometry());
                 if (numAttributes == 0) {
-                    Assert.assertNull(feature.getAttributes());
+                    Assert.assertTrue(feature.getAttributes().getAttributesMap().isEmpty());
                 } else {
                     Assert.assertNotNull(feature.getAttributes());
                     Map<String, String> fMap = feature.getAttributes().getAttributesMap();
@@ -152,8 +150,8 @@ public class FeatureReaderTest {
                     logger.debug("#################FMAP_SIZE : {}\n", fMap.size());
                     Assert.assertEquals(numAttributes, fMap.size());
                 }
-                logger.debug("\n\n@@@@@@@@@@@@@@@@@@@@ {} - {}", feature.getFID(), feature.getAttributes());
-                logger.debug("\nGEOMETRY @@@@@@@@@@@@@@@@ : {}", feature.getGeometry());
+                logger.debug("@@@@@@@@@@@@@@@@@@@@ {} - {}", feature.getFID(), feature.getAttributes());
+                logger.debug("GEOMETRY @@@@@@@@@@@@@@@@ : {}", feature.getGeometry());
             }
 
         } finally {

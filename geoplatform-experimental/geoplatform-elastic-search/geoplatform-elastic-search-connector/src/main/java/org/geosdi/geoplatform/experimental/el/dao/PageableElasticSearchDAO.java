859b95056aecae4e3d013d27828204334527f62c
Giuseppe La Scaleia <glascaleia@gmail.com>
2017-05-30 15:24:05 +0200

    Improvements

--- geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-connector/src/main/java/org/geosdi/geoplatform/experimental/el/dao/PageableElasticSearchDAO.java
+++ geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-connector/src/main/java/org/geosdi/geoplatform/experimental/el/dao/PageableElasticSearchDAO.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.experimental.el.dao;
 
@@ -71,7 +70,7 @@ public abstract class PageableElasticSearchDAO<D extends Document> extends GPBas
         }
         Long total = searchResponse.getHits().getTotalHits();
         logger.debug("###################TOTAL HITS FOUND : {} .\n\n", total);
-        return new PageResult<D>(total, Stream.of(searchResponse.getHits().hits())
+        return new PageResult<D>(total, Stream.of(searchResponse.getHits().getHits())
                 .map(searchHit -> this.readDocument(searchHit))
                 .filter(s -> s != null).collect(Collectors.toList()));
     }
@@ -150,7 +149,9 @@ public abstract class PageableElasticSearchDAO<D extends Document> extends GPBas
         return searchResponse;
     }
 
-    /**s
+    /**
+     * s
+     *
      * @param page
      * @throws Exception
      */
@@ -177,5 +178,23 @@ public abstract class PageableElasticSearchDAO<D extends Document> extends GPBas
                 throw new IllegalStateException(ex);
             }
         }, this.elasticSearchExecutor);
+    }
+
+    /**
+     * @param page
+     * @return {@link Long}
+     * @throws Exception
+     */
+    @Override
+    public <P extends Page> Long count(P page) throws Exception {
+        Preconditions.checkArgument((page != null), "Page must not be null.");
+        SearchRequestBuilder builder = page.buildPage(this.elastichSearchClient
+                .prepareSearch(getIndexName()).setTypes(getIndexType()));
+        logger.trace("#########################Builder : {}\n\n", builder.toString());
+        SearchResponse searchResponse = builder.get();
+        if (searchResponse.status() != RestStatus.OK) {
+            throw new IllegalStateException("Problem in Search : " + searchResponse.status());
+        }
+        return searchResponse.getHits().getTotalHits();
     }
 }

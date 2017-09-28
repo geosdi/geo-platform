87fbee4da35e77d34046818d723b360339e71273
Giuseppe La Scaleia <glascaleia@gmail.com>
2017-09-28 10:11:26 +0200

    Added New SecurityConnector @TODO) Try the point of Code to set the new Value

---
+++ geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-connector-api/src/main/java/org/geosdi/geoplatform/connector/server/security/GPHeaderSecurityConnector.java
@@ -0,0 +1,49 @@
+package org.geosdi.geoplatform.connector.server.security;
+
+import com.google.common.base.Preconditions;
+import net.jcip.annotations.Immutable;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.geosdi.geoplatform.connector.server.request.GPConnectorRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
+ * @email giuseppe.lascaleia@geosdi.org
+ */
+@Immutable
+public class GPHeaderSecurityConnector implements GPSecurityConnector {
+
+    private static final Logger logger = LoggerFactory.getLogger(GPHeaderSecurityConnector.class);
+    //
+    private final Map<String, String> headerParams;
+
+    public GPHeaderSecurityConnector(Map<String, String> theHeaderParams) {
+        Preconditions.checkArgument((theHeaderParams != null) && !(theHeaderParams.isEmpty()),
+                "The HeaderParams must not be null or Empty Map.");
+        this.headerParams = theHeaderParams;
+    }
+
+    /**
+     * <p>Method to secure Connection</p>
+     *
+     * @param connectorRequest
+     * @param httpRequest
+     * @return CloseableHttpResponse
+     * @throws {@link IOException}
+     */
+    @Override
+    public <C extends GPConnectorRequest, H extends HttpUriRequest> CloseableHttpResponse secure(C connectorRequest,
+            H httpRequest) throws IOException {
+        logger.debug("#########################{} is injecting values {} in Header Request.\n",
+                this.getClass().getSimpleName(), this.headerParams);
+        this.headerParams.entrySet()
+                .stream()
+                .forEach(entry -> httpRequest.addHeader(entry.getKey(), entry.getValue()));
+        return connectorRequest.getClientConnection().execute(httpRequest);
+    }
+}

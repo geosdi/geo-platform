2ed06b766458a7e3c37e1eef61afe087c1154d7a
Giuseppe La Scaleia <glascaleia@gmail.com>
2017-10-04 10:14:07 +0200

    Upgrade cobertura maven plugin

--- geoplatform-tools/geoplatform-persistence-layer/geoplatform-persistence-config/src/main/java/org/geosdi/geoplatform/persistence/configuration/jpa/GPPersistenceJpaConfig.java
+++ geoplatform-tools/geoplatform-persistence-layer/geoplatform-persistence-config/src/main/java/org/geosdi/geoplatform/persistence/configuration/jpa/GPPersistenceJpaConfig.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.persistence.configuration.jpa;
 
@@ -53,7 +52,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 
 /**
- *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
  * @email giuseppe.lascaleia@geosdi.org
  */
@@ -61,7 +59,7 @@ import javax.sql.DataSource;
 @Profile(value = "jpa")
 @EnableTransactionManagement
 class GPPersistenceJpaConfig {
-    
+
     @Autowired
     private GPPersistenceConnector gpPersistenceConnector;
     //
@@ -73,38 +71,31 @@ class GPPersistenceJpaConfig {
     //
     @Autowired
     private PropertiesStrategyManager hibPropStrategyManager;
-    
+
     @Bean(name = "entityManagerFactory")
     public LocalContainerEntityManagerFactoryBean gpEntityManagerFactory() {
-        final LocalContainerEntityManagerFactoryBean gpFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        LocalContainerEntityManagerFactoryBean gpFactoryBean = new LocalContainerEntityManagerFactoryBean();
         gpFactoryBean.setDataSource(this.persitenceDataSource);
-
-        gpFactoryBean.setPackagesToScan(
-                this.gpPersistenceConnector.getPackagesToScan());
-        
+        gpFactoryBean.setPackagesToScan(this.gpPersistenceConnector.getPackagesToScan());
         gpFactoryBean.setJpaVendorAdapter(this.jpaVendorAdapter);
         gpFactoryBean.setLoadTimeWeaver(this.gpLoadTimeWeaver());
-        gpFactoryBean.setJpaProperties(
-                this.hibPropStrategyManager.getProperties());
+        gpFactoryBean.setJpaProperties(this.hibPropStrategyManager.getProperties());
         gpFactoryBean.setPersistenceUnitName("geoplatform-persistence-layer");
-        
         return gpFactoryBean;
     }
-    
+
     @Bean(name = "transactionManager")
     public PlatformTransactionManager gpTransactionManager() {
-        final JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(
-                this.gpEntityManagerFactory().getObject());
-        
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(this.gpEntityManagerFactory().getObject());
         return transactionManager;
     }
-    
+
     @Bean
     public PersistenceExceptionTranslationPostProcessor persistenceExceptionTranslationPostProcessor() {
         return new PersistenceExceptionTranslationPostProcessor();
     }
-    
+
     @Bean
     public LoadTimeWeaver gpLoadTimeWeaver() {
         return new InstrumentationLoadTimeWeaver();

0a7d514b624e58546e139f2ca5fe8882f42d9181
Giuseppe La Scaleia <glascaleia@gmail.com>
2017-10-17 16:37:17 +0200

    Added Modules

--- geoplatform-experimental/geoplatform-mongodb/src/test/java/org/geosdi/geoplatform/experimental/mongodb/template/GPMongoConfigTest.java
+++ geoplatform-experimental/geoplatform-mongodb/src/test/java/org/geosdi/geoplatform/experimental/mongodb/template/GPMongoConfigTest.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.experimental.mongodb.template;
 
@@ -51,13 +50,14 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.*;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
- *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
  * @email giuseppe.lascaleia@geosdi.org
  */
@@ -95,11 +95,12 @@ public class GPMongoConfigTest {
         Assert.assertNotNull("Address Repo must not be NULL",
                 addressRepo);
 
-        addressRepo.save(Arrays.asList(new Address[]{new Address("A", 0.001, -0.002),
-            new Address("B", 1, 1), new Address("C", 0.5, 0.5),
-            new Address("D", -0.5, -0.5), new Address("Berlin", 13.405838, 52.531261),
-            new Address("Cologne", 6.921272, 50.960157),
-            new Address("Dusseldorf", 6.810036, 51.224088)}));
+        addressRepo.save(Stream.of(new Address("A", 0.001, -0.002),
+                new Address("B", 1, 1), new Address("C", 0.5, 0.5),
+                new Address("D", -0.5, -0.5), new Address("Berlin", 13.405838, 52.531261),
+                new Address("Cologne", 6.921272, 50.960157),
+                new Address("Dusseldorf", 6.810036, 51.224088))
+                .collect(toList()));
     }
 
     @Test
@@ -116,20 +117,15 @@ public class GPMongoConfigTest {
     @Test
     public void shouldFindAroundOrigin() {
         List<Address> addresses = addressRepo.findByLocationWithin(new Circle(0, 0, 0.75));
-
         Assert.assertEquals(3, addresses.size());
-
-        logger.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ shouldFindAroundOrigin() Found "
-                + ": {}\n\n", addresses);
+        logger.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ shouldFindAroundOrigin() Found : {}\n\n", addresses);
     }
 
     @Test
     public void shouldFindWithinBox() {
         List<Address> addresses = addressRepo.findByLocationWithin(new Box(new Point(0.25, 0.25),
                 new Point(1, 1)));
-
         Assert.assertEquals(2, addresses.size());
-
         logger.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ shouldFindWithinBox() Found "
                 + ": {}\n\n", addresses);
     }
@@ -161,7 +157,7 @@ public class GPMongoConfigTest {
         }
 
         logger.info("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ insertMassiveAddressTest "
-                + ": Executed {} Inserts in {} Minutes \n", 4000,
+                        + ": Executed {} Inserts in {} Minutes \n", 4000,
                 TimeUnit.MILLISECONDS.toMinutes(time));
     }
 

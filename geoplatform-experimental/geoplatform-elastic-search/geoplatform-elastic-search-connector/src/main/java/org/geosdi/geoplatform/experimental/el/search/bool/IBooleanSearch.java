4be2193b341da284de49774fa01e8e5dc152944c
Giuseppe La Scaleia <glascaleia@gmail.com>
2017-11-12 11:56:38 +0100

    Added Comments

--- geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-connector/src/main/java/org/geosdi/geoplatform/experimental/el/search/bool/IBooleanSearch.java
+++ geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-connector/src/main/java/org/geosdi/geoplatform/experimental/el/search/bool/IBooleanSearch.java
@@ -1,43 +1,43 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.experimental.el.search.bool;
 
-import com.google.common.base.Preconditions;
 import org.elasticsearch.index.query.Operator;
 import org.geosdi.geoplatform.experimental.el.search.IGPQuerySearch;
+
+import static com.google.common.base.Preconditions.checkArgument;
 
 /**
  * @author Vito Salvia - CNR IMAA geoSDI Group
@@ -73,40 +73,60 @@ public interface IBooleanSearch extends IGPQuerySearch {
         protected final BooleanQueryType type;
         protected Operator operator;
 
+        /**
+         * @param theField
+         * @param theType
+         */
         public AbstractBooleanSearch(String theField, BooleanQueryType theType) {
-            Preconditions.checkArgument(((theField != null) && !(theField.isEmpty())),
+            checkArgument(((theField != null) && !(theField.isEmpty())),
                     "The Parameter Field must not be null or an Empty String.");
-            Preconditions.checkArgument((theType != null), "The Parameter Type must not be null.");
+            checkArgument((theType != null), "The Parameter Type must not be null.");
             this.field = theField;
             this.type = theType;
         }
 
+        /**
+         * @param theField
+         * @param theValue
+         * @param theType
+         */
         public AbstractBooleanSearch(String theField, Object theValue, BooleanQueryType theType) {
-            Preconditions.checkArgument(((theField != null) && !(theField.isEmpty())),
+            checkArgument(((theField != null) && !(theField.isEmpty())),
                     "The Parameter Field must not be null or an Empty String.");
-            Preconditions.checkArgument(((theValue != null)), "The Parameter Value must not be null.");
-            Preconditions.checkArgument((theType != null), "The Parameter Type must not be null.");
+            checkArgument(((theValue != null)), "The Parameter Value must not be null.");
+            checkArgument((theType != null), "The Parameter Type must not be null.");
             this.field = theField;
             this.value = theValue;
             this.type = theType;
         }
 
+        /**
+         * @param theValue
+         * @param theType
+         * @param theOperator
+         */
         public AbstractBooleanSearch(Object theValue, BooleanQueryType theType,
-               Operator theOperator) {
-            Preconditions.checkArgument(((theValue != null)), "The Parameter Value must not be null.");
-            Preconditions.checkArgument((theType != null), "The Parameter Type must not be null.");
-            Preconditions.checkArgument((theOperator != null), "The Parameter Operator must not be null.");
+                Operator theOperator) {
+            checkArgument(((theValue != null)), "The Parameter Value must not be null.");
+            checkArgument((theType != null), "The Parameter Type must not be null.");
+            checkArgument((theOperator != null), "The Parameter Operator must not be null.");
             this.value = theValue;
             this.type = theType;
             this.operator = theOperator;
         }
 
+        /**
+         * @param theValue
+         * @param theField
+         * @param theType
+         * @param theOperator
+         */
         public AbstractBooleanSearch(Object theValue, String theField, BooleanQueryType theType,
                 Operator theOperator) {
-            Preconditions.checkArgument(((theValue != null)), "The Parameter Value must not be null.");
-            Preconditions.checkArgument((theType != null), "The Parameter Type must not be null.");
-            Preconditions.checkArgument((theOperator != null), "The Parameter Operator must not be null.");
-            Preconditions.checkArgument(((theField != null) && !(theField.isEmpty())),
+            checkArgument(((theValue != null)), "The Parameter Value must not be null.");
+            checkArgument((theType != null), "The Parameter Type must not be null.");
+            checkArgument((theOperator != null), "The Parameter Operator must not be null.");
+            checkArgument(((theField != null) && !(theField.isEmpty())),
                     "The Parameter Field must not be null or an Empty String.");
             this.value = theValue;
             this.field = theField;

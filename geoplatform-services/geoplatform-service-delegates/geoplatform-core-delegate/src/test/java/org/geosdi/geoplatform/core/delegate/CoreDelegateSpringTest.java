5868000ca187537da56b8b4f92eab9e7503ff1d1
Giuseppe La Scaleia <glascaleia@gmail.com>
2017-11-18 09:40:24 +0100

    FIXED Test Problems

--- geoplatform-services/geoplatform-service-delegates/geoplatform-core-delegate/src/test/java/org/geosdi/geoplatform/core/delegate/CoreDelegateSpringTest.java
+++ geoplatform-services/geoplatform-service-delegates/geoplatform-core-delegate/src/test/java/org/geosdi/geoplatform/core/delegate/CoreDelegateSpringTest.java
@@ -48,6 +48,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -58,6 +59,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:applicationContext.xml"})
+@ActiveProfiles(value = {"jpa"})
 public class CoreDelegateSpringTest {
 
     @Autowired

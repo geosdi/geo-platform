9538f00dd3c62e957a93f43f857036e0a03be26c
Giuseppe La Scaleia <glascaleia@gmail.com>
2018-01-25 15:37:12 +0100

    Improvements and Fixing Bad Reference to other DataSource

--- geoplatform-tools/geoplatform-persistence-layer/geoplatform-persistence-config/src/main/java/org/geosdi/geoplatform/persistence/configuration/hibernate/GPPersistenceHibernateConfig.java
+++ geoplatform-tools/geoplatform-persistence-layer/geoplatform-persistence-config/src/main/java/org/geosdi/geoplatform/persistence/configuration/hibernate/GPPersistenceHibernateConfig.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2017 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.persistence.configuration.hibernate;
 
@@ -42,26 +41,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 /**
- *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
  * @email giuseppe.lascaleia@geosdi.org
  */
 @Configuration
 @Profile(value = "hibernate")
 @EnableTransactionManagement
-public class GPPersistenceHibernateConfig {
+class GPPersistenceHibernateConfig {
 
     @Autowired
     private GPPersistenceConnector gpPersistenceConnector;
     //
-    @Autowired
+    @Resource(name = "persitenceDataSource")
     private DataSource persitenceDataSource;
     //
     @Autowired
@@ -69,21 +68,17 @@ public class GPPersistenceHibernateConfig {
 
     @Bean
     public LocalSessionFactoryBean gpSessionFactoryBean() {
-        final LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
         factoryBean.setDataSource(this.persitenceDataSource);
-        factoryBean.setPackagesToScan(
-                this.gpPersistenceConnector.getPackagesToScan());
-        factoryBean.setHibernateProperties(
-                this.hibPropStrategyManager.getProperties());
-
+        factoryBean.setPackagesToScan(this.gpPersistenceConnector.getPackagesToScan());
+        factoryBean.setHibernateProperties(this.hibPropStrategyManager.getProperties());
         return factoryBean;
     }
 
     @Bean
     public HibernateTransactionManager transactionManager() {
-        final HibernateTransactionManager txManager = new HibernateTransactionManager();
+        HibernateTransactionManager txManager = new HibernateTransactionManager();
         txManager.setSessionFactory(this.gpSessionFactoryBean().getObject());
-
         return txManager;
     }
 

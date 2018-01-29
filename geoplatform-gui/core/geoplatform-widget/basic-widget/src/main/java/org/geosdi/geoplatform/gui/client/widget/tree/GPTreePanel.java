a68a830371e10a48877e3dd30b2c30f8e2f8eb45
Giuseppe La Scaleia <glascaleia@gmail.com>
2018-01-29 18:36:47 +0100

    FIXED Heavy Error to load all layers on Tree

--- geoplatform-gui/core/geoplatform-widget/basic-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/tree/GPTreePanel.java
+++ geoplatform-gui/core/geoplatform-widget/basic-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/tree/GPTreePanel.java
@@ -1,49 +1,42 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2018 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2018 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.widget.tree;
 
 import com.extjs.gxt.ui.client.data.ModelData;
 import com.extjs.gxt.ui.client.widget.treepanel.TreePanel;
 import com.google.common.collect.Lists;
-
-import java.util.Iterator;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.geosdi.geoplatform.gui.client.widget.map.event.LayerRangeEventHandler;
 import org.geosdi.geoplatform.gui.client.widget.tree.decorator.GPTreeCheckDecorator;
 import org.geosdi.geoplatform.gui.client.widget.tree.decorator.GPTreeIconDecorator;
@@ -58,18 +51,24 @@ import org.geosdi.geoplatform.gui.model.tree.GPLayerTreeModel;
 import org.geosdi.geoplatform.gui.puregwt.layers.LayerHandlerManager;
 import org.geosdi.geoplatform.gui.shared.util.GPSharedUtils;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
- * @author Nazzareno Sileno - CNR IMAA geoSDI Group
- * @email nazzareno.sileno@geosdi.org
- *
  * @param <T>
+ * @author Nazzareno Sileno  <nazzareno.sileno@geosdi.org>
+ * @author Giuseppe La Scaleia <giuseppe.lascaleia@geosdi.org>
  */
-public class GPTreePanel<T extends GPBeanTreeModel> extends TreePanel<T>
-        implements GPTreeLabelDecorator<T>, GPTreeIconDecorator<T>,
-        GPTreeCheckDecorator<T>, GPTreeLayerPresenceDecorator, LayerRangeEventHandler {
+public class GPTreePanel<T extends GPBeanTreeModel> extends TreePanel<T> implements GPTreeLabelDecorator<T>,
+        GPTreeIconDecorator<T>, GPTreeCheckDecorator<T>, GPTreeLayerPresenceDecorator, LayerRangeEventHandler {
 
     private final static Logger logger = Logger.getLogger("GPTreePanel");
 
+    /**
+     * @param store
+     */
     public GPTreePanel(GPTreeStore<T> store) {
         super(store);
         LayerHandlerManager.addHandler(TYPE, this);
@@ -86,11 +85,17 @@ public class GPTreePanel<T extends GPBeanTreeModel> extends TreePanel<T>
         ((GPTreeStore) this.store).swapModelInstance(oldModel, newModel);
     }
 
+    /**
+     * @param model
+     */
     @Override
     public void refresh(T model) {
         super.refresh(model);
     }
 
+    /**
+     * @param model
+     */
     @Override
     public void refreshLabel(T model) {
         if (rendered) {
@@ -101,6 +106,9 @@ public class GPTreePanel<T extends GPBeanTreeModel> extends TreePanel<T>
         }
     }
 
+    /**
+     * @param model
+     */
     @Override
     public void refreshIcon(T model) {
         if (rendered) {
@@ -111,6 +119,9 @@ public class GPTreePanel<T extends GPBeanTreeModel> extends TreePanel<T>
         }
     }
 
+    /**
+     * @param model
+     */
     @Override
     public void refreshCheck(T model) {
         if (rendered) {
@@ -121,17 +132,27 @@ public class GPTreePanel<T extends GPBeanTreeModel> extends TreePanel<T>
         }
     }
 
+    /**
+     * @param layerBean
+     */
     @Override
     public void inRange(GPLayerBean layerBean) {
         this.changeTextElementStyle(layerBean, "black");
     }
 
+    /**
+     * @param layerBean
+     */
     @Override
     public void outRange(GPLayerBean layerBean) {
         this.changeTextElementStyle(layerBean, "gray");
     }
 
-    private void changeTextElementStyle(GPLayerBean layerBean, String textColor) {
+    /**
+     * @param layerBean
+     * @param textColor
+     */
+    protected void changeTextElementStyle(GPLayerBean layerBean, String textColor) {
         logger.log(Level.FINEST, "inRange: " + layerBean);
         T layerElement = this.store.findModel((T) layerBean);
         TreeNode node = findNode(layerElement);
@@ -142,6 +163,11 @@ public class GPTreePanel<T extends GPBeanTreeModel> extends TreePanel<T>
         }
     }
 
+    /**
+     * @param model
+     * @param node
+     * @param deep
+     */
     @Override
     protected void onExpand(T model, TreeNode node, boolean deep) {
         super.onExpand(model, node, deep);
@@ -155,6 +181,9 @@ public class GPTreePanel<T extends GPBeanTreeModel> extends TreePanel<T>
         }
     }
 
+    /**
+     * @return {@link List<GPLayerBean>}
+     */
     @Override
     public List<GPLayerBean> getVisibleLayersOnTree() {
         List<GPLayerBean> visibleLayers = Lists.<GPLayerBean>newArrayList();
@@ -165,6 +194,9 @@ public class GPTreePanel<T extends GPBeanTreeModel> extends TreePanel<T>
         return this.getVisibleLayersOnTree(root.getChildren(), visibleLayers);
     }
 
+    /**
+     * @return {@link List<GPLayerBean>}
+     */
     @Override
     public List<GPLayerBean> getAllLayersOnTree() {
         List<GPLayerBean> allLayers = Lists.<GPLayerBean>newArrayList();
@@ -175,7 +207,12 @@ public class GPTreePanel<T extends GPBeanTreeModel> extends TreePanel<T>
         return this.getAllLayersOnTree(root.getChildren(), allLayers);
     }
 
-    private List<GPLayerBean> getVisibleLayersOnTree(List<ModelData> layers,
+    /**
+     * @param layers
+     * @param visibleLayers
+     * @return {@link List<GPLayerBean>}
+     */
+    protected List<GPLayerBean> getVisibleLayersOnTree(List<ModelData> layers,
             List<GPLayerBean> visibleLayers) {
         for (Iterator<ModelData> it = layers.iterator(); it.hasNext(); ) {
             GPBeanTreeModel element = (GPBeanTreeModel) it.next();
@@ -189,13 +226,17 @@ public class GPTreePanel<T extends GPBeanTreeModel> extends TreePanel<T>
         return visibleLayers;
     }
 
-    private List<GPLayerBean> getAllLayersOnTree(List<ModelData> layers,
+    /**
+     * @param layers
+     * @param allLayers
+     * @return {@link List<GPLayerBean>}
+     */
+    protected List<GPLayerBean> getAllLayersOnTree(List<ModelData> layers,
             List<GPLayerBean> allLayers) {
         for (Iterator<ModelData> it = layers.iterator(); it.hasNext(); ) {
             GPBeanTreeModel element = (GPBeanTreeModel) it.next();
-            if (element instanceof AbstractFolderTreeNode
-                    && element.getChildCount() != 0) {
-                this.getVisibleLayersOnTree(element.getChildren(), allLayers);
+            if (element instanceof AbstractFolderTreeNode && element.getChildCount() != 0) {
+                this.getAllLayersOnTree(element.getChildren(), allLayers);
             } else if (element instanceof GPLayerTreeModel) {
                 allLayers.add((GPLayerBean) element);
             }
@@ -203,10 +244,12 @@ public class GPTreePanel<T extends GPBeanTreeModel> extends TreePanel<T>
         return allLayers;
     }
 
-    private void activeRasterMinMaxScaleLabel(GPRasterBean rasterBean) {
+    /**
+     * @param rasterBean
+     */
+    protected void activeRasterMinMaxScaleLabel(GPRasterBean rasterBean) {
         if (rasterBean.getMaxScale() != null || rasterBean.getMinScale() != null) {
             this.outRange(rasterBean);
         }
     }
-
 }

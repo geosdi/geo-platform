28325c6e6dc0e8ec788068dd6456651f58d7c0b5
Giuseppe La Scaleia <glascaleia@gmail.com>
2018-05-15 16:47:50 +0200

    Improvements for Code Style

--- geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/LayerManagementWidget.java
+++ geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/LayerManagementWidget.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2018 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2018 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.widget;
 
@@ -43,16 +42,19 @@ import com.extjs.gxt.ui.client.util.Margins;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayout;
 import com.extjs.gxt.ui.client.widget.layout.BorderLayoutData;
 import com.google.gwt.user.client.ui.Widget;
-import javax.inject.Inject;
-import javax.inject.Singleton;
 import org.geosdi.geoplatform.gui.client.i18n.LayerModuleConstants;
 import org.geosdi.geoplatform.gui.client.model.layout.LayerBeanLayoutFactory;
 import org.geosdi.geoplatform.gui.impl.view.LayoutManager;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 /**
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
  * @email giuseppe.lascaleia@geosdi.org
- *
  */
 @Singleton
 public class LayerManagementWidget extends GeoPlatformContentPanel {
@@ -60,9 +62,12 @@ public class LayerManagementWidget extends GeoPlatformContentPanel {
     private final LayerTreePanel layerTreePanel;
     private final GPLegendPanel legendPanel;
 
+    /**
+     * @param theLayerTreePanel
+     * @param theLegendPanel
+     */
     @Inject
-    public LayerManagementWidget(LayerTreePanel theLayerTreePanel,
-            GPLegendPanel theLegendPanel) {
+    public LayerManagementWidget(LayerTreePanel theLayerTreePanel, GPLegendPanel theLegendPanel) {
         super(true);
         this.layerTreePanel = theLayerTreePanel;
         this.legendPanel = theLegendPanel;
@@ -72,11 +77,8 @@ public class LayerManagementWidget extends GeoPlatformContentPanel {
     public void addComponent() {
         BorderLayoutData northData = new BorderLayoutData(LayoutRegion.NORTH);
         northData.setMargins(new Margins(5, 5, 0, 5));
-
         LayerManagementWidget.super.add(layerTreePanel, northData);
-
-        BorderLayoutData southData = new BorderLayoutData(LayoutRegion.SOUTH,
-                180);
+        BorderLayoutData southData = new BorderLayoutData(LayoutRegion.SOUTH, 180);
         southData.setMargins(new Margins(5, 5, 5, 5));
         if (LayerBeanLayoutFactory.getLayerBeanLayout().getLayerTreePosition()
                 == LayerBeanLayoutFactory.getLayerBeanLayout().getLegendPosition()) {
@@ -118,7 +120,7 @@ public class LayerManagementWidget extends GeoPlatformContentPanel {
         if (LayoutManager.isWidgetPresentOnPanel(panel, w)) {
             LayoutManager.removeComponentFromPanel(panel, w);
             if (!LayoutManager.isOneWidgetVisibleAtPanel(panel)) {
-                LayoutManager.managePanel(panel, false);
+                LayoutManager.managePanel(panel, FALSE);
             }
         }
     }
@@ -132,7 +134,7 @@ public class LayerManagementWidget extends GeoPlatformContentPanel {
         setHeadingHtml(LayerModuleConstants.INSTANCE.LayerManagementWidget_headingText());
         setLayout(new BorderLayout());
 
-        setLayoutOnChange(true);
+        setLayoutOnChange(TRUE);
 
         addWidgetListener(new WidgetListener() {
             @Override

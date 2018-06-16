88343c076bc41ba498bf028a310cc852e31e71ed
Giuseppe La Scaleia <glascaleia@gmail.com>
2018-06-16 17:20:17 +0200

    Upgrade Elasticsearch and Smack

--- geoplatform-experimental/geoplatform-elastic-search/gp-elasticsearch-query-builder/gp-elasticsearch-query-support/gp-elasticsearch-query-mediator/src/main/java/org/geosdi/geoplatform/experimental/el/query/mediator/colleague/config/QueryColleagueRegistryConfig.java
+++ geoplatform-experimental/geoplatform-elastic-search/gp-elasticsearch-query-builder/gp-elasticsearch-query-support/gp-elasticsearch-query-mediator/src/main/java/org/geosdi/geoplatform/experimental/el/query/mediator/colleague/config/QueryColleagueRegistryConfig.java
@@ -1,44 +1,41 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2018 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2018 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.experimental.el.query.mediator.colleague.config;
 
-import com.google.common.base.Preconditions;
-import org.geosdi.geoplatform.experimental.el.index.GPBaseIndexCreator;
+import org.geosdi.geoplatform.experimental.el.index.GPBaseIndexCreator.GPIndexSettings;
 import org.geosdi.geoplatform.experimental.el.query.mediator.colleague.GPElasticSearchQueryColleague;
-import org.geosdi.geoplatform.experimental.el.query.mediator.colleague.decorator.GPElasticSearchQueryColleagueDecorator;
 import org.geosdi.geoplatform.experimental.el.query.mediator.colleague.decorator.IGPElasticSearchQueryColleagueDecorator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,8 +43,12 @@ import org.springframework.context.annotation.Configuration;
 import javax.annotation.Nonnull;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.Objects;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+import static org.geosdi.geoplatform.experimental.el.query.mediator.colleague.decorator.GPElasticSearchQueryColleagueDecorator.of;
 
 /**
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
@@ -56,15 +57,19 @@ import java.util.stream.Collectors;
 @Configuration
 class QueryColleagueRegistryConfig {
 
+    /**
+     * @param queryColleagues
+     * @return {@link Map<GPIndexSettings, IGPElasticSearchQueryColleagueDecorator>}
+     * @throws Exception
+     */
     @Bean
-    public static Map<GPBaseIndexCreator.GPIndexSettings, IGPElasticSearchQueryColleagueDecorator> elasticSearchQueryColleagueRegistry(@Nonnull List<GPElasticSearchQueryColleague> queryColleagues)
+    public static Map<GPIndexSettings, IGPElasticSearchQueryColleagueDecorator> elasticSearchQueryColleagueRegistry(@Nonnull List<GPElasticSearchQueryColleague> queryColleagues)
             throws Exception {
-        Preconditions.checkArgument((queryColleagues != null) && !(queryColleagues.isEmpty()), "Attention " +
+        checkArgument((queryColleagues != null) && !(queryColleagues.isEmpty()), "Attention " +
                 "in classpath there are no instance of ", GPElasticSearchQueryColleague.class.getSimpleName());
         return queryColleagues.stream()
-                .filter(queryColleague -> queryColleague != null)
-                .map(queryColleague -> GPElasticSearchQueryColleagueDecorator.of(queryColleague))
-                .collect(Collectors.toMap(IGPElasticSearchQueryColleagueDecorator::getQueryColleagueKey,
-                        Function.identity()));
+                .filter(Objects::nonNull)
+                .map(queryColleague -> of(queryColleague))
+                .collect(toMap(IGPElasticSearchQueryColleagueDecorator::getQueryColleagueKey, identity()));
     }
 }

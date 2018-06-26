ef88fb41ff09f86535399261f47b6dc967d6563c
Giuseppe La Scaleia <glascaleia@gmail.com>
2018-06-26 12:22:40 +0200

    Improvements

--- geoplatform-tools/geoplatform-persistence-layer/geoplatform-persistence-api/src/main/java/org/geosdi/geoplatform/persistence/dao/jpa/GPAbstractJpaDAO.java
+++ geoplatform-tools/geoplatform-persistence-layer/geoplatform-persistence-api/src/main/java/org/geosdi/geoplatform/persistence/dao/jpa/GPAbstractJpaDAO.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2018 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2018 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.persistence.dao.jpa;
 
@@ -47,12 +46,13 @@ import javax.persistence.criteria.Root;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.StreamSupport;
+import java.util.Objects;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static java.lang.Boolean.FALSE;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.StreamSupport.stream;
 
 /**
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
@@ -105,8 +105,8 @@ public abstract class GPAbstractJpaDAO<T extends Object, ID extends Serializable
     public <S extends T> Collection<S> update(Iterable<T> entities) throws GPDAOException {
         checkNotNull(entities != null, "The Parameter entities must not be null.");
         try {
-            return StreamSupport.stream(entities.spliterator(), FALSE)
-                    .filter(e -> (e != null))
+            return stream(entities.spliterator(), FALSE)
+                    .filter(Objects::nonNull)
                     .map(e -> (S) update(e))
                     .collect(toList());
         } catch (Exception ex) {
@@ -123,8 +123,8 @@ public abstract class GPAbstractJpaDAO<T extends Object, ID extends Serializable
     public Collection<T> persist(Iterable<T> entities) throws GPDAOException {
         checkNotNull(entities != null, "The Parameter entities must not be null.");
         try {
-            return StreamSupport.stream(entities.spliterator(), FALSE)
-                    .filter(e -> (e != null))
+            return stream(entities.spliterator(), FALSE)
+                    .filter(Objects::nonNull)
                     .map(e -> persist(e))
                     .collect(toList());
         } catch (Exception ex) {

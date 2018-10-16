42598b23488efe94adf4b1533d0afba9de7a9700
Giuseppe La Scaleia <glascaleia@gmail.com>
2018-10-16 14:33:12 +0200

    Upgrade Smack. @TODO:Try to delete dependency from Scheduler Module

--- geoplatform-support/geoplatform-xmpp-support/src/main/java/org/geosdi/geoplatform/support/xmpp/spring/connection/config/GPXMPPConnectionManagerConfig.java
+++ geoplatform-support/geoplatform-xmpp-support/src/main/java/org/geosdi/geoplatform/support/xmpp/spring/connection/config/GPXMPPConnectionManagerConfig.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2018 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2018 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.support.xmpp.spring.connection.config;
 
@@ -40,13 +39,11 @@ import org.geosdi.geoplatform.support.xmpp.spring.annotation.GPXMPPConnection;
 import org.geosdi.geoplatform.support.xmpp.spring.connection.manager.GPXMPPConnectionManager;
 import org.geosdi.geoplatform.support.xmpp.spring.connection.manager.XMPPConnectionManager;
 import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
  * @email giuseppe.lascaleia@geosdi.org
  */
@@ -54,14 +51,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 class GPXMPPConnectionManagerConfig {
 
-    @Bean(name = "gpXMPPConnectionManager",
-            initMethod = "initXMPPConnectionManager")
-    @Autowired
-    public XMPPConnectionManager xmppConnectionManager(@Qualifier(
-            value = "xmppConnectionConfiguration") XMPPTCPConnectionConfiguration xmppConnectionConfiguration, @Qualifier(
-                    value = "gpSpringXMPPAuth") XMPPAuth gpSpringXMPPAuth) {
-
+    /**
+     * @param xmppConnectionConfiguration
+     * @param gpSpringXMPPAuth
+     * @return {@link XMPPConnectionManager}
+     */
+    @Bean(name = "gpXMPPConnectionManager", initMethod = "initXMPPConnectionManager")
+    public XMPPConnectionManager xmppConnectionManager(@Qualifier(value = "xmppConnectionConfiguration") XMPPTCPConnectionConfiguration xmppConnectionConfiguration,
+            @Qualifier(value = "gpSpringXMPPAuth") XMPPAuth gpSpringXMPPAuth) {
         return new GPXMPPConnectionManager(xmppConnectionConfiguration, gpSpringXMPPAuth);
     }
-
 }

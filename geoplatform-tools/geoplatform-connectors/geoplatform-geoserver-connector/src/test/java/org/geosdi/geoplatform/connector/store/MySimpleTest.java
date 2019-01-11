e25932018fa2146ec3de8fcf76a70e6d45c47e21
Giuseppe La Scaleia <glascaleia@gmail.com>
2019-01-11 16:12:24 +0100

    Simple Test

---
+++ geoplatform-tools/geoplatform-connectors/geoplatform-geoserver-connector/src/test/java/org/geosdi/geoplatform/connector/store/MySimpleTest.java
@@ -0,0 +1,8 @@
+package org.geosdi.geoplatform.connector.store;
+
+/**
+ * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
+ * @email giuseppe.lascaleia@geosdi.org
+ */
+public class MySimpleTest {
+}

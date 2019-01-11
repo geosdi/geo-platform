7c74dc53b230c8d4f8e3e5b53b2ef8ceeeaf96d8
Giuseppe La Scaleia <glascaleia@gmail.com>
2019-01-11 15:08:54 +0100

    Try to cherry-pick

---
+++ geoplatform-services/geoplatform-ws-base-util/src/main/java/org/geosdi/geoplatform/response/collection/GPGenericMapType.java
@@ -0,0 +1,42 @@
+package org.geosdi.geoplatform.response.collection;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
+ * @email giuseppe.lascaleia@geosdi.org
+ */
+@Getter
+@Setter
+public abstract class GPGenericMapType<K, V, Entry extends GPGenericEntryType<K, V>> implements Serializable {
+
+    private static final long serialVersionUID = 8201121335219233924L;
+    //
+    private List<Entry> entry = new ArrayList<>();
+
+    public GPGenericMapType() {
+    }
+
+    /**
+     * @param map
+     */
+    public GPGenericMapType(Map<K, V> map) {
+        this.entry = map.entrySet()
+                .stream()
+                .map(entry -> toEntry(entry))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * @param entry
+     * @return {@link Entry}
+     */
+    protected abstract Entry toEntry(Map.Entry<K, V> entry);
+}

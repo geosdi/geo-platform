fb03d228ead085b509c8c05fa354d93c40c52c46
Giuseppe La Scaleia <glascaleia@gmail.com>
2019-01-21 14:11:56 +0100

    FIXED Jenkins Build Failure

--- test/geoplatform-jackson-test/src/test/java/org/geosdi/geoplatform/support/jackson/GPJacksonFolderSupportTest.java
+++ test/geoplatform-jackson-test/src/test/java/org/geosdi/geoplatform/support/jackson/GPJacksonFolderSupportTest.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.support.jackson;
 
@@ -41,7 +40,6 @@ import org.geosdi.geoplatform.request.folder.WSAddFolderAndTreeModificationsRequ
 import org.geosdi.geoplatform.request.folder.WSDDFolderAndTreeModifications;
 import org.geosdi.geoplatform.request.folder.WSDeleteFolderAndTreeModifications;
 import org.geosdi.geoplatform.response.collection.ChildrenFolderStore;
-import static org.geosdi.geoplatform.support.jackson.GPBaseJacksonSupportTest.jacksonSupport;
 import org.junit.Test;
 
 /**
@@ -54,12 +52,8 @@ public class GPJacksonFolderSupportTest extends GPBaseJacksonSupportTest {
     @Test
     public void folderDataMapperTest() throws Exception {
         GPFolder folder = jacksonSupport.getDefaultMapper().readValue(
-                Thread.currentThread().getContextClassLoader().getResourceAsStream(
-                        FOLDER_DATA_JSON), GPFolder.class);
-
-        logger.info("\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@FOLDER_DATA_MAPPING"
-                + " : {}\n\n", folder);
-        
+                Thread.currentThread().getContextClassLoader().getResourceAsStream(FOLDER_DATA_JSON), GPFolder.class);
+        logger.info("\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@FOLDER_DATA_MAPPING" + " : {}\n\n", folder);
         super.marshall(folder);
     }
 
@@ -72,7 +66,7 @@ public class GPJacksonFolderSupportTest extends GPBaseJacksonSupportTest {
 
         logger.info("\n\n@@@@@@@@@@@@@@@@@@@@INSERT_FOLDER_REQUEST_DATA_MAPPING"
                 + " : {}\n\n", insertFolderRequest);
-        
+
         super.marshall(insertFolderRequest);
     }
 
@@ -87,7 +81,7 @@ public class GPJacksonFolderSupportTest extends GPBaseJacksonSupportTest {
         logger.info("\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
                 + "ADD_FOLDER_TREE_MODIFICATION_REQUEST_DATA_MAPPING"
                 + " : {}\n\n", addFolderRequest);
-        
+
         super.marshall(addFolderRequest);
     }
 
@@ -102,7 +96,7 @@ public class GPJacksonFolderSupportTest extends GPBaseJacksonSupportTest {
         logger.info("\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
                 + "DELETE_FOLDER_TREE_MODIFICATION_REQUEST_DATA_MAPPING"
                 + " : {}\n\n", deleteFolderRequest);
-        
+
         super.marshall(deleteFolderRequest);
     }
 
@@ -115,7 +109,7 @@ public class GPJacksonFolderSupportTest extends GPBaseJacksonSupportTest {
 
         logger.info("\n\n@@@@@@@@@@@@@@@@@@@@@@GET_CHILDREN_FOLDER_DATA_MAPPING"
                 + " : {}\n\n", folderStore);
-        
+
         super.marshall(folderStore);
     }
 
@@ -130,7 +124,7 @@ public class GPJacksonFolderSupportTest extends GPBaseJacksonSupportTest {
         logger.info("\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
                 + "DD_FOLDER_TREE_MODIFICATION_REQUEST_DATA_MAPPING"
                 + " : {}\n\n", ddFolderRequest);
-        
+
         super.marshall(ddFolderRequest);
     }
 

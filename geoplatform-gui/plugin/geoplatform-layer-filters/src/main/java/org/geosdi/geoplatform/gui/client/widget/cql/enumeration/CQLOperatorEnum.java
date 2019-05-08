ec63e2186df05af0b26bef9aeed46b9260021111
Vito Salvia <vito.salvia@gmail.com>
2019-05-08 16:37:53 +0200

    Fixed Issue #25

--- geoplatform-gui/plugin/geoplatform-layer-filters/src/main/java/org/geosdi/geoplatform/gui/client/widget/cql/enumeration/CQLOperatorEnum.java
+++ geoplatform-gui/plugin/geoplatform-layer-filters/src/main/java/org/geosdi/geoplatform/gui/client/widget/cql/enumeration/CQLOperatorEnum.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.widget.cql.enumeration;
 
@@ -43,17 +42,27 @@ import org.geosdi.geoplatform.gui.client.widget.cql.ICQLBasicOperator;
  * @author Nazzareno Sileno - CNR IMAA geoSDI Group
  * @email nazzareno.sileno@geosdi.org
  */
-public enum CQLOperatorEnum {
+public enum CQLOperatorEnum implements ICQLOperator {
 
     EQUAL("=") {
         @Override
         public ICQLBasicOperator getOperatorInstance() {
             return new CQLSimpleOperator(super.symbol);
         }
+
+        @Override
+        public String getValue() {
+            return "=";
+        }
     }, NOT_EQUAL("<>") {
         @Override
         public ICQLBasicOperator getOperatorInstance() {
             return new CQLSimpleOperator(super.symbol);
+        }
+
+        @Override
+        public String getValue() {
+            return "<>";
         }
     },
     LESS("<") {
@@ -61,10 +70,20 @@ public enum CQLOperatorEnum {
         public ICQLBasicOperator getOperatorInstance() {
             return new CQLSimpleOperator(super.symbol);
         }
+
+        @Override
+        public String getValue() {
+            return "<";
+        }
     }, GREATER(">") {
         @Override
         public ICQLBasicOperator getOperatorInstance() {
             return new CQLSimpleOperator(super.symbol);
+        }
+
+        @Override
+        public String getValue() {
+            return ">";
         }
     },
     LESS_OR_EQUAL("<=") {
@@ -72,17 +91,32 @@ public enum CQLOperatorEnum {
         public ICQLBasicOperator getOperatorInstance() {
             return new CQLSimpleOperator(super.symbol);
         }
+
+        @Override
+        public String getValue() {
+            return "<=";
+        }
     },
     GREATER_OR_EQUAL(">=") {
         @Override
         public ICQLBasicOperator getOperatorInstance() {
             return new CQLSimpleOperator(super.symbol);
         }
+
+        @Override
+        public String getValue() {
+            return ">=";
+        }
     },
     LIKE("like") {
         @Override
         public ICQLBasicOperator getOperatorInstance() {
             return new CQLStringOperator(super.symbol);
+        }
+
+        @Override
+        public String getValue() {
+            return "like";
         }
     },
     STARTS_WITH("starts with") {
@@ -95,6 +129,11 @@ public enum CQLOperatorEnum {
                 }
             };
         }
+
+        @Override
+        public String getValue() {
+            return "like";
+        }
     },
     ENDS_WITH("ends with") {
         @Override
@@ -105,6 +144,11 @@ public enum CQLOperatorEnum {
                     return super.generateExpression(featureAttribute, "%" + conditionValue);
                 }
             };
+        }
+
+        @Override
+        public String getValue() {
+            return "like";
         }
     },
     CONTAINS("contains") {
@@ -117,14 +161,21 @@ public enum CQLOperatorEnum {
                 }
             };
         }
+
+        @Override
+        public String getValue() {
+            return "like";
+        }
     };
     private String symbol;
 
-    private CQLOperatorEnum(String symbol) {
+    CQLOperatorEnum(String symbol) {
         this.symbol = symbol;
     }
 
     public abstract ICQLBasicOperator getOperatorInstance();
+
+    public abstract String getValue();
 
     @Override
     public String toString() {

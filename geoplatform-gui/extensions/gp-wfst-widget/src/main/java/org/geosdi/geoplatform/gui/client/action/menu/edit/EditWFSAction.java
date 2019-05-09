3bde6a9ca40c508601da54297519033294a94595
Vito Salvia <vito.salvia@gmail.com>
2019-05-09 16:37:56 +0200

    Fixed Issue #22

--- geoplatform-gui/extensions/gp-wfst-widget/src/main/java/org/geosdi/geoplatform/gui/client/action/menu/edit/EditWFSAction.java
+++ geoplatform-gui/extensions/gp-wfst-widget/src/main/java/org/geosdi/geoplatform/gui/client/action/menu/edit/EditWFSAction.java
@@ -1,56 +1,63 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.action.menu.edit;
 
 import com.extjs.gxt.ui.client.event.MenuEvent;
 import com.extjs.gxt.ui.client.widget.treepanel.TreePanel;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import org.geosdi.geoplatform.gui.action.menu.MenuBaseAction;
 import org.geosdi.geoplatform.gui.client.BasicWidgetResources;
 import org.geosdi.geoplatform.gui.client.action.menu.edit.responsibility.LayerTypeHandlerManager;
 import org.geosdi.geoplatform.gui.client.action.menu.strategy.IActionStrategy;
+import org.geosdi.geoplatform.gui.client.command.datasource.CheckDataSourceRequest;
+import org.geosdi.geoplatform.gui.client.command.datasource.CheckDataSourceResponse;
 import org.geosdi.geoplatform.gui.client.config.FeatureInjector;
+import org.geosdi.geoplatform.gui.client.i18n.BasicWidgetModuleMessages;
 import org.geosdi.geoplatform.gui.client.i18n.WFSTWidgetConstants;
 import org.geosdi.geoplatform.gui.client.puregwt.action.IEditWFSActionHandler;
 import org.geosdi.geoplatform.gui.client.puregwt.map.initializer.event.BindLayersEvent;
 import org.geosdi.geoplatform.gui.client.puregwt.wfs.event.BuildLayerTreeEvent;
 import org.geosdi.geoplatform.gui.client.widget.tree.GPTreePanel;
+import org.geosdi.geoplatform.gui.command.api.ClientCommandDispatcher;
+import org.geosdi.geoplatform.gui.command.api.GPClientCommand;
+import org.geosdi.geoplatform.gui.configuration.message.GeoPlatformMessage;
 import org.geosdi.geoplatform.gui.impl.view.LayoutManager;
 import org.geosdi.geoplatform.gui.model.GPLayerBean;
 import org.geosdi.geoplatform.gui.model.tree.GPBeanTreeModel;
+import org.geosdi.geoplatform.gui.model.tree.GPLayerTreeModel;
 import org.geosdi.geoplatform.gui.puregwt.GPEventBus;
 
 import java.util.Collections;
@@ -69,6 +76,7 @@ public class EditWFSAction extends MenuBaseAction implements IEditWFSAction, IEd
     private final LayerTypeHandlerManager layerTypeHandlerManager;
     private final GPEventBus bus;
     private final BindLayersEvent bindLayersEvent = new BindLayersEvent();
+    private final CheckDataSourceRequest checkDataSourceRequest = GWT.<CheckDataSourceRequest>create(CheckDataSourceRequest.class);
     private IActionStrategy actionStrategy;
     private GPLayerBean layer;
 
@@ -87,10 +95,32 @@ public class EditWFSAction extends MenuBaseAction implements IEditWFSAction, IEd
 
     @Override
     public void componentSelected(MenuEvent e) {
-        this.actionStrategy.setWidgetType(IActionStrategy.WidgetType.EDIT_WFS_ACTION);
-        this.layer = (GPLayerBean) this.treePanel.getSelectionModel().getSelectedItem();
-        LayoutManager.getInstance().getStatusMap().setBusy(INSTANCE.checkingIfLayerIsAVectorMessage(layer.getName()));
-        this.layerTypeHandlerManager.forwardLayerType(layer);
+        GPBeanTreeModel itemSelected = this.treePanel.getSelectionModel().getSelectedItem();
+        this.checkDataSourceRequest.setDatasource(((GPLayerTreeModel)itemSelected).getDataSource());
+
+        ClientCommandDispatcher.getInstance().execute(new GPClientCommand<CheckDataSourceResponse>() {
+            {
+                super.setCommandRequest(checkDataSourceRequest);
+            }
+
+            @Override
+            public void onCommandSuccess(final CheckDataSourceResponse response) {
+                if (response.getResult()) {
+                    actionStrategy.setWidgetType(IActionStrategy.WidgetType.EDIT_WFS_ACTION);
+                    layer = (GPLayerBean) treePanel.getSelectionModel().getSelectedItem();
+                    LayoutManager.getInstance().getStatusMap().setBusy(INSTANCE.checkingIfLayerIsAVectorMessage(layer.getName()));
+                    layerTypeHandlerManager.forwardLayerType(layer);
+                } else
+                    GeoPlatformMessage.errorMessage(BasicWidgetModuleMessages.INSTANCE.errorDataSource(),
+                            BasicWidgetModuleMessages.INSTANCE.datasourceNotMatches());
+            }
+
+            @Override
+            public void onCommandFailure(Throwable exception) {
+                GeoPlatformMessage.errorMessage(BasicWidgetModuleMessages.INSTANCE.errorDataSource(), exception.getMessage());
+            }
+        });
+
     }
 
     @Override

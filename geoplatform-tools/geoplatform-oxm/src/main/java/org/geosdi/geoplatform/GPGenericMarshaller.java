1d89610c35ec8d4632a379e0a43979abefed9552
Giuseppe La Scaleia <glascaleia@gmail.com>
2019-05-20 10:44:59 +0200

    Added Support for JIBXBindingContext to use in Styler-Module

--- geoplatform-tools/geoplatform-oxm/src/main/java/org/geosdi/geoplatform/GPGenericMarshaller.java
+++ geoplatform-tools/geoplatform-oxm/src/main/java/org/geosdi/geoplatform/GPGenericMarshaller.java
@@ -1,45 +1,44 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.Reader;
+import org.geosdi.geoplatform.oxm.GPMarshaller;
+import org.geosdi.geoplatform.oxm.GPUnmarshaller;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.oxm.support.AbstractMarshaller;
+
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.sax.SAXResult;
@@ -47,69 +46,54 @@ import javax.xml.transform.sax.SAXSource;
 import javax.xml.transform.stax.StAXResult;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import org.geosdi.geoplatform.oxm.GPMarshaller;
-import org.geosdi.geoplatform.oxm.GPUnmarshaller;
-import org.springframework.beans.factory.InitializingBean;
-import org.springframework.oxm.support.AbstractMarshaller;
+import java.io.*;
 
 /**
- *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
  * @email giuseppe.lascaleia@geosdi.org
  */
-public abstract class GPGenericMarshaller<M extends AbstractMarshaller>
-        implements GPMarshaller, GPUnmarshaller, InitializingBean {
+public abstract class GPGenericMarshaller<M extends AbstractMarshaller> implements GPMarshaller, GPUnmarshaller, InitializingBean {
 
     protected M marshaller;
 
     @Override
-    public void marshal(Object jaxbElement, java.io.OutputStream os)
-            throws IOException {
+    public void marshal(Object jaxbElement, java.io.OutputStream os) throws IOException {
         this.marshaller.marshal(jaxbElement, new StreamResult(os));
     }
 
     @Override
     public void marshal(Object jaxbElement, File output) throws IOException {
-        this.marshaller.marshal(jaxbElement, new StreamResult(
-                new FileOutputStream(output)));
+        this.marshaller.marshal(jaxbElement, new StreamResult(new FileOutputStream(output)));
     }
 
     @Override
-    public void marshal(Object jaxbElement, java.io.Writer writer)
-            throws IOException {
+    public void marshal(Object jaxbElement, java.io.Writer writer) throws IOException {
         this.marshaller.marshal(jaxbElement, new StreamResult(writer));
     }
 
     @Override
-    public void marshal(Object jaxbElement, org.xml.sax.ContentHandler handler)
-            throws IOException {
+    public void marshal(Object jaxbElement, org.xml.sax.ContentHandler handler) throws IOException {
         this.marshaller.marshal(jaxbElement, new SAXResult(handler));
     }
 
     @Override
-    public void marshal(Object jaxbElement, org.w3c.dom.Node node)
-            throws IOException {
+    public void marshal(Object jaxbElement, org.w3c.dom.Node node) throws IOException {
         this.marshaller.marshal(jaxbElement, new DOMResult(node));
     }
 
     @Override
-    public void marshal(Object jaxbElement,
-            javax.xml.stream.XMLStreamWriter writer)
-            throws IOException {
+    public void marshal(Object jaxbElement, javax.xml.stream.XMLStreamWriter writer) throws IOException {
         this.marshaller.marshal(jaxbElement, new StAXResult(writer));
     }
 
     @Override
-    public void marshal(Object jaxbElement,
-            javax.xml.stream.XMLEventWriter writer)
-            throws IOException {
+    public void marshal(Object jaxbElement, javax.xml.stream.XMLEventWriter writer) throws IOException {
         this.marshaller.marshal(jaxbElement, new StAXResult(writer));
     }
 
     @Override
     public Object unmarshal(java.io.File f) throws IOException {
-        return this.marshaller.unmarshal(
-                new StreamSource(new FileInputStream(f)));
+        return this.marshaller.unmarshal(new StreamSource(new FileInputStream(f)));
     }
 
     @Override
@@ -138,7 +122,7 @@ public abstract class GPGenericMarshaller<M extends AbstractMarshaller>
     }
 
     public abstract void setMarshaller(M theMarshaller);
-    
+
     public M getMarshaller() {
         return this.marshaller;
     }
@@ -146,9 +130,7 @@ public abstract class GPGenericMarshaller<M extends AbstractMarshaller>
     @Override
     public void afterPropertiesSet() throws Exception {
         if (marshaller == null) {
-            throw new IllegalArgumentException("The property marshaller "
-                    + "must not be null.");
+            throw new IllegalArgumentException("The property marshaller must not be null.");
         }
     }
-
 }
--- test/geoplatform-test-utils/geoplatform-oxm-model-test/src/main/java/org/geosdi/geoplatform/mock/ClassToXMLMap.java
+++ test/geoplatform-test-utils/geoplatform-oxm-model-test/src/main/java/org/geosdi/geoplatform/mock/ClassToXMLMap.java
@@ -1,91 +1,60 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.mock;
 
-import javax.xml.bind.annotation.XmlRootElement;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
 
 /**
- *
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
  * @email giuseppe.lascaleia@geosdi.org
  */
+@Getter
+@Setter
+@ToString
 @XmlRootElement(name = "ClassToXMLMap")
 @XStreamAlias("ClassToXMLMap")
-public class ClassToXMLMap {
+public class ClassToXMLMap implements Serializable {
 
+    private static final long serialVersionUID = 1265951453846922406L;
+    //
     private String data;
     private String history;
-
-    /**
-     * @return the data
-     */
-    public String getData() {
-        return data;
-    }
-
-    /**
-     * @param data the data to set
-     */
-    public void setData(String data) {
-        this.data = data;
-    }
-
-    /**
-     * @return the history
-     */
-    public String getHistory() {
-        return history;
-    }
-
-    /**
-     * @param history the history to set
-     */
-    public void setHistory(String history) {
-        this.history = history;
-    }
-
-    /**
-     * (non-Javadoc)
-     *
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-        return "ClassToXMLMap [data=" + data + ", history=" + history + "]";
-    }
-
 }

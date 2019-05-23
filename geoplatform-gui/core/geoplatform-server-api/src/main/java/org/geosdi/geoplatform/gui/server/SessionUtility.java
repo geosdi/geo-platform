bfc237e01d343e2a60343ccc2f75a39681610ff2
Giuseppe La Scaleia <glascaleia@gmail.com>
2019-05-23 16:04:14 +0200

    Try to set Session Expiration to -1

--- geoplatform-gui/core/geoplatform-server-api/src/main/java/org/geosdi/geoplatform/gui/server/SessionUtility.java
+++ geoplatform-gui/core/geoplatform-server-api/src/main/java/org/geosdi/geoplatform/gui/server/SessionUtility.java
@@ -1,45 +1,47 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.server;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import org.geosdi.geoplatform.core.model.GPAccount;
 import org.geosdi.geoplatform.gui.utility.GPSessionTimeout;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author Nazzareno Sileno - CNR IMAA geoSDI Group
@@ -49,59 +51,79 @@ import org.springframework.stereotype.Service;
 public class SessionUtility {
 
     private static final int SESSIONE_EXPIRATION = 86400;
+    //
+    private static final Logger logger = LoggerFactory.getLogger(SessionUtility.class);
 
     public enum SessionProperty {
 
         LOGGED_ACCOUNT, DEFAULT_PROJECT;
     }
 
-    public Long getDefaultProject(HttpServletRequest httpServletRequest)
-            throws GPSessionTimeout {
+    /**
+     * @param httpServletRequest
+     * @return {@link Long}
+     * @throws GPSessionTimeout
+     */
+    public Long getDefaultProject(HttpServletRequest httpServletRequest) throws GPSessionTimeout {
         HttpSession session = httpServletRequest.getSession();
         Long projectId = (Long) session.getAttribute(SessionProperty.DEFAULT_PROJECT.toString());
         if (projectId != null) {
             return projectId;
         } else {
-            System.out.println("\n*** Session Project ID null ***");
+            logger.info("\n*** Session Project ID null ***");
             throw new GPSessionTimeout("Session Timeout");
         }
     }
 
-    public GPAccount getLoggedAccount(HttpServletRequest httpServletRequest)
-            throws GPSessionTimeout {
+    /**
+     * @param httpServletRequest
+     * @return {@link GPAccount}
+     * @throws GPSessionTimeout
+     */
+    public GPAccount getLoggedAccount(HttpServletRequest httpServletRequest) throws GPSessionTimeout {
         GPAccount account = null;
         HttpSession session = httpServletRequest.getSession();
         Object accountObj = session.getAttribute(SessionProperty.LOGGED_ACCOUNT.toString());
         if (accountObj != null && accountObj instanceof GPAccount) {
             account = (GPAccount) accountObj;
         } else {
-            System.out.println("\n*** Session Account null ***");
+            logger.info("\n*** Session Account null ***");
             throw new GPSessionTimeout("Session Timeout");
         }
         return account;
     }
 
-    public void storeLoggedAccount(GPAccount account,
-            HttpServletRequest httpServletRequest) {
+    /**
+     * @param account
+     * @param httpServletRequest
+     */
+    public void storeLoggedAccount(GPAccount account, HttpServletRequest httpServletRequest) {
         HttpSession session = httpServletRequest.getSession();
         //TODO: Set the right time in seconds before session interrupt
-        session.setMaxInactiveInterval(SESSIONE_EXPIRATION);
+        session.setMaxInactiveInterval(-1);
         session.setAttribute(SessionProperty.LOGGED_ACCOUNT.toString(), account);
     }
 
-    public void storeDefaultProject(Long projectID,
-            HttpServletRequest httpServletRequest) {
+    /**
+     * @param projectID
+     * @param httpServletRequest
+     */
+    public void storeDefaultProject(Long projectID, HttpServletRequest httpServletRequest) {
         HttpSession session = httpServletRequest.getSession();
         //TODO: Set the right time in seconds before session interrupt
         session.setMaxInactiveInterval(SESSIONE_EXPIRATION);
         session.setAttribute(SessionProperty.DEFAULT_PROJECT.toString(), projectID);
     }
 
-    public void storeLoggedAccountAndDefaultProject(GPAccount account,
-            Long projectID, HttpServletRequest httpServletRequest) {
+    /**
+     * @param account
+     * @param projectID
+     * @param httpServletRequest
+     */
+    public void storeLoggedAccountAndDefaultProject(GPAccount account, Long projectID, HttpServletRequest httpServletRequest) {
         HttpSession session = httpServletRequest.getSession();
         //TODO: Set the right time in seconds before session interrupt
-        session.setMaxInactiveInterval(SESSIONE_EXPIRATION);
+        session.setMaxInactiveInterval(-1);
         session.setAttribute(SessionProperty.LOGGED_ACCOUNT.toString(), account);
         session.setAttribute(SessionProperty.DEFAULT_PROJECT.toString(), projectID);
     }
--- geoplatform-gui/core/geoplatform-widget/security-widget/src/main/java/org/geosdi/geoplatform/gui/server/service/impl/SecurityService.java
+++ geoplatform-gui/core/geoplatform-widget/security-widget/src/main/java/org/geosdi/geoplatform/gui/server/service/impl/SecurityService.java
@@ -1,47 +1,40 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.server.service.impl;
 
 import com.google.common.collect.Lists;
-import java.util.Date;
-import java.util.List;
-import java.util.StringTokenizer;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.xml.ws.soap.SOAPFaultException;
 import org.geosdi.geoplatform.core.model.*;
 import org.geosdi.geoplatform.exception.AccountLoginFault;
 import org.geosdi.geoplatform.exception.IllegalParameterFault;
@@ -70,6 +63,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.xml.ws.soap.SOAPFaultException;
+import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * @author Nazzareno Sileno - CNR IMAA geoSDI Group
@@ -117,10 +117,10 @@ public class SecurityService implements ISecurityService {
         } catch (ResourceNotFoundFault ex) {
             logger.error("SecurityService",
                     "Unable to find user with username or email: " + username
-                    + " Error: " + ex);
+                            + " Error: " + ex);
             throw new GeoPlatformException(
                     "Unable to find user with username or email: "
-                    + username);
+                            + username);
         } catch (SOAPFaultException ex) {
             logger.error(
                     "Error on SecurityService: " + ex + " password incorrect");
@@ -153,10 +153,10 @@ public class SecurityService implements ISecurityService {
             } catch (ResourceNotFoundFault ex) {
                 logger.error("SecurityService",
                         "Unable to find user with username: "
-                        + userName + " Error: " + ex);
+                                + userName + " Error: " + ex);
                 throw new GeoPlatformException(
                         "Unable to find user with username: "
-                        + userName);
+                                + userName);
             }
         }
         return xMPPLoginDetails;
@@ -179,10 +179,10 @@ public class SecurityService implements ISecurityService {
             } catch (ResourceNotFoundFault ex) {
                 logger.error("SecurityService",
                         "Unable to find user with username or email: " + ivUser
-                        + " Error: " + ex);
+                                + " Error: " + ex);
                 throw new GeoPlatformException(
                         "Unable to find user with username or email: "
-                        + ivUser);
+                                + ivUser);
             } catch (SOAPFaultException ex) {
                 logger.error(
                         "Error on SecurityService: " + ex + " password incorrect");
@@ -213,7 +213,7 @@ public class SecurityService implements ISecurityService {
             } catch (ResourceNotFoundFault ex) {
                 logger.info("SecurityService",
                         "Unable to find user with username or email: " + casUserName
-                        + " Error: " + ex);
+                                + " Error: " + ex);
                 accountDetail = this.createNewCASUser(casUserName,
                         httpServletRequest);
             } catch (SOAPFaultException ex) {
@@ -244,7 +244,7 @@ public class SecurityService implements ISecurityService {
         } catch (IllegalParameterFault ex) {
             logger.error("SecurityService",
                     "Unable to send email to: " + this.casAdminEmails
-                    + " after new cas user creation. Error: " + ex);
+                            + " after new cas user creation. Error: " + ex);
         }
         return this.casLogin(httpServletRequest);
     }
@@ -264,9 +264,7 @@ public class SecurityService implements ISecurityService {
         return userToReturn;
     }
 
-    private IGPAccountDetail executeLoginOnGPAccount(GPAccount account,
-            GuiComponentsPermissionMapData guiComponentPermission, Long projectID,
-            HttpServletRequest httpServletRequest)
+    private IGPAccountDetail executeLoginOnGPAccount(GPAccount account, GuiComponentsPermissionMapData guiComponentPermission, Long projectID, HttpServletRequest httpServletRequest)
             throws ResourceNotFoundFault, SOAPFaultException {
         logger.info("Account id: " + account.getId());
         GPAccountProject accountProject = null;
@@ -274,8 +272,7 @@ public class SecurityService implements ISecurityService {
             accountProject = geoPlatformServiceClient.getDefaultAccountProject(
                     account.getId());
         } else {
-            accountProject = geoPlatformServiceClient.
-                    getAccountProjectByAccountAndProjectIDs(account.getId(), projectID);
+            accountProject = geoPlatformServiceClient.getAccountProjectByAccountAndProjectIDs(account.getId(), projectID);
         }
         logger.info("Account project: " + accountProject);
         GPProject project;
@@ -287,38 +284,29 @@ public class SecurityService implements ISecurityService {
             project.setId(this.saveDefaultProject(account, project));
         } else {
             project = accountProject.getProject();
-            viewport = geoPlatformServiceClient.getDefaultViewport(
-                    accountProject.getId());
+            viewport = geoPlatformServiceClient.getDefaultViewport(accountProject.getId());
         }
-        this.sessionUtility.storeLoggedAccountAndDefaultProject(account,
-                project.getId(), httpServletRequest);
-        List<GPMessage> unreadMessages = geoPlatformServiceClient.getUnreadMessagesByRecipient(
-                account.getId()).getMessages();
-        IGPAccountDetail userDetail = this.dtoConverter.convertAccountToDTO(
-                account, accountProject, viewport, unreadMessages);
-        userDetail.setComponentPermission(
-                guiComponentPermission.getPermissionMap());
+        this.sessionUtility.storeLoggedAccountAndDefaultProject(account, project.getId(), httpServletRequest);
+        List<GPMessage> unreadMessages = geoPlatformServiceClient.getUnreadMessagesByRecipient(account.getId()).getMessages();
+        IGPAccountDetail userDetail = this.dtoConverter.convertAccountToDTO(account, accountProject, viewport, unreadMessages);
+        userDetail.setComponentPermission(guiComponentPermission.getPermissionMap());
         return userDetail;
     }
 
     @Override
-    public IGPAccountDetail applicationLogin(String appID,
-            HttpServletRequest httpServletRequest)
-            throws GeoPlatformException {
+    public IGPAccountDetail applicationLogin(String appID, HttpServletRequest httpServletRequest) throws GeoPlatformException {
         GPApplication application;
         try {
             application = geoPlatformServiceClient.getApplication(appID);
             return this.executeLoginOnGPAccount(application,
-                    geoPlatformServiceClient.getApplicationPermission(
-                            application.getAppID()), null,
-                    httpServletRequest);
+                    geoPlatformServiceClient.getApplicationPermission(application.getAppID()), null, httpServletRequest);
         } catch (ResourceNotFoundFault ex) {
             logger.error("SecurityService",
                     "Unable to find application with appID: " + appID
-                    + " Error: " + ex);
+                            + " Error: " + ex);
             throw new GeoPlatformException(
                     "Unable to find application with appID: "
-                    + appID);
+                            + appID);
         } catch (AccountLoginFault ex) {
             logger.error("Error on SecurityService: " + ex);
             throw new GeoPlatformException(

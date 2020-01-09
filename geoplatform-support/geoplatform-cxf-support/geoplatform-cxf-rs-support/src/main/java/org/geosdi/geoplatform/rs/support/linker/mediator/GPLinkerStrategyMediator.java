62cc14cc15e26c64c6dd2b422f29598567f80276
Giuseppe La Scaleia <glascaleia@gmail.com>
2020-01-09 14:07:19 +0100

    Added Support for Basic GPBaseLinkerStrategyMediator

--- geoplatform-support/geoplatform-cxf-support/geoplatform-cxf-rs-support/src/main/java/org/geosdi/geoplatform/rs/support/linker/mediator/GPLinkerStrategyMediator.java
+++ geoplatform-support/geoplatform-cxf-support/geoplatform-cxf-rs-support/src/main/java/org/geosdi/geoplatform/rs/support/linker/mediator/GPLinkerStrategyMediator.java
@@ -1,9 +1,11 @@
 package org.geosdi.geoplatform.rs.support.linker.mediator;
 
+import org.geosdi.geoplatform.rs.support.linker.GPLinkerStrategy;
 import org.geosdi.geoplatform.rs.support.request.linker.GPBaseLinkerRequest;
 import org.geosdi.geoplatform.rs.support.response.GPStatusResponse;
 
 import javax.annotation.Nonnull;
+import java.util.Map;
 
 import static javax.annotation.meta.When.NEVER;
 
@@ -20,4 +22,11 @@ public interface GPLinkerStrategyMediator<Request extends GPBaseLinkerRequest> {
      * @throws Exception
      */
     <Status extends GPStatusResponse> Status link(@Nonnull(when = NEVER) Request theRequest) throws Exception;
+
+    /**
+     * @param theRequest
+     * @return {@link Map<GPLinkerStrategy.IGPLinkerStrategyKey, GPLinkerStrategy>}
+     * @throws Exception
+     */
+    Map<GPLinkerStrategy.IGPLinkerStrategyKey, GPLinkerStrategy> findStrategies(@Nonnull(when = NEVER) Request theRequest) throws Exception;
 }

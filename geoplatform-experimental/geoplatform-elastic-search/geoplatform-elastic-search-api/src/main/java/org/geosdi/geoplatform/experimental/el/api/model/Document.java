0dc60027068039d4e2723dc29d0d5d8d148022ab
Giuseppe La Scaleia <glascaleia@gmail.com>
2020-01-10 09:35:29 +0100

    Removed @JsonIgnore from getId() for Document

--- geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-api/src/main/java/org/geosdi/geoplatform/experimental/el/api/model/Document.java
+++ geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-api/src/main/java/org/geosdi/geoplatform/experimental/el/api/model/Document.java
@@ -47,7 +47,6 @@ public interface Document extends Serializable {
     /**
      * @return {@link String} ElasticSearch ID
      */
-    @JsonIgnore
     String getId();
 
     /**
--- geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-api/src/main/java/org/geosdi/geoplatform/experimental/el/api/model/GPElasticSearchDocument.java
+++ geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-api/src/main/java/org/geosdi/geoplatform/experimental/el/api/model/GPElasticSearchDocument.java
@@ -34,7 +34,6 @@
  */
 package org.geosdi.geoplatform.experimental.el.api.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -50,6 +49,5 @@ public abstract class GPElasticSearchDocument implements Document {
 
     private static final long serialVersionUID = 3295273925910743927L;
     //
-    @JsonIgnore
     private String id;
 }

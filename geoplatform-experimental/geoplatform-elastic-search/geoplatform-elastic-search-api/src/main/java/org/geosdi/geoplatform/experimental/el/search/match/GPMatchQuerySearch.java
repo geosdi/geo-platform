bea7eab599f9e089bcbc02bd692255fdb11c3bfc
Giuseppe La Scaleia <glascaleia@gmail.com>
2020-03-04 09:54:40 +0100

    Improvements

--- geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-api/src/main/java/org/geosdi/geoplatform/experimental/el/search/match/GPMatchQuerySearch.java
+++ geoplatform-experimental/geoplatform-elastic-search/geoplatform-elastic-search-api/src/main/java/org/geosdi/geoplatform/experimental/el/search/match/GPMatchQuerySearch.java
@@ -1,11 +1,14 @@
 package org.geosdi.geoplatform.experimental.el.search.match;
 
+import org.elasticsearch.index.query.Operator;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.geosdi.geoplatform.experimental.el.search.bool.IBooleanSearch;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import static javax.annotation.meta.When.NEVER;
+import static org.elasticsearch.index.query.Operator.OR;
 import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 
 /**
@@ -14,13 +17,17 @@ import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
  */
 public class GPMatchQuerySearch extends IBooleanSearch.AbstractBooleanSearch {
 
+    private final Operator operator;
+
     /**
      * @param theField
      * @param theValue
      * @param theType
      */
-    public GPMatchQuerySearch(@Nonnull(when = NEVER) String theField, @Nonnull(when = NEVER) Object theValue, @Nonnull(when = NEVER) BooleanQueryType theType) {
+    public GPMatchQuerySearch(@Nonnull(when = NEVER) String theField, @Nonnull(when = NEVER) Object theValue,
+            @Nonnull(when = NEVER) BooleanQueryType theType, @Nullable Operator theOperator) {
         super(theField, theValue, theType);
+        this.operator = ((theOperator != null) ? theOperator : OR);
     }
 
     /**
@@ -28,6 +35,6 @@ public class GPMatchQuerySearch extends IBooleanSearch.AbstractBooleanSearch {
      */
     @Override
     public QueryBuilder buildQuery() {
-        return matchQuery(field, value);
+        return matchQuery(field, value).operator(this.operator);
     }
 }

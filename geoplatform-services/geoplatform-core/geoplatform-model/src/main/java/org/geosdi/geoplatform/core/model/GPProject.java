c4d50ab1696a88d7d87f2cdf7e1ec0b4da1718b3
Giuseppe La Scaleia <glascaleia@gmail.com>
2020-04-20 11:13:01 +0200

    Added two properties for GPProject. @TODO Change Rest , Soap part for DTO.

--- geoplatform-services/geoplatform-core/geoplatform-model/src/main/java/org/geosdi/geoplatform/core/model/GPProject.java
+++ geoplatform-services/geoplatform-core/geoplatform-model/src/main/java/org/geosdi/geoplatform/core/model/GPProject.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2020 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.core.model;
 
@@ -48,7 +47,6 @@ import java.util.Date;
 /**
  * @author Vincenzo Monteverde <vincenzo.monteverde@geosdi.org>
  */
-//@XmlRootElement(name = "GPProject")
 @XmlAccessorType(XmlAccessType.FIELD)
 @Entity(name = "Project")
 @Table(name = "gp_project")
@@ -61,8 +59,7 @@ public class GPProject implements Serializable {
     private static final long serialVersionUID = 8397860970222813277L;
     //
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "GP_PROJECT_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "GP_PROJECT_SEQ")
     @SequenceGenerator(name = "GP_PROJECT_SEQ", sequenceName = "GP_PROJECT_SEQ")
     private Long id;
     //
@@ -89,6 +86,10 @@ public class GPProject implements Serializable {
     //
     @Column(name = "image_project_path")
     private String imagePath;
+    @Column(name = "internal_public")
+    private boolean internalPublic;
+    @Column(name = "external_public")
+    private boolean externalPublic;
 
     /**
      * @return the id
@@ -210,22 +211,51 @@ public class GPProject implements Serializable {
     }
 
     /**
+     * @return {@link Boolean}
+     */
+    public boolean isInternalPublic() {
+        return internalPublic;
+    }
+
+    /**
+     * @param internalPublic
+     */
+    public void setInternalPublic(boolean internalPublic) {
+        this.internalPublic = internalPublic;
+    }
+
+    /**
+     * @return {@link Boolean}
+     */
+    public boolean isExternalPublic() {
+        return externalPublic;
+    }
+
+    /**
+     * @param externalPublic
+     */
+    public void setExternalPublic(boolean externalPublic) {
+        this.externalPublic = externalPublic;
+    }
+
+    /**
      * (non-Javadoc)
      *
      * @see java.lang.Object#toString()
      */
     @Override
     public String toString() {
-        StringBuilder str = new StringBuilder(this.getClass().getSimpleName()).append(
-                " {");
-        str.append("id=").append(id);
-        str.append(", name=").append(name);
-        str.append(", numberOfElements=").append(numberOfElements);
-        str.append(", creationDate=").append(creationDate);
-        str.append(", version=").append(version);
-        str.append(", shared=").append(shared);
-        str.append(", description=").append(description);
-        str.append(", imagePath=").append(imagePath);
+        StringBuilder str = new StringBuilder(this.getClass().getSimpleName()).append(" {");
+        str.append("id = ").append(id);
+        str.append(", name = ").append(name);
+        str.append(", numberOfElements = ").append(numberOfElements);
+        str.append(", creationDate = ").append(creationDate);
+        str.append(", version = ").append(version);
+        str.append(", shared = ").append(shared);
+        str.append(", description = ").append(description);
+        str.append(", imagePath = ").append(imagePath);
+        str.append(", internalPublic = ").append(internalPublic);
+        str.append(", externalPublic = ").append(externalPublic);
         return str.append('}').toString();
     }
 }

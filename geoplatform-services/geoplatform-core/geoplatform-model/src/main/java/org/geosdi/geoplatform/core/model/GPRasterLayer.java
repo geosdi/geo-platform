3229ff7e97ac0cf64424a45db5c0020d25bc5647
Giuseppe La Scaleia <glascaleia@gmail.com>
2020-05-14 18:43:01 +0200

    Change Model to have Temporal Extension

--- geoplatform-services/geoplatform-core/geoplatform-model/src/main/java/org/geosdi/geoplatform/core/model/GPRasterLayer.java
+++ geoplatform-services/geoplatform-core/geoplatform-model/src/main/java/org/geosdi/geoplatform/core/model/GPRasterLayer.java
@@ -1,53 +1,54 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2020 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2020 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.core.model;
 
-import org.geosdi.geoplatform.gui.shared.GPLayerType;
+import org.geosdi.geoplatform.core.model.temporal.GPTemporalLayer;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.joining;
 import static javax.xml.bind.annotation.XmlAccessType.FIELD;
+import static org.geosdi.geoplatform.gui.shared.GPLayerType.WMS;
 
 /**
  * @author Francesco Izzi - CNR IMAA - geoSDI
@@ -55,9 +56,7 @@ import static javax.xml.bind.annotation.XmlAccessType.FIELD;
 @XmlRootElement(name = "RasterLayer")
 @XmlAccessorType(FIELD)
 @Entity(name = "RasterLayer")
-@Table(name = "gp_raster_layer", indexes = {
-        @Index(columnList = "project_id", name = "RASTER_PROJECT_ID_INDEX")
-})
+@Table(name = "gp_raster_layer", indexes = {@Index(columnList = "project_id", name = "RASTER_PROJECT_ID_INDEX")})
 public class GPRasterLayer extends GPLayer {
 
     /**
@@ -79,6 +78,8 @@ public class GPRasterLayer extends GPLayer {
     //
     @Column(name = "min_scale")
     private Float minScale;
+    @Embedded
+    private GPTemporalLayer temporalLayer;
     //
     @ManyToOne(optional = true)
     @OnDelete(action = OnDeleteAction.CASCADE)
@@ -89,7 +90,7 @@ public class GPRasterLayer extends GPLayer {
     private GPProject project;
 
     public GPRasterLayer() {
-        super.setLayerType(GPLayerType.WMS);
+        super.setLayerType(WMS);
     }
 
     /**
@@ -166,6 +167,28 @@ public class GPRasterLayer extends GPLayer {
     }
 
     /**
+     * @return {@link GPTemporalLayer}
+     */
+    public GPTemporalLayer getTemporalLayer() {
+        return temporalLayer;
+    }
+
+    /**
+     * @param theTemporalLayer
+     */
+    public void setTemporalLayer(GPTemporalLayer theTemporalLayer) {
+        this.temporalLayer = theTemporalLayer;
+    }
+
+    /**
+     * @return {@link Boolean}
+     */
+    @XmlTransient
+    public boolean isTemporalLayer() {
+        return ((this.temporalLayer != null) && (this.temporalLayer.isTemporalLayer()));
+    }
+
+    /**
      * @return the folder
      */
     @Override
@@ -199,15 +222,14 @@ public class GPRasterLayer extends GPLayer {
 
     @Override
     public String toString() {
-        StringBuilder str = new StringBuilder(this.getClass().getSimpleName()).append(
-                " {");
+        StringBuilder str = new StringBuilder(this.getClass().getSimpleName()).append(" {");
         str.append(super.toString());
         str.append(", opacity = ").append(opacity);
         str.append(", styles = ").append(styles);
         str.append(", layerInfo = ").append(layerInfo);
         str.append(", maxScale = ").append(maxScale);
         str.append(", minScale = ").append(minScale);
+        str.append(", isTemporal = ").append((this.isTemporalLayer() ? "YES" : "NO"));
         return str.append("}").toString();
     }
-
 }

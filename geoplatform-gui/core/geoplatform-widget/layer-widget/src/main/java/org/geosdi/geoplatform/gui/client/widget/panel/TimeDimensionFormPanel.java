1670d7d295757cb04a4aa21787e25b6527166ba0
Vito Salvia <vito.salvia@gmail.com>
2020-05-19 17:00:17 +0200

    Added animation on slider

--- geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/panel/TimeDimensionFormPanel.java
+++ geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/panel/TimeDimensionFormPanel.java
@@ -12,6 +12,7 @@ import com.extjs.gxt.ui.client.widget.layout.ColumnData;
 import com.extjs.gxt.ui.client.widget.layout.ColumnLayout;
 import com.extjs.gxt.ui.client.widget.layout.FlowData;
 import com.extjs.gxt.ui.client.widget.layout.FormLayout;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import org.geosdi.geoplatform.gui.client.LayerResources;
@@ -29,9 +30,9 @@ import static org.geosdi.geoplatform.gui.client.widget.time.LayerTimeFilterWidge
 public class TimeDimensionFormPanel extends FormPanel {
 
     private final LayoutContainer sliderContainer;
+    private final StartDateMultifield startDateMultifield;
+    private final EndDateMultifield endDateMultifield;
     private CheckBox endDateCheckBox;
-    private StartDateMultifield startDateMultifield;
-    private EndDateMultifield endDateMultifield;
     private Slider slider;
     private LabelField labelCurrenteTime;
     private ToggleButton playButton = new ToggleButton(null,
@@ -44,39 +45,14 @@ public class TimeDimensionFormPanel extends FormPanel {
             AbstractImagePrototype.create(LayerResources.ICONS.forwardTimeDimension()));
     private SelectionListener<ButtonEvent> playSelectioListener;
     private SelectionListener<ButtonEvent> playReverseSelectioListener;
+    private Timer animationTimer;
+    private int currentValue;
 
     @Inject
-    public TimeDimensionFormPanel(StartDateMultifield startDateMultifield, final EndDateMultifield endDateMultifield) {
-        this.startDateMultifield = startDateMultifield;
-        this.endDateMultifield = endDateMultifield;
-        this.sliderContainer = new LayoutContainer(new FormLayout()) {
-            @Override
-            protected void onAttach() {
-                super.onAttach();
-//                playButton.enable();
-
-            }
-
-            @Override
-            protected void onHide() {
-                super.onHide();
-//                stopPlayer();
-//                playButton.disable();
-            }
-
-            @Override
-            protected void onShow() {
-                super.onShow();
-//                playButton.enable();
-            }
-
-            @Override
-            protected void onDetach() {
-                super.onDetach();
-//                stopPlayer();
-//                playButton.disable();
-            }
-        };
+    public TimeDimensionFormPanel(StartDateMultifield theStartDateMultifield, final EndDateMultifield theEndDateMultifield) {
+        this.startDateMultifield = theStartDateMultifield;
+        this.endDateMultifield = theEndDateMultifield;
+        this.sliderContainer = new LayoutContainer(new FormLayout());
         this.addComponents();
     }
 
@@ -105,13 +81,14 @@ public class TimeDimensionFormPanel extends FormPanel {
     }
 
     private void buildTimeTimension() {
-        HorizontalPanel container = new HorizontalPanel();
-        container.setSpacing(2);
+        HorizontalPanel buttonsContainer = new HorizontalPanel();
+        buttonsContainer.setSpacing(2);
         this.slider = new Slider() {
             @Override
             public void setValue(int value) {
                 super.setValue(value);
-//                super.setMessage("" + (startStore.getModels().size() - value));
+                currentValue = value;
+                enableOnPlaying();
             }
         };
         this.slider.setIncrement(1);
@@ -119,64 +96,127 @@ public class TimeDimensionFormPanel extends FormPanel {
         this.reversePlayButton.setToolTip(ButtonsConstants.INSTANCE.playReverseText());
         this.forwardPlayButton.setToolTip(ButtonsConstants.INSTANCE.nextText());
         this.backwardButton.setToolTip(ButtonsConstants.INSTANCE.prevText());
-        this.playSelectioListener = new SelectionListener<ButtonEvent>() {
-            @Override
-            public void componentSelected(ButtonEvent ce) {
-//                GWT.log("Play button status: "+playButton.isPressed());
-                if (!playButton.isPressed()) {
-                    playButton.setToolTip(ButtonsConstants.INSTANCE.playText());
-                    enableButtonsOnPlay();
-                    playButton.setIcon(AbstractImagePrototype.create(
-                            LayerResources.ICONS.playTimeDimension()));
-
-                } else {
-                    if (isValid() && !validateForm()) {
-                        GeoPlatformMessage.errorMessage(LayerModuleConstants.INSTANCE.
-                                        LayerTimeFilterWidget_timeFilterWarningTitleText(),
-                                LayerModuleMessages.INSTANCE.
-                                        LayerTimeFilterWidget_rangeDateErrorMessage());
-                    } else if (isValid() && validateForm()) {
-                        disableButtonsOnPlay(Boolean.FALSE);
-                        playButton.setIcon(AbstractImagePrototype.create(
-                                LayerResources.ICONS.pauseTimeDimension()));
-                    }
-                }
-            }
-        };
+        this.addListener();
         this.playButton.addSelectionListener(playSelectioListener);
-        container.add((this.backwardButton));
-        container.add((this.reversePlayButton));
-        container.add((this.playButton));
-        container.add((this.forwardPlayButton));
+        buttonsContainer.add((this.backwardButton));
+        buttonsContainer.add((this.reversePlayButton));
+        buttonsContainer.add((this.playButton));
+        buttonsContainer.add((this.forwardPlayButton));
         this.sliderContainer.setLayout(new ColumnLayout());
-        this.sliderContainer.add(container, new ColumnData(.3));
+        this.sliderContainer.add(buttonsContainer, new ColumnData(.3));
         this.sliderContainer.add(this.slider, new ColumnData(.7));
         this.slider.setAutoWidth(Boolean.TRUE);
         super.add(this.sliderContainer);
         this.labelCurrenteTime = new LabelField();
         this.labelCurrenteTime.setFieldLabel(LayerModuleConstants.INSTANCE.LayerTimeFilterWidget_currentDateTooltipText());
         this.labelCurrenteTime.setLabelSeparator(":");
-        this.labelCurrenteTime.setValue("TEST");
         super.add(this.labelCurrenteTime, new FlowData(5));
     }
 
+    private void enableOnPlaying() {
+        backwardButton.setEnabled(!playButton.isPressed() && this.currentValue > slider.getMinValue());
+        reversePlayButton.setEnabled(!playButton.isPressed() && this.currentValue > slider.getMinValue());
+        forwardPlayButton.setEnabled(!playButton.isPressed() && this.currentValue < slider.getMaxValue());
+        playButton.setEnabled(this.currentValue < slider.getMaxValue());
+    }
+
+    private void addListener() {
+        this.playSelectioListener = new SelectionListener<ButtonEvent>() {
+            @Override
+            public void componentSelected(ButtonEvent ce) {
+//                GWT.log("Play button status: "+playButton.isPressed());
+                if (!playButton.isPressed()) {
+                    playButton.setToolTip(ButtonsConstants.INSTANCE.playText());
+                    playButton.setIcon(AbstractImagePrototype.create(
+                            LayerResources.ICONS.playTimeDimension()));
+                    animationTimer.cancel();
+                    enableOnPlaying();
+                } else {
+                    if (validateForm()) {
+                        playButton.setToolTip(ButtonsConstants.INSTANCE.pauseText());
+                        playButton.setIcon(AbstractImagePrototype.create(
+                                LayerResources.ICONS.pauseTimeDimension()));
+                        playTimeFilter();
+                    }
+
+                }
+            }
+        };
+        this.backwardButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
+            @Override
+            public void componentSelected(ButtonEvent ce) {
+                slider.setValue(slider.getValue() - 1);
+            }
+        });
+        this.forwardPlayButton.addSelectionListener(new SelectionListener<ButtonEvent>() {
+            @Override
+            public void componentSelected(ButtonEvent ce) {
+                slider.setValue(slider.getValue() + 1);
+            }
+        });
+    }
+
+    private void playTimeFilter() {
+        animationTimer = new Timer() {
+            @Override
+            public void run() {
+                if (currentValue < slider.getMaxValue()) {
+                    slider.setValue(++currentValue);
+                    labelCurrenteTime.setValue(currentValue);
+                } else {
+                    stopPlayer();
+                }
+            }
+        };
+        animationTimer.scheduleRepeating(500);
+    }
+
+    private void stopPlayer() {
+        if (this.playButton.isPressed()) {
+            this.playButton.toggle(Boolean.FALSE);
+            playSelectioListener.componentSelected(null);
+            this.reversePlayButton.enable();
+            this.backwardButton.enable();
+        }
+    }
+
     public boolean validateForm() {
-        return this.startDateMultifield.getDate().before(this.endDateMultifield.getDate());
+        boolean check = isValid();
+        if (!check) {
+            this.playButton.toggle(Boolean.FALSE);
+            return check;
+        } else if (!this.startDateMultifield.getDate().before(this.endDateMultifield.getDate())) {
+            GeoPlatformMessage.errorMessage(LayerModuleConstants.INSTANCE.
+                            LayerTimeFilterWidget_timeFilterWarningTitleText(),
+                    LayerModuleMessages.INSTANCE.
+                            LayerTimeFilterWidget_rangeDateErrorMessage());
+            this.playButton.toggle(Boolean.FALSE);
+            return false;
+        }
+        return true;
     }
 
-    private void disableButtonsOnPlay(boolean reverse) {
+    private void initComponents() {
         this.backwardButton.disable();
-        this.forwardPlayButton.disable();
-        if (reverse)
-            this.playButton.disable();
-        else
-            this.reversePlayButton.disable();
+        this.forwardPlayButton.enable();
+        this.playButton.toggle(Boolean.FALSE);
+        this.reversePlayButton.toggle(Boolean.FALSE);
+        this.startDateMultifield.initComponents();
+        this.endDateMultifield.initComponents();
+        this.slider.setValue(0);
+        this.endDateCheckBox.setValue(true);
+        this.endDateMultifield.setVisible(Boolean.TRUE);
     }
 
-    private void enableButtonsOnPlay() {
-        this.backwardButton.enable();
-        this.forwardPlayButton.enable();
-        this.reversePlayButton.enable();
-        this.playButton.enable();
+    @Override
+    protected void onAttach() {
+        super.onAttach();
+        initComponents();
+    }
+
+    @Override
+    protected void onDetach() {
+        super.onDetach();
+        this.stopPlayer();
     }
 }

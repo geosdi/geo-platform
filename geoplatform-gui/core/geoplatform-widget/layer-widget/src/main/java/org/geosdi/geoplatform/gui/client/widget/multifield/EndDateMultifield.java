553bfbf9c5ee9eae12cd35733df884516829cc7a
Vito Salvia <vito.salvia@gmail.com>
2020-05-21 16:19:10 +0200

    Binding date form with bean

--- geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/multifield/EndDateMultifield.java
+++ geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/multifield/EndDateMultifield.java
@@ -2,8 +2,13 @@ package org.geosdi.geoplatform.gui.client.widget.multifield;
 
 import com.extjs.gxt.ui.client.widget.form.Field;
 import com.extjs.gxt.ui.client.widget.form.Validator;
+import com.google.gwt.core.client.GWT;
 import org.geosdi.geoplatform.gui.client.i18n.LayerModuleConstants;
 import org.geosdi.geoplatform.gui.client.i18n.LayerModuleMessages;
+import org.geosdi.geoplatform.gui.client.model.RasterTreeNode;
+import org.geosdi.geoplatform.gui.client.puregwt.binding.GPDateBindingHandler;
+import org.geosdi.geoplatform.gui.model.tree.GPBeanTreeModel;
+import org.geosdi.geoplatform.gui.puregwt.properties.WidgetPropertiesHandlerManager;
 
 /**
  * @author Vito Salvia - CNR IMAA geoSDI Group
@@ -13,6 +18,8 @@ public class EndDateMultifield extends TimeDimensionDateMultifield {
 
     public EndDateMultifield() {
         super();
+        WidgetPropertiesHandlerManager.addHandler(GPDateBindingHandler.TYPE, this);
+
     }
 
     @Override
@@ -44,6 +51,15 @@ public class EndDateMultifield extends TimeDimensionDateMultifield {
     @Override
     protected String fieldLabel() {
         return LayerModuleConstants.INSTANCE.LayerTimeFilterWidget_multidatesFromText();
+    }
+
+    @Override
+    public void bindDate(GPBeanTreeModel gpTreePanel) {
+        GWT.log("@@@@@@@@@@@@@@@" + ((RasterTreeNode) gpTreePanel));
+        String dates[] = ((RasterTreeNode) gpTreePanel).getExtent().getValue().split(",");
+        this.limitDate = this.parseDateFormat.parse(dates[dates.length - 1].replace("Z", ""));
+        super.buildDateField();
+        super.dateField.setMaxValue(this.limitDate);
     }
 
 }
--- geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/multifield/StartDateMultifield.java
+++ geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/multifield/StartDateMultifield.java
@@ -4,6 +4,10 @@ import com.extjs.gxt.ui.client.widget.form.Field;
 import com.extjs.gxt.ui.client.widget.form.Validator;
 import org.geosdi.geoplatform.gui.client.i18n.LayerModuleConstants;
 import org.geosdi.geoplatform.gui.client.i18n.LayerModuleMessages;
+import org.geosdi.geoplatform.gui.client.model.RasterTreeNode;
+import org.geosdi.geoplatform.gui.client.puregwt.binding.GPDateBindingHandler;
+import org.geosdi.geoplatform.gui.model.tree.GPBeanTreeModel;
+import org.geosdi.geoplatform.gui.puregwt.properties.WidgetPropertiesHandlerManager;
 
 /**
  * @author Vito Salvia - CNR IMAA geoSDI Group
@@ -13,6 +17,7 @@ public class StartDateMultifield extends TimeDimensionDateMultifield {
 
     public StartDateMultifield() {
         super();
+        WidgetPropertiesHandlerManager.addHandler(GPDateBindingHandler.TYPE, this);
     }
 
     @Override
@@ -43,6 +48,14 @@ public class StartDateMultifield extends TimeDimensionDateMultifield {
     @Override
     protected String fieldLabel() {
         return LayerModuleConstants.INSTANCE.LayerTimeFilterWidget_multidatesFromText();
+    }
+
+    @Override
+    public void bindDate(GPBeanTreeModel gpTreePanel) {
+        String dates[] = ((RasterTreeNode) gpTreePanel).getExtent().getValue().split(",");
+        this.limitDate = this.parseDateFormat.parse(dates[0].replace("Z", ""));
+        super.buildDateField();
+        super.dateField.setMinValue(this.limitDate);
     }
 
 }

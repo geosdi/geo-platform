c3bc6905cda0b796a944d719689d3f4747e387ea
Vito Salvia <vito.salvia@gmail.com>
2020-05-25 12:39:11 +0200

    Removing adding close button

--- geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/time/panel/TimePeriodPanel.java
+++ geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/time/panel/TimePeriodPanel.java
@@ -1,13 +1,7 @@
 package org.geosdi.geoplatform.gui.client.widget.time.panel;
 
-import com.extjs.gxt.ui.client.event.ButtonEvent;
-import com.extjs.gxt.ui.client.event.SelectionListener;
-import com.extjs.gxt.ui.client.widget.button.Button;
 import com.extjs.gxt.ui.client.widget.form.FormPanel;
-import org.geosdi.geoplatform.gui.client.i18n.buttons.ButtonsConstants;
-import org.geosdi.geoplatform.gui.client.puregwt.filter.event.GPHideFilterWidgetEvent;
 import org.geosdi.geoplatform.gui.client.widget.GeoPlatformContentPanel;
-import org.geosdi.geoplatform.gui.puregwt.properties.WidgetPropertiesHandlerManager;
 
 /**
  * @author Vito Salvia - CNR IMAA geoSDI Group
@@ -25,16 +19,7 @@ public class TimePeriodPanel extends GeoPlatformContentPanel {
     @Override
     public void addComponent() {
         super.add(this.panel);
-        Button close = new Button(ButtonsConstants.INSTANCE.closeText(),
-                new SelectionListener<ButtonEvent>() {
-                    @Override
-                    public void componentSelected(ButtonEvent ce) {
-                        WidgetPropertiesHandlerManager.fireEvent(new GPHideFilterWidgetEvent());
-                    }
-                });
-        this.panel.addButton(close);
     }
-
 
     @Override
     public void initSize() {

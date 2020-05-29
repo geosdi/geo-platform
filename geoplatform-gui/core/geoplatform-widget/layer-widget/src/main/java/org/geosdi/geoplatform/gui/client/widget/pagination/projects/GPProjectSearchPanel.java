fbd84c398130a601619901fc630d986182225c57
Vito Salvia <vito.salvia@gmail.com>
2020-05-29 10:50:06 +0200

    Fixed Bug geoportale webgis- Load on Tree

--- geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/pagination/projects/GPProjectSearchPanel.java
+++ geoplatform-gui/core/geoplatform-widget/layer-widget/src/main/java/org/geosdi/geoplatform/gui/client/widget/pagination/projects/GPProjectSearchPanel.java
@@ -1,37 +1,36 @@
 /**
- *
- *    geo-platform
- *    Rich webgis framework
- *    http://geo-platform.org
- *   ====================================================================
- *
- *   Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
- *
- *   This program is free software: you can redistribute it and/or modify it
- *   under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version. This program is distributed in the
- *   hope that it will be useful, but WITHOUT ANY WARRANTY; without
- *   even the implied warranty of MERCHANTABILITY or FITNESS FOR
- *   A PARTICULAR PURPOSE. See the GNU General Public License
- *   for more details. You should have received a copy of the GNU General
- *   Public License along with this program. If not, see http://www.gnu.org/licenses/
- *
- *   ====================================================================
- *
- *   Linking this library statically or dynamically with other modules is
- *   making a combined work based on this library. Thus, the terms and
- *   conditions of the GNU General Public License cover the whole combination.
- *
- *   As a special exception, the copyright holders of this library give you permission
- *   to link this library with independent modules to produce an executable, regardless
- *   of the license terms of these independent modules, and to copy and distribute
- *   the resulting executable under terms of your choice, provided that you also meet,
- *   for each linked independent module, the terms and conditions of the license of
- *   that module. An independent module is a module which is not derived from or
- *   based on this library. If you modify this library, you may extend this exception
- *   to your version of the library, but you are not obligated to do so. If you do not
- *   wish to do so, delete this exception statement from your version.
+ * geo-platform
+ * Rich webgis framework
+ * http://geo-platform.org
+ * ====================================================================
+ * <p>
+ * Copyright (C) 2008-2019 geoSDI Group (CNR IMAA - Potenza - ITALY).
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version. This program is distributed in the
+ * hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details. You should have received a copy of the GNU General
+ * Public License along with this program. If not, see http://www.gnu.org/licenses/
+ * <p>
+ * ====================================================================
+ * <p>
+ * Linking this library statically or dynamically with other modules is
+ * making a combined work based on this library. Thus, the terms and
+ * conditions of the GNU General Public License cover the whole combination.
+ * <p>
+ * As a special exception, the copyright holders of this library give you permission
+ * to link this library with independent modules to produce an executable, regardless
+ * of the license terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided that you also meet,
+ * for each linked independent module, the terms and conditions of the license of
+ * that module. An independent module is a module which is not derived from or
+ * based on this library. If you modify this library, you may extend this exception
+ * to your version of the library, but you are not obligated to do so. If you do not
+ * wish to do so, delete this exception statement from your version.
  */
 package org.geosdi.geoplatform.gui.client.widget.pagination.projects;
 
@@ -97,12 +96,12 @@ public class GPProjectSearchPanel extends GPListViewSearchPanel<GPClientProject>
     //
     private final GPDefaultProjectTreeEvent defaultProjectEvent = new GPDefaultProjectTreeEvent();
     private final GPDefaultProjectSelector selector;
+    //
+    private final GPProjectManagementWidget projectManagementWidget;
     //Protect this
     private GPSecureButton deleteButton;
     private GPSecureButton editButton;
     private GPSecureButton shareButton;
-    //
-    private final GPProjectManagementWidget projectManagementWidget;
 
     public GPProjectSearchPanel(GPProjectManagementWidget projectManagementWidget) {
         super(true, 12);
@@ -173,7 +172,7 @@ public class GPProjectSearchPanel extends GPListViewSearchPanel<GPClientProject>
 
             @Override
             protected void load(final Object loadConfig,
-                    final AsyncCallback<PagingLoadResult<GPClientProject>> callback) {
+                                final AsyncCallback<PagingLoadResult<GPClientProject>> callback) {
                 final SearchProjectsRequest searchProjectsRequest = GWT.
                         <SearchProjectsRequest>create(SearchProjectsRequest.class);
 
@@ -242,9 +241,8 @@ public class GPProjectSearchPanel extends GPListViewSearchPanel<GPClientProject>
                                 PeekCacheEvent peekCacheEvent = new PeekCacheEvent();
                                 LayerHandlerManager.fireEvent(peekCacheEvent);
                             } else {
-                                GeoPlatformMessage.errorMessage(
-                                        LayerModuleConstants.INSTANCE.ShareProjectPanel_shareVerifySaveOperationsTitleText(),
-                                        LayerModuleConstants.INSTANCE.ShareProjectPanel_shareVerifySaveOperationsMessageText());
+                                MementoModuleInjector.MainInjector.getInstance().getMementoSave().clear();
+                                selector.selectDefaultProject();
                             }
                         }
                     });

a19addb6fb1285a99d0fa95f714f6c301c89cad5
Giuseppe La Scaleia <glascaleia@gmail.com>
2020-11-17 09:42:51 +0100

    Upgrade XStream

--- geoplatform-support/geoplatform-websocket-server-support/src/main/java/org/geosdi/geoplatform/websocket/support/spring/configuration/broker/mem/GPWebSocketMemBroker.java
+++ geoplatform-support/geoplatform-websocket-server-support/src/main/java/org/geosdi/geoplatform/websocket/support/spring/configuration/broker/mem/GPWebSocketMemBroker.java
@@ -7,6 +7,8 @@ import org.geosdi.geoplatform.websocket.support.spring.configuration.broker.GPWe
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.stream.Stream.of;
 
@@ -39,6 +41,8 @@ class GPWebSocketMemBroker implements GPWebSocketStompBroker {
     @Override
     public String[] getEndpoints() {
         return of(this.endpoints.split(","))
+                .filter(Objects::nonNull)
+                .filter(value -> !value.trim().isEmpty())
                 .map(value -> (value.startsWith("/") ? value : "/".concat(value)))
                 .toArray(String[]::new);
     }
@@ -49,6 +53,8 @@ class GPWebSocketMemBroker implements GPWebSocketStompBroker {
     @Override
     public String[] getDestinationApplicationPrefix() {
         return of(this.destinationApplicationPrefix.split(","))
+                .filter(Objects::nonNull)
+                .filter(value -> !value.trim().isEmpty())
                 .map(value -> (value.startsWith("/") ? value : "/".concat(value)))
                 .toArray(String[]::new);
     }
@@ -59,10 +65,21 @@ class GPWebSocketMemBroker implements GPWebSocketStompBroker {
     @Override
     public String[] getStompBrokers() {
         return of(this.stompBrokers.split(","))
+                .filter(Objects::nonNull)
+                .filter(value -> !value.trim().isEmpty())
                 .map(value -> (value.startsWith("/") ? value : "/".concat(value)))
                 .toArray(size -> new String[size]);
     }
 
+    /**
+     * Invoked by the containing {@code BeanFactory} after it has set all bean properties
+     * and satisfied {@link org.springframework.beans.factory.BeanFactoryAware}, {@code ApplicationContextAware} etc.
+     * <p>This method allows the bean instance to perform validation of its overall
+     * configuration and final initialization when all bean properties have been set.
+     *
+     * @throws Exception in the event of misconfiguration (such as failure to set an
+     *                   essential property) or if initialization fails for any other reason
+     */
     @Override
     public void afterPropertiesSet() throws Exception {
         checkArgument((this.endpoints != null) && !(this.endpoints.trim().isEmpty()) && !(this.endpoints.equals(ENDPOINTS_VALUE)), "Parameter Endpoints can not be Null or an Empty String or " + ENDPOINTS_VALUE);

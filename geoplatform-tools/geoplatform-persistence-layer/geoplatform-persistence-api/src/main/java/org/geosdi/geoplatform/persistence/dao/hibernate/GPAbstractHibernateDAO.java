39b55dccfae137bd49f9969aa3f42f5011045b3d
Giuseppe La Scaleia <glascaleia@gmail.com>
2021-01-29 09:19:50 +0100

    Improvements

--- geoplatform-tools/geoplatform-persistence-layer/geoplatform-persistence-api/src/main/java/org/geosdi/geoplatform/persistence/dao/hibernate/GPAbstractHibernateDAO.java
+++ geoplatform-tools/geoplatform-persistence-layer/geoplatform-persistence-api/src/main/java/org/geosdi/geoplatform/persistence/dao/hibernate/GPAbstractHibernateDAO.java
@@ -214,7 +214,7 @@ public abstract class GPAbstractHibernateDAO<T extends Object, ID extends Serial
             Root<T> root = criteriaQuery.from(super.getPersistentClass());
             criteriaQuery.select(root);
             return this.currentSession().createQuery(criteriaQuery)
-                    .setFirstResult(start)
+                    .setFirstResult((start == 0) ? 0 : ((start * end)))
                     .setMaxResults(end)
                     .getResultList();
         } catch (HibernateException ex) {

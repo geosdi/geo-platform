88ff034aaf432f665b8cf17b21fd4f4f32cd7f91
Giuseppe La Scaleia <glascaleia@gmail.com>
2021-04-08 10:11:25 +0200

    Tests Improvements

--- geoplatform-services/geoplatform-modules/geoplatform-ogc/wms/wms-impl/src/main/java/org/geosdi/geoplatform/services/builder/GeoSDIHttpClient5.java
+++ geoplatform-services/geoplatform-modules/geoplatform-ogc/wms/wms-impl/src/main/java/org/geosdi/geoplatform/services/builder/GeoSDIHttpClient5.java
@@ -106,7 +106,7 @@ public class GeoSDIHttpClient5 implements HTTPClient {
 
     GeoSDIHttpClient5() {
         System.setProperty("jsse.enableSNIExtension", "false");
-        System.setProperty("sun.security.ssl.allowUnsafeRenegotiation", "true");
+//        System.setProperty("sun.security.ssl.allowUnsafeRenegotiation", "true");
         this.httpClient = HttpClients
                 .custom()
                 .setConnectionManagerShared(TRUE)

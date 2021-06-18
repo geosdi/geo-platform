949007bc758b013b1bafdc0e1e3495fa78bc975a
vitosalvia <vito.salvia@gmail.com>
2021-06-18 17:23:33 +0200

    Fixed doPost

--- geoplatform-gui/core/geoplatform-server-api/src/main/java/org/geosdi/geoplatform/gui/server/GPServerProxy.java
+++ geoplatform-gui/core/geoplatform-server-api/src/main/java/org/geosdi/geoplatform/gui/server/GPServerProxy.java
@@ -38,6 +38,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.config.RegistryBuilder;
 import org.apache.http.conn.HttpClientConnectionManager;
@@ -45,26 +46,25 @@ import org.apache.http.conn.socket.ConnectionSocketFactory;
 import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.DefaultConnectionReuseStrategy;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.ssl.SSLContextBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URI;
-import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.Enumeration;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.stream.Collectors.joining;
 
 /**
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
@@ -73,8 +73,6 @@ import java.util.Enumeration;
 public class GPServerProxy extends HttpServlet {
 
     private static final long serialVersionUID = -8296565872107565884L;
-    //
-    private static final Logger logger = LoggerFactory.getLogger(GPServerProxy.class);
 
     @Override
     public void init() throws ServletException {
@@ -88,8 +86,8 @@ public class GPServerProxy extends HttpServlet {
      * @throws IOException
      */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        logger.info("@@@@@@@@@@@@@@@@@@@@@Called {}#doGet.\n", this.getClass().getSimpleName());
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.log("@@@@@@@@@@@@@@@@@@@@@Called " + this.getClass().getSimpleName() + "#doGet.");
         try {
             if ((request.getParameter("targetURL") != null) && (request.getParameter("targetURL") != "")) {
                 CloseableHttpClient httpClient =  HttpClients
@@ -113,27 +111,27 @@ public class GPServerProxy extends HttpServlet {
                     }
                 }
                 URI uri = uriBuilder.build();
-                logger.info("############################URI to call : {}\n", uri.toString());
+                this.log("############################URI to call : "+ uri.toString());
                 HttpGet httpGet = new HttpGet(uri);
                 if(((request.getParameter("v") != null) && (request.getParameter("v") != "")) && ((request.getParameter("p") != null) && (request.getParameter("p") != ""))) {
                     String userName = request.getParameter("v");
                     String password = request.getParameter("p");
                     String userpass = userName + ":" + password;
-                    logger.info("@@@@@@@@@@@@@@@@@Trying to inject basicAuth with parameter : {}\n", userpass);
+                    this.log("@@@@@@@@@@@@@@@@@Trying to inject basicAuth with parameter :" + userpass);
                     String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()));
                     httpGet.setHeader(HttpHeaders.AUTHORIZATION, basicAuth);
                 }
                 CloseableHttpResponse httpClientResponse = httpClient.execute(httpGet);
-                logger.trace("###########################STATUS_CODE : {}\n\n", httpClientResponse.getStatusLine());
+                this.log("###########################STATUS_CODE :" + httpClientResponse.getStatusLine());
                 if (httpClientResponse.getStatusLine().getStatusCode() != 200) {
                     String exceptionMessage = IOUtils
-                            .toString(httpClientResponse.getEntity().getContent(), StandardCharsets.UTF_8);
-                    logger.error("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ExecutionError : {}\n", exceptionMessage);
+                            .toString(httpClientResponse.getEntity().getContent(), UTF_8);
+                    this.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ExecutionError : " + exceptionMessage);
                     response.setStatus(500);
                 } else {
                     OutputStream ostream = response.getOutputStream();
                     copy(httpClientResponse.getEntity().getContent(), ostream);
-                    logger.debug("#########################EXECUTE SUCCESS \n\n");
+                    this.log("#########################EXECUTE SUCCESS \n\n");
                 }
             }
         } catch (Exception ex) {
@@ -143,20 +141,79 @@ public class GPServerProxy extends HttpServlet {
     }
 
     /**
-     * @param req
-     * @param resp
+     * @param request
+     * @param response
      * @throws ServletException
      * @throws IOException
      */
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.log("@@@@@@@@@@@@@@@@@@@@@Called " + this.getClass().getSimpleName()+ "#doPost.");
+        try {
+            if ((request.getParameter("targetURL") != null) && (request.getParameter("targetURL") != "")) {
+                CloseableHttpClient httpClient = HttpClients
+                        .custom()
+                        .setConnectionReuseStrategy(new DefaultConnectionReuseStrategy())
+                        .setConnectionManager(createClientConnectionManager())
+                        .setDefaultCredentialsProvider(new BasicCredentialsProvider())
+                        .build();
+                String targetUrl = request.getParameter("targetURL");
+                URIBuilder uriBuilder = new URIBuilder(targetUrl);
+                Enumeration<String> parametersName = request.getParameterNames();
+                while (parametersName.hasMoreElements()) {
+                    String requestParamName = parametersName.nextElement();
+                    switch (requestParamName) {
+                        case "targetURL":
+                        case "v":
+                        case "p":
+                            break;
+                        default:
+                            uriBuilder.addParameter(requestParamName, request.getParameter(requestParamName));
+                    }
+                }
+                URI uri = uriBuilder.build();
+                this.log("############################URI to call : " + uri.toString());
+                HttpPost httpPost = new HttpPost(uri);
+                if (request.getHeader("Content-Type") != null) {
+                    httpPost.setHeader("Content-Type", request.getContentType());
+                }
+                if(((request.getParameter("v") != null) && (request.getParameter("v") != "")) && ((request.getParameter("p") != null) && (request.getParameter("p") != ""))) {
+                    String userName = request.getParameter("v");
+                    String password = request.getParameter("p");
+                    String userpass = userName + ":" + password;
+                    this.log("@@@@@@@@@@@@@@@@@Trying to inject basicAuth with parameter :" + userpass);
+                    String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()));
+                    httpPost.setHeader(HttpHeaders.AUTHORIZATION, basicAuth);
+                }
+                int contentLength = request.getContentLength();
+                if (contentLength > 0) {
+                    httpPost.setEntity(new StringEntity(new BufferedReader(new InputStreamReader(request.getInputStream(), UTF_8))
+                            .lines()
+                            .collect(joining("\n"))));
+                }
+                CloseableHttpResponse httpClientResponse = httpClient.execute(httpPost);
+                this.log("###########################STATUS_CODE :" + httpClientResponse.getStatusLine().getStatusCode());
+                if (httpClientResponse.getStatusLine().getStatusCode() != 200) {
+                    String exceptionMessage = IOUtils
+                            .toString(httpClientResponse.getEntity().getContent(), UTF_8);
+                    this.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ExecutionError :" + exceptionMessage);
+                    response.setStatus(500);
+                } else {
+                    OutputStream ostream = response.getOutputStream();
+                    copy(httpClientResponse.getEntity().getContent(), ostream);
+                    this.log("#########################EXECUTE SUCCESS");
+                }
+            }
+        } catch (Exception ex) {
+            response.setStatus(500);
+            ex.printStackTrace();
+        }
     }
 
     @Override
     public void destroy() {
         super.destroy();
-        logger.debug("@@@@@@@@@@@@@@@@@@Called {}#destroy\n", this.getClass().getSimpleName());
+        this.log("@@@@@@@@@@@@@@@@@@Called " + this.getClass().getSimpleName() + "#destroy");
     }
 
     /**

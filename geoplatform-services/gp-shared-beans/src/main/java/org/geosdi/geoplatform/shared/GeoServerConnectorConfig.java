2f8c04a592554cea8ae15e36e4987a021068412f
vitosalvia <vito.salvia@gmail.com>
2021-09-12 19:57:54 +0200

    Added config class to create GeoserverConnector

---
+++ geoplatform-services/gp-shared-beans/src/main/java/org/geosdi/geoplatform/shared/GeoServerConnectorConfig.java
@@ -0,0 +1,35 @@
+package org.geosdi.geoplatform.shared;
+
+import org.geosdi.geoplatform.connector.server.security.BasicPreemptiveSecurityConnector;
+import org.geosdi.geoplatform.connector.store.GPGeoserverConnectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.URL;
+
+import static org.geosdi.geoplatform.connector.server.config.GPPooledConnectorConfigBuilder.PooledConnectorConfigBuilder.pooledConnectorConfigBuilder;
+import static org.geosdi.geoplatform.connector.store.GPGeoserverConnectorStoreBuilder.geoserverConnectorBuilder;
+
+/**
+ * @author Vito Salvia - CNR IMAA geoSDI Group
+ * @email vito.salvia@gmail.com
+ */
+@Configuration
+class GeoServerConnectorConfig {
+
+    @Bean(name = "geoserverConnectorStore")
+    public GPGeoserverConnectorStore geoserverConnectorStore(@Value(value = "configurator{geoserver_url}") String geoserverUrl,
+            @Value(value = "configurator{geoserver_username}") String username, @Value(value = "configurator{geoserver_password}") String password) throws Exception {
+        System.out.println("@@@@@@@@@ "+geoserverUrl);
+        return geoserverConnectorBuilder().withServerUrl(new URL(geoserverUrl))
+                .withPooledConnectorConfig(pooledConnectorConfigBuilder()
+                        .withMaxTotalConnections(80)
+                        .withDefaultMaxPerRoute(40)
+                        .withMaxRedirect(10)
+                        .build())
+                .withClientSecurity(new BasicPreemptiveSecurityConnector(username, password))
+                .build();
+    }
+
+}

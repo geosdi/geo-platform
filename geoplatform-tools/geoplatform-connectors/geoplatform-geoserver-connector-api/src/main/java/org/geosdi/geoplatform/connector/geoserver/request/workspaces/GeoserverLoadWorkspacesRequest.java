8951ade70b20d754a0e46a517cab8122648939ee
vito <vito.salvia@gmail.com>
2021-09-20 11:56:23 +0200

    Added getWorkpacesNames methods

--- geoplatform-tools/geoplatform-connectors/geoplatform-geoserver-connector-api/src/main/java/org/geosdi/geoplatform/connector/geoserver/request/workspaces/GeoserverLoadWorkspacesRequest.java
+++ geoplatform-tools/geoplatform-connectors/geoplatform-geoserver-connector-api/src/main/java/org/geosdi/geoplatform/connector/geoserver/request/workspaces/GeoserverLoadWorkspacesRequest.java
@@ -37,9 +37,18 @@ package org.geosdi.geoplatform.connector.geoserver.request.workspaces;
 import org.geosdi.geoplatform.connector.geoserver.model.workspace.GPGeoserverWorkspaces;
 import org.geosdi.geoplatform.connector.server.request.json.GPJsonConnectorRequest;
 
+import java.util.List;
+
 /**
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
  * @email giuseppe.lascaleia@geosdi.org
  */
 public interface GeoserverLoadWorkspacesRequest extends GPJsonConnectorRequest<GPGeoserverWorkspaces, GeoserverLoadWorkspacesRequest> {
+
+    /**
+     * @return {@link List<String>}
+     * @throws Exception
+     */
+    List<String> getWorkpacesNames() throws Exception;
+
 }

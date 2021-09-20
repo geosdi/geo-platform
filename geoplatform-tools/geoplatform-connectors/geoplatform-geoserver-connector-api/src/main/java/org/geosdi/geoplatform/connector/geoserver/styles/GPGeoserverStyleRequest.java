57ecda3090e929cb39de1676da218004510a020d
Giuseppe La Scaleia <glascaleia@gmail.com>
2021-09-20 09:15:07 +0200

    Improvements

--- geoplatform-tools/geoplatform-connectors/geoplatform-geoserver-connector-api/src/main/java/org/geosdi/geoplatform/connector/geoserver/styles/GPGeoserverStyleRequest.java
+++ geoplatform-tools/geoplatform-connectors/geoplatform-geoserver-connector-api/src/main/java/org/geosdi/geoplatform/connector/geoserver/styles/GPGeoserverStyleRequest.java
@@ -39,14 +39,12 @@ import net.jcip.annotations.ThreadSafe;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.geosdi.geoplatform.connector.geoserver.model.styles.GPGeoserverSingleStyle;
 import org.geosdi.geoplatform.connector.geoserver.request.styles.GeoserverStyleRequest;
+import org.geosdi.geoplatform.connector.geoserver.styles.base.GPGeoserverBaseStyleRequest;
 import org.geosdi.geoplatform.connector.server.GPServerConnector;
-import org.geosdi.geoplatform.connector.server.request.json.GPJsonGetConnectorRequest;
 import org.geosdi.geoplatform.support.jackson.JacksonSupport;
 
 import javax.annotation.Nonnull;
 
-import static com.google.common.base.Preconditions.checkArgument;
-import static java.lang.ThreadLocal.withInitial;
 import static javax.annotation.meta.When.NEVER;
 
 /**
@@ -54,9 +52,7 @@ import static javax.annotation.meta.When.NEVER;
  * @email giuseppe.lascaleia@geosdi.org
  */
 @ThreadSafe
-public class GPGeoserverStyleRequest extends GPJsonGetConnectorRequest<GPGeoserverSingleStyle, GeoserverStyleRequest> implements GeoserverStyleRequest {
-
-    private final ThreadLocal<String> styleName;
+public class GPGeoserverStyleRequest extends GPGeoserverBaseStyleRequest<GPGeoserverSingleStyle, GeoserverStyleRequest> implements GeoserverStyleRequest {
 
     /**
      * @param server
@@ -64,27 +60,6 @@ public class GPGeoserverStyleRequest extends GPJsonGetConnectorRequest<GPGeoserv
      */
     GPGeoserverStyleRequest(@Nonnull(when = NEVER) GPServerConnector server, @Nonnull(when = NEVER) JacksonSupport theJacksonSupport) {
         super(server, theJacksonSupport);
-        this.styleName = withInitial(() -> null);
-    }
-
-    /**
-     * @param theStyleName
-     */
-    public GeoserverStyleRequest withStyleName(@Nonnull(when = NEVER) String theStyleName) {
-        this.styleName.set(theStyleName);
-        return self();
-    }
-
-    /**
-     * @return {@link String}
-     */
-    @Override
-    protected String createUriPath() throws Exception {
-        String styleName = this.styleName.get();
-        checkArgument(((styleName != null) && !(styleName.trim().isEmpty())), "The Parameter Style Name must not be null or an Empty String.");
-        String baseURI = this.serverURI.toString();
-        return ((baseURI.endsWith("/") ? baseURI.concat("styles/").concat(styleName).concat(".json")
-                : baseURI.concat("/styles/").concat(styleName).concat(".json")));
     }
 
     /**

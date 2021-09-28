c386e816a597d02c705bde9b996d580806173da6
Giuseppe La Scaleia <glascaleia@gmail.com>
2021-09-28 12:06:07 +0200

    Fixed Import

--- geoplatform-tools/geoplatform-connectors/geoplatform-geoserver-connector-api/src/main/java/org/geosdi/geoplatform/connector/geoserver/styles/GPGeoserverCreateWorkspaceStyleRequest.java
+++ geoplatform-tools/geoplatform-connectors/geoplatform-geoserver-connector-api/src/main/java/org/geosdi/geoplatform/connector/geoserver/styles/GPGeoserverCreateWorkspaceStyleRequest.java
@@ -35,8 +35,8 @@
 package org.geosdi.geoplatform.connector.geoserver.styles;
 
 import net.jcip.annotations.ThreadSafe;
-import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.StringEntity;
 import org.geosdi.geoplatform.connector.geoserver.model.styles.IGPGeoserverStyleBody;
 import org.geosdi.geoplatform.connector.geoserver.request.styles.GeoserverCreateWorkspaceStyleRequest;
 import org.geosdi.geoplatform.connector.geoserver.styles.base.GPGeoserverBaseCreateWorkspaceStyleRequest;
@@ -47,7 +47,7 @@ import javax.annotation.Nonnull;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static javax.annotation.meta.When.NEVER;
-import static org.apache.hc.core5.http.ContentType.APPLICATION_JSON;
+import static org.apache.http.entity.ContentType.APPLICATION_JSON;
 
 /**
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group

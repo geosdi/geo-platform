b531350dcea4ca065992d46ea2b80fce96fbb148
Giuseppe La Scaleia <glascaleia@gmail.com>
2021-11-18 15:56:54 +0100

    FIXED Problem for JDK17

--- geoplatform-tools/geoplatform-logger-support/src/main/java/org/geosdi/geoplatform/logger/support/spring/GPLoggerPostProcessor.java
+++ geoplatform-tools/geoplatform-logger-support/src/main/java/org/geosdi/geoplatform/logger/support/spring/GPLoggerPostProcessor.java
@@ -35,14 +35,14 @@
  */
 package org.geosdi.geoplatform.logger.support.spring;
 
-import java.lang.reflect.Field;
 import org.geosdi.geoplatform.logger.support.annotation.GeoPlatformLog;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.util.ReflectionUtils;
-import org.springframework.util.ReflectionUtils.FieldCallback;
 
 /**
  *
@@ -51,33 +51,56 @@ import org.springframework.util.ReflectionUtils.FieldCallback;
  */
 public class GPLoggerPostProcessor implements BeanPostProcessor {
 
+    /**
+     * Apply this {@code BeanPostProcessor} to the given new bean instance <i>before</i> any bean
+     * initialization callbacks (like InitializingBean's {@code afterPropertiesSet}
+     * or a custom init-method). The bean will already be populated with property values.
+     * The returned bean instance may be a wrapper around the original.
+     * <p>The default implementation returns the given {@code bean} as-is.
+     *
+     * @param bean the new bean instance
+     * @param beanName the name of the bean
+     * @return the bean instance to use, either the original or a wrapped one;
+     * if {@code null}, no subsequent BeanPostProcessors will be invoked
+     * @throws BeansException in case of errors
+     * @see InitializingBean#afterPropertiesSet
+     */
     @Override
-    public Object postProcessBeforeInitialization(final Object o, String string)
-            throws BeansException {
-
-        ReflectionUtils.doWithFields(o.getClass(), new FieldCallback() {
-
-            @Override
-            public void doWith(Field field) throws IllegalArgumentException,
-                    IllegalAccessException {
-
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        ReflectionUtils.doWithFields(bean.getClass(), field -> {
+            if (field.getAnnotation(GeoPlatformLog.class) != null) {
                 ReflectionUtils.makeAccessible(field);
-
-                if (field.getAnnotation(GeoPlatformLog.class) != null) {
-                    Logger logger = LoggerFactory.getLogger(o.getClass());
-                    field.set(o, logger);
-                }
+                Logger logger = LoggerFactory.getLogger(bean.getClass());
+                field.set(bean, logger);
             }
-
         });
-
-        return o;
+        return bean;
     }
 
+    /**
+     * Apply this {@code BeanPostProcessor} to the given new bean instance <i>after</i> any bean
+     * initialization callbacks (like InitializingBean's {@code afterPropertiesSet}
+     * or a custom init-method). The bean will already be populated with property values.
+     * The returned bean instance may be a wrapper around the original.
+     * <p>In case of a FactoryBean, this callback will be invoked for both the FactoryBean
+     * instance and the objects created by the FactoryBean (as of Spring 2.0). The
+     * post-processor can decide whether to apply to either the FactoryBean or created
+     * objects or both through corresponding {@code bean instanceof FactoryBean} checks.
+     * <p>This callback will also be invoked after a short-circuiting triggered by a
+     * {@link org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor#postProcessBeforeInstantiation} method,
+     * in contrast to all other {@code BeanPostProcessor} callbacks.
+     * <p>The default implementation returns the given {@code bean} as-is.
+     *
+     * @param bean the new bean instance
+     * @param beanName the name of the bean
+     * @return the bean instance to use, either the original or a wrapped one;
+     * if {@code null}, no subsequent BeanPostProcessors will be invoked
+     * @throws BeansException in case of errors
+     * @see InitializingBean#afterPropertiesSet
+     * @see FactoryBean
+     */
     @Override
-    public Object postProcessAfterInitialization(Object o, String string) throws
-            BeansException {
-        return o;
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
     }
-
 }

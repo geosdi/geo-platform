c88af6e8d23e15f9358e9eeb9b2443f56aae78ae
vitosalvia <vito.salvia@gmail.com>
2022-01-18 16:54:05 +0100

    Fixed imports

--- geoplatform-tools/geoplatform-connectors/geoplatform-geoserver-rest/geoplatform-geoserver-connector-api/src/main/java/org/geosdi/geoplatform/connector/geoserver/security/base/GPGeoserverBaseCreateUserRequest.java
+++ geoplatform-tools/geoplatform-connectors/geoplatform-geoserver-rest/geoplatform-geoserver-connector-api/src/main/java/org/geosdi/geoplatform/connector/geoserver/security/base/GPGeoserverBaseCreateUserRequest.java
@@ -35,8 +35,8 @@
 package org.geosdi.geoplatform.connector.geoserver.security.base;
 
 import com.google.common.io.CharStreams;
-import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.StringEntity;
 import org.geosdi.geoplatform.connector.geoserver.model.security.user.GPGeoserverUserBody;
 import org.geosdi.geoplatform.connector.geoserver.request.security.users.base.GeoserverBaseCreateUserRequest;
 import org.geosdi.geoplatform.connector.server.GPServerConnector;
@@ -52,7 +52,7 @@ import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static java.lang.ThreadLocal.withInitial;
 import static javax.annotation.meta.When.NEVER;
-import static org.apache.hc.core5.http.ContentType.APPLICATION_XML;
+import static org.apache.http.entity.ContentType.APPLICATION_XML;
 
 /**
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group

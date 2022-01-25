a3b77883011bda7c0a1fbcb130db7d077bde891e
vitosalvia <vito.salvia@gmail.com>
2022-01-25 14:14:46 +0100

    Fixed imports

--- geoplatform-tools/geoplatform-connectors/geoplatform-geoserver-rest/geoplatform-geoserver-connector-api/src/main/java/org/geosdi/geoplatform/connector/geoserver/security/services/GPGeoserverUpdateAclServicesRequest.java
+++ geoplatform-tools/geoplatform-connectors/geoplatform-geoserver-rest/geoplatform-geoserver-connector-api/src/main/java/org/geosdi/geoplatform/connector/geoserver/security/services/GPGeoserverUpdateAclServicesRequest.java
@@ -36,8 +36,8 @@ package org.geosdi.geoplatform.connector.geoserver.security.services;
 
 import com.google.common.io.CharStreams;
 import net.jcip.annotations.ThreadSafe;
-import org.apache.http.HttpEntity;
-import org.apache.http.entity.StringEntity;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.geosdi.geoplatform.connector.geoserver.model.security.rule.GPGeoserverRules;
 import org.geosdi.geoplatform.connector.geoserver.model.security.rule.GeoserverRules;
 import org.geosdi.geoplatform.connector.geoserver.request.security.services.GeoserverUpdateAclServicesRequest;
@@ -53,7 +53,7 @@ import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
 import static java.lang.ThreadLocal.withInitial;
 import static javax.annotation.meta.When.NEVER;
-import static org.apache.http.entity.ContentType.APPLICATION_JSON;
+import static org.apache.hc.core5.http.ContentType.APPLICATION_JSON;
 
 /**
  * @author Vito Salvia - CNR IMAA geoSDI Group

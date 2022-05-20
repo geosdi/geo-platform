b109409368bd95b4a8a353a1e09f54dc2a3a4114
Giuseppe La Scaleia <glascaleia@gmail.com>
2022-05-20 12:32:08 +0200

    FIXED Jenkins Build Failure

--- geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-wms-connector/src/test/java/org/geosdi/geoplatform/connector/wms/stax2/WMSGetFeatureInfoAaltoReaderTest.java
+++ geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-wms-connector/src/test/java/org/geosdi/geoplatform/connector/wms/stax2/WMSGetFeatureInfoAaltoReaderTest.java
@@ -37,14 +37,10 @@ package org.geosdi.geoplatform.connector.wms.stax2;
 
 import org.geosdi.geoplatform.connector.reader.stax2.GPWMSGetFeatureInfoStax2Gml2Reader;
 import org.geosdi.geoplatform.connector.wms.WMSGetFeatureInfoReaderFileLoaderTest;
-import org.geosdi.geoplatform.support.jackson.GPJacksonSupport;
-import org.geosdi.geoplatform.support.jackson.JacksonSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.geosdi.geoplatform.connector.reader.stax2.GPWMSGetFeatureInfoStax2Gml2Reader.aaltoConfigureForSpeed;
-import static org.geosdi.geoplatform.support.jackson.property.GPJacksonSupportEnum.*;
-import static org.geosdi.geoplatform.support.jackson.property.GPJsonIncludeFeature.NON_NULL;
 
 /**
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
@@ -54,10 +50,5 @@ public class WMSGetFeatureInfoAaltoReaderTest extends WMSGetFeatureInfoReaderFil
 
     protected final Logger logger = LoggerFactory.getLogger(this.getClass());
     //
-    protected static final JacksonSupport JACKSON_SUPPORT = new GPJacksonSupport(UNWRAP_ROOT_VALUE_DISABLE,
-            FAIL_ON_UNKNOW_PROPERTIES_DISABLE,
-            ACCEPT_SINGLE_VALUE_AS_ARRAY_ENABLE,
-            WRAP_ROOT_VALUE_DISABLE,
-            INDENT_OUTPUT_ENABLE, NON_NULL);
     protected static final GPWMSGetFeatureInfoStax2Gml2Reader wmsGetFeatureInfoAaltoReader = aaltoConfigureForSpeed();
 }

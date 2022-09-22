6bc8ffcec302f640750dba49ec8fb52eaedc938c
Giuseppe La Scaleia <glascaleia@gmail.com>
2022-09-22 16:29:41 +0200

    Improvements

--- geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-wms-connector/src/test/java/org/geosdi/geoplatform/connector/wms/WMSGetFeatureInfoReaderFileLoaderTest.java
+++ geoplatform-tools/geoplatform-connectors/geoplatform-ogc-connectors/geoplatform-wms-connector/src/test/java/org/geosdi/geoplatform/connector/wms/WMSGetFeatureInfoReaderFileLoaderTest.java
@@ -115,7 +115,8 @@ public class WMSGetFeatureInfoReaderFileLoaderTest {
                 "CentraliElettriche.xml", "features.xml", "Geositi.xml", "ADBRisk.xml", "PoliziaIDR.xml",
                 "ParchiRegionaliRiserve.xml", "ReteGeodetica.xml", "ElementiRidotti.xml", "UsoSuoloRT.xml",
                 "AlberiMonumentali.xml", "AcqueSecondarie.xml", "AdminGeo.xml", "IDT_SU20G_ZONIZZAZIONE.xml",
-                "CBLN_COMPRENSORIO.xml", "Biodiversit_ittica_acque_correnti.xml", "RegimeSperimentale.xml");
+                "CBLN_COMPRENSORIO.xml", "Biodiversit_ittica_acque_correnti.xml", "RegimeSperimentale.xml",
+                "EMSR634_AOI01_GRA_PRODUCT_builtUpP_r1_v1.xml");
     }
 
     /**

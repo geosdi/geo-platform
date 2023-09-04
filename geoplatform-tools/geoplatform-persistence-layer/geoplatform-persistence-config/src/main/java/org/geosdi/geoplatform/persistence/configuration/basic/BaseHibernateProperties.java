05db3fdb98627bd1c122a4f5aba22ecf16a874c0
Giuseppe La Scaleia <glascaleia@gmail.com>
2023-09-04 10:32:23 +0200

    Upgrade Hibernate to latest version and fix problem on startup

--- geoplatform-tools/geoplatform-persistence-layer/geoplatform-persistence-config/src/main/java/org/geosdi/geoplatform/persistence/configuration/basic/BaseHibernateProperties.java
+++ geoplatform-tools/geoplatform-persistence-layer/geoplatform-persistence-config/src/main/java/org/geosdi/geoplatform/persistence/configuration/basic/BaseHibernateProperties.java
@@ -47,6 +47,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Properties;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static java.lang.Boolean.FALSE;
 
 /**
  * @author Giuseppe La Scaleia - CNR IMAA geoSDI Group
@@ -82,6 +83,9 @@ class BaseHibernateProperties implements PersistenceHibernateStrategy {
                 this.put("hibernate.hbm2ddl.auto", gpHibernateProperties.getHibHbm2ddlAuto());
                 this.put("hibernate.show_sql", gpHibernateProperties.isHibShowSql());
                 this.put("hibernate.generate_statistics", gpHibernateProperties.isHibGenerateStatistics());
+                this.put("hibernate.enhancer.enableAssociationManagement", FALSE);
+                this.put("hibernate.enhancer.enableDirtyTracking", FALSE);
+                this.put("hibernate.enhancer.enableLazyInitialization", FALSE);
                 if ((gpHibernateProperties.getHibDefaultSchema() != null) && !(gpHibernateProperties.getHibDefaultSchema().trim().isEmpty())) {
                     this.put("hibernate.default_schema", gpHibernateProperties.getHibDefaultSchema());
                 }

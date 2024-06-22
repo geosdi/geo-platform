da70437a06f0bb32923dfa0363cc62194783fe26
Giuseppe La Scaleia <glascaleia@gmail.com>
2024-06-22 15:29:32 +0200

    Improvements

--- geoplatform-experimental/geoplatform-rest-security/geoplatform-rest-jwt-support/src/main/java/org/geosdi/geoplatform/experimental/jwt/support/service/GPJwtServiceSupport.java
+++ geoplatform-experimental/geoplatform-rest-security/geoplatform-rest-jwt-support/src/main/java/org/geosdi/geoplatform/experimental/jwt/support/service/GPJwtServiceSupport.java
@@ -34,21 +34,34 @@
  */
 package org.geosdi.geoplatform.experimental.jwt.support.service;
 
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.jackson.io.JacksonDeserializer;
+import io.jsonwebtoken.jackson.io.JacksonSerializer;
 import lombok.Getter;
 import lombok.ToString;
 import net.jcip.annotations.Immutable;
 import org.geosdi.geoplatform.experimental.jwt.support.spring.configuration.IGPJwtConfiguration;
+import org.geosdi.geoplatform.support.jackson.GPJacksonSupport;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.security.SignatureException;
 import java.util.Map;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static io.jsonwebtoken.Jwts.ZIP.GZIP;
 import static io.jsonwebtoken.Jwts.builder;
 import static io.jsonwebtoken.Jwts.parser;
+import static java.lang.Boolean.TRUE;
+import static java.util.Map.of;
 import static javax.annotation.meta.When.NEVER;
+import static org.geosdi.geoplatform.support.jackson.annotation.JacksonXmlAnnotationIntrospectorBuilder.JAKARTA;
+import static org.geosdi.geoplatform.support.jackson.property.GPJacksonSupportEnum.*;
+import static org.geosdi.geoplatform.support.jackson.property.GPJsonIncludeFeature.NON_NULL;
 import static org.joda.time.DateTime.now;
 
 /**
@@ -60,6 +73,15 @@ import static org.joda.time.DateTime.now;
 class GPJwtServiceSupport implements IGPJwtServiceSupport {
 
     private static final long serialVersionUID = 6730444116638398822L;
+    //
+    private static final GPJacksonSupport jacksonSupport = new GPJacksonSupport(JAKARTA, UNWRAP_ROOT_VALUE_DISABLE,
+            FAIL_ON_UNKNOW_PROPERTIES_DISABLE,
+            ACCEPT_SINGLE_VALUE_AS_ARRAY_ENABLE,
+            WRAP_ROOT_VALUE_DISABLE,
+            INDENT_OUTPUT_ENABLE, NON_NULL)
+            .registerModule(new JodaModule())
+            .configure(WRITE_DATES_AS_TIMESTAMPS_DISABLE);
+    private static final JacksonSerializer jacksonSerializer = new JacksonSerializer<>(jacksonSupport.getDefaultMapper());
     //
     @Getter
     private final IGPJwtConfiguration jwtConfiguration;
@@ -94,7 +116,11 @@ class GPJwtServiceSupport implements IGPJwtServiceSupport {
      */
     @Override
     public boolean isTokenExpired(@Nonnull(when = NEVER) String theToken) throws Exception {
-        return this.extractClaim(theToken, Claims::getExpiration).before(now().toDate());
+        try {
+            return this.extractClaim(theToken, Claims::getExpiration).before(now().toDate());
+        } catch (ExpiredJwtException | SignatureException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException ex) {
+            return TRUE;
+        }
     }
 
     /**
@@ -117,7 +143,8 @@ class GPJwtServiceSupport implements IGPJwtServiceSupport {
     public String generateToken(@Nonnull(when = NEVER) String theUsername, @Nullable Map<String, Object> theExtraClaims) throws Exception {
         checkArgument((theUsername != null) && !(theUsername.trim().isEmpty()), "The Parameter username must not be null or an empty string");
         return builder()
-                .claims(theExtraClaims != null ? theExtraClaims : Map.of())
+                .json(jacksonSerializer)
+                .claims(theExtraClaims != null ? theExtraClaims : of())
                 .subject(theUsername)
                 .issuedAt(now().toDate())
                 .expiration(now().plus(this.jwtConfiguration.getExpiration()).toDate())
@@ -135,11 +162,33 @@ class GPJwtServiceSupport implements IGPJwtServiceSupport {
     public String generateRefreshToken(@Nonnull(when = NEVER) String theUsername) throws Exception {
         checkArgument((theUsername != null) && !(theUsername.trim().isEmpty()), "The Parameter username must not be null or an empty string");
         return builder()
+                .json(jacksonSerializer)
                 .subject(theUsername)
                 .issuedAt(now().toDate())
                 .expiration(now().plus(this.jwtConfiguration.getRefreshTokenExpiration()).toDate())
                 .signWith(this.jwtConfiguration.getSecretKey())
                 .compressWith(GZIP)
                 .compact();
+    }
+
+    /**
+     * @param theToken
+     * @param theClaimType
+     * @param theClaimKey
+     * @return {@link T}
+     * @throws Exception
+     */
+    @Override
+    public <T> T extractClaim(@Nonnull(when = NEVER) String theToken, @Nonnull(when = NEVER) Class<T> theClaimType, @Nonnull(when = NEVER) String theClaimKey) throws Exception {
+        checkArgument((theToken != null) && !(theToken.trim().isEmpty()), "The Parameter token must not be null or an empty string");
+        checkArgument(theClaimType != null, "The Parameter claimType must not be null.");
+        checkArgument((theClaimKey != null) && !(theClaimKey.trim().isEmpty()), "The Parameter claimKey must not be null or an empty string");
+        return parser()
+                .json(new JacksonDeserializer<>(of(theClaimKey, theClaimType)))
+                .verifyWith(this.jwtConfiguration.getSecretKey())
+                .build()
+                .parseSignedClaims(theToken)
+                .getPayload()
+                .get(theClaimKey, theClaimType);
     }
 }
